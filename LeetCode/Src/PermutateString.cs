df9708258031303c167abc8f3d7c6121f3745d77
Ji <jjeonie@gmail.com>
2020-04-26 10:54:50 +1200

    LeetCode Weekly Contest 185:P1417 Reformat The String

---
+++ LeetCode/Src/PermutateString.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingPractice.Src {
+    class PermutateString {
+        internal PermutateString () { }
+
+        internal string Permutate (string s) {
+            if (s.Length == 1) {
+                // If string only contains one character, output string input
+                return s;
+            } else if (!CanPermutate(s)) {
+                return String.Empty;
+            }
+
+            // Get all the digits
+            Char[] digits = s.ToCharArray().Where(y => Char.IsDigit(y)).ToArray();
+            // Get all the characters
+            Char[] characters = s.ToCharArray().Where(y => Char.IsLetter(y)).ToArray();
+
+            // Perform permutation
+            string output = String.Empty;
+            if (digits.Length > characters.Length) {
+                output = CombineArray(characters, digits);
+                output += digits[digits.Length - 1];
+            } else if (characters.Length > digits.Length) {
+                output = CombineArray(digits, characters);
+                output += characters[characters.Length - 1];
+            } else {
+                output = CombineArray(digits, characters);
+            }
+
+            return output;
+        }
+
+        internal string CombineArray (Char[] smallerArray, Char[] largerArray) {
+            string output = String.Empty;
+            for (int i = 0; i < smallerArray.Length; i++) {
+                output += largerArray[i];
+                output += smallerArray[i];
+            }
+            return output;
+        }
+
+        internal bool CanPermutate (string s) {
+            int digitCount = s.Count(x => Char.IsDigit(x));
+            int charCount = s.Length - digitCount;
+
+            if (!s.Any(char.IsDigit) || !s.Any(char.IsLetter)) {
+                // Check if there are a combination of digits and characters
+                return false;
+            } else if (digitCount >= charCount + 2 || digitCount <= charCount - 2) {
+                // Check if there are enough digits and strings to perform permutations
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/PermutateStringTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CodingPractice.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingPractice.Test {
+    [TestClass]
+    public class PermutateStringTest {
+        [TestMethod]
+        public void CanPermutateAlphanumericString () {
+            PermutateString permutateString = new PermutateString();
+            Assert.AreEqual("a0b1c2", permutateString.Permutate("a0b1c2"));
+            Assert.AreEqual("c2o0v2i0d", permutateString.Permutate("covid2020"));
+        }
+
+        [TestMethod]
+        public void CannotPermutateAlphaString() {
+            Assert.AreEqual("", new PermutateString().Permutate("leetcode"));
+        }
+
+        [TestMethod]
+        public void CannotPermuateNumericString() {
+            PermutateString permutateString = new PermutateString();
+            Assert.AreEqual("", new PermutateString().Permutate("1229857369"));
+        }
+    }
+}
