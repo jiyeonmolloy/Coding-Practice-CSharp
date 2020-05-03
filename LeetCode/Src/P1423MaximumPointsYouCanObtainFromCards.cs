2c5fbb7ac7d62ccd800a81c2be4ec9a92db72499
Ji <jjeonie@gmail.com>
2020-05-03 14:21:19 +1200

    LeetCode Weekly Contest 186:{1423 Maximum Points You Can Obtain from Cards

---
+++ LeetCode/Src/P1423MaximumPointsYouCanObtainFromCards.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1423: Maximum Points You Can Obtain From Cards. href = https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+    /// </summary>
+    class P1423MaximumPointsYouCanObtainFromCards {
+        public static int MaxScore (int[] cardPoints, int k) {
+            int cardSize = cardPoints.Length;
+            int maxScore = 0;
+
+            // Get k number of cards from the right and add to maximum score
+            for (int i = cardSize - k; i < cardSize; i++) {
+                maxScore += cardPoints[i];
+            }
+
+            // Sliding window
+            int score = maxScore;
+            for (int i = 0; i < k; i++) {
+                score -= cardPoints[cardSize - k + i];
+                score += cardPoints[i];
+                maxScore = Math.Max(maxScore, score);
+            }
+
+            return maxScore;
+        }
+    }
+}
---
+++ LeetCode/Test/P1423MaximumPointsYouCanObtainFromCardsTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1423MaximumPointsYouCanObtainFromCardsTest {
+        [TestMethod]
+        public void MaximuPointsObtainedFromCards () {
+            int[] test = new int[7] { 1, 2, 3, 4, 5, 6, 1 };
+            Assert.AreEqual(12, P1423MaximumPointsYouCanObtainFromCards.MaxScore(test, 3));
+
+            test = new int[3] { 2, 2, 2 };
+            Assert.AreEqual(4, P1423MaximumPointsYouCanObtainFromCards.MaxScore(test, 2));
+
+            test = new int[8] { 1, 400, 1, 1, 1, 1, 200, 1 };
+            Assert.AreEqual(402, P1423MaximumPointsYouCanObtainFromCards.MaxScore(test, 3));
+        }
+    }
+}
