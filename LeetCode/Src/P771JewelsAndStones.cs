c2afd2e15e3811cf43a6506752671a11921781ab
Ji <jjeonie@gmail.com>
2020-05-03 14:25:11 +1200

    LeetCode P771 Number Of Jewels

---
+++ LeetCode/Src/P771JewelsAndStones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P771 Jewels And Stones. href = https://leetcode.com/problems/jewels-and-stones/
+    /// </summary>
+    class P771JewelsAndStones {
+        public static int NumJewelsInStones (string J, string S) {
+            int jewels = 0;
+
+            foreach (char jewel in J) {
+                jewels += S.Count(x => x == jewel);
+            }
+
+            return jewels;
+        }
+    }
+}
---
+++ LeetCode/Test/P771JewelsAndStonesTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P771JewelsAndStonesTest {
+        [TestMethod]
+        public void NumberOfJewels () {
+            Assert.AreEqual(3, P771JewelsAndStones.NumJewelsInStones("aA", "aAAbbbb"));
+            Assert.AreEqual(0, P771JewelsAndStones.NumJewelsInStones("z", "ZZ"));
+        }
+    }
+}
