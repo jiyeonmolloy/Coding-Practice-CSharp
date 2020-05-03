9dc3985254c0e859d5fb7f3aef0a3a7e249cccff
Ji <jjeonie@gmail.com>
2020-05-03 14:14:26 +1200

    LeetCode Weekly Contest 186:P1422 Maximum Score After Splitting A String

---
+++ LeetCode/Src/P1422MaximumScoreAfterSplittingAString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1422 Maximum Score After Splitting a String. href = https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+    /// </summary>
+    class P1422MaximumScoreAfterSplittingAString {
+        public static int MaxScore (string s) {
+            List<int> score = s.Select(x => int.Parse(x.ToString())).ToList();
+
+            int leftScore = 0;
+            int rightScore = 0;
+
+            HashSet<int> scoreList = new HashSet<int>();
+            for (int i = 1; i < score.Count; i++) {
+                leftScore = score.Take(i).ToList().Count(x => x == 0);
+                rightScore = score.Skip(i).ToList().Count(x => x == 1);
+
+                scoreList.Add(leftScore + rightScore);
+            }
+
+            return scoreList.Max();
+        }
+    }
+}
---
+++ LeetCode/Test/P1422MaximumScoreAfterSplittingAStringTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1422MaximumScoreAfterSplittingAStringTest {
+        [TestMethod]
+        public void MaximumScore () {
+            Assert.AreEqual(5, P1422MaximumScoreAfterSplittingAString.MaxScore("011101"));
+            Assert.AreEqual(5, P1422MaximumScoreAfterSplittingAString.MaxScore("00111"));
+            Assert.AreEqual(3, P1422MaximumScoreAfterSplittingAString.MaxScore("1111"));
+        }
+    }
+}
