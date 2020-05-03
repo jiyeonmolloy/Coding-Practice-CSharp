25e82e64965f74ed3bbcf1d9db16f79f49bce1b3
Ji <jjeonie@gmail.com>
2020-05-03 20:02:22 +1200

    LeetCode Weekly Contest 187: P1437 Check If All 1's Are at Least Length K Places Away

---
+++ LeetCode/Src/P1437CheckIfAll1sAreAtLeastKPlacesAway.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1437 Check If All 1's Are at Least Length K Places Away. href = https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+    /// </summary>
+    class P1437CheckIfAll1sAreAtLeastKPlacesAway {
+        public static bool KLengthApart (int[] nums, int k) {
+            // Find position of 1 in nums array
+            List<int> indexList = nums.Select((n, i) => new { i, n }).Where(t => t.n == 1).Select(t => t.i).ToList();
+
+            int size = indexList.Count();
+            int distance = 0;
+            for (int i = size - 1; i > 0; --i) {
+                distance = indexList[i] - indexList[i - 1] - 1;
+                if (distance < k) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Test/P1437CheckIfAll1sAreAtLeastKPlacesAwayTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1437CheckIfAll1sAreAtLeastKPlacesAwayTest {
+        [TestMethod]
+        public void All1sAreAtLeastKPlacesAway () {
+            int[] test = new int[8] { 1, 0, 0, 0, 1, 0, 0, 1 };
+            Assert.IsTrue(P1437CheckIfAll1sAreAtLeastKPlacesAway.KLengthApart(test, 2));
+
+            test = new int[5] { 1, 1, 1, 1, 1 };
+            Assert.IsTrue(P1437CheckIfAll1sAreAtLeastKPlacesAway.KLengthApart(test, 0));
+
+            test = new int[4] { 0, 1, 0, 1 };
+            Assert.IsTrue(P1437CheckIfAll1sAreAtLeastKPlacesAway.KLengthApart(test, 1));
+        }
+
+        [TestMethod]
+        public void All1sAreNotKPlacesAway () {
+            int[] test = new int[6] { 1, 0, 0, 1, 0, 1 };
+            Assert.IsFalse(P1437CheckIfAll1sAreAtLeastKPlacesAway.KLengthApart(test, 2));
+        }
+    }
+}
