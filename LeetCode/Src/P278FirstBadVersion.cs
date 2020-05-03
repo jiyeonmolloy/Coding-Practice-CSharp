d0ac40516a918e05de302eb5e75e504c01bc0405
Ji <jjeonie@gmail.com>
2020-05-03 14:30:50 +1200

    LeetCode P278 First Bad Version

---
+++ LeetCode/Src/P278FirstBadVersion.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P278 First Bad Version. href = https://leetcode.com/problems/first-bad-version/
+    /// Key: Binary Search
+    /// </summary>
+    class P278FirstBadVersion {
+        private int badVersion;
+
+        public P278FirstBadVersion(int badVersion) {
+            this.badVersion = badVersion;
+        }
+
+        public int FirstBadVersion (int n) {
+            int left = 1;
+            int badVersion = n;
+
+            while (left < badVersion) {
+                int mid = left + ((badVersion - left) / 2);
+
+                if (IsBadVersion(mid)) {
+                    badVersion = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return badVersion;
+        }
+
+        private bool IsBadVersion (int version) {
+            if (version >= badVersion) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/P278FirstBadVersionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P278FirstBadVersionTest {
+        [TestMethod]
+        public void FirstBadVersion () {
+            P278FirstBadVersion p278FirstBadVersion = new P278FirstBadVersion(4);
+            Assert.AreEqual(4, p278FirstBadVersion.FirstBadVersion(5));
+            Assert.AreEqual(4, p278FirstBadVersion.FirstBadVersion(254));
+        }
+    }
+}
