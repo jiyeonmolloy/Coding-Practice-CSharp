879aa9681adce91b33804fb0fa8499e3c6ced116
Ji <jjeonie@gmail.com>
2020-05-04 21:56:52 +1200

    LeetCode P476 Number Complement

---
+++ LeetCode/Src/P476NumberComplement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    class P476NumberComplement {
+        /// <summary>
+        /// P476 Number Complement. href = https://leetcode.com/problems/number-complement/
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int FindComplement (int num) {
+            if (num == 0) {
+                return Int32.MaxValue;
+            }
+
+            char[] binaries = Convert.ToString((int)num, 2).ToCharArray();
+
+            for (int i = 0; i < binaries.Length; i++) {
+                if (binaries[i] == '0') {
+                    binaries[i] = '1';
+                } else {
+                    binaries[i] = '0';
+                }
+            }
+
+            return Convert.ToInt32(new string(binaries), 2);
+        }
+    }
+}
---
+++ LeetCode/Test/P476NumberComplementTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P476NumberComplementTest {
+        [TestMethod]
+        public void FindBinaryComplement () {
+            Assert.AreEqual(2, P476NumberComplement.FindComplement(5));
+            Assert.AreEqual(0, P476NumberComplement.FindComplement(1));
+            Assert.AreEqual(Int32.MaxValue, P476NumberComplement.FindComplement(0));
+        }
+    }
+}
