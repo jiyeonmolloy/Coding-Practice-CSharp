9584990fbc51d62de6f6b28b1d7cfb0c67cbeae6
Ji <jjeonie@gmail.com>
2020-05-04 21:51:59 +1200

    LeetCode P383 Ransom Note

---
+++ LeetCode/Src/P383RansomNote.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P383 Ransom Note. href = https://leetcode.com/problems/ransom-note/
+    /// </summary>
+    class P383RansomNote {
+        public static bool CanConstruct (string ransomNote, string magazine) {
+            List<char> magazineList = magazine.ToCharArray().ToList();
+
+            foreach (char ransomNoteChar in ransomNote) {
+                if (magazineList.Contains(ransomNoteChar)) {
+                    magazineList.Remove(ransomNoteChar);
+                } else {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Test/P383RansomNoteTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P383RansomNoteTest {
+        [TestMethod]
+        public void CanConstructRansomNote () {
+            Assert.IsTrue(P383RansomNote.CanConstruct("aa", "aab"));
+        }
+
+        [TestMethod]
+        public void CannoteConstructRansomNote () {
+            Assert.IsFalse(P383RansomNote.CanConstruct("a", "b"));
+            Assert.IsFalse(P383RansomNote.CanConstruct("aa", "ab"));
+        }
+    }
+}
