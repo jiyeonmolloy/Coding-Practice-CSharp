91a6fb9bd38e2264d7731ffb708138f5514925ae
Ji <jjeonie@gmail.com>
2020-05-06 22:46:40 +1200

    LeetCode P169 Majority Element

---
+++ LeetCode/Src/P169MajorityElement.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P169 Majority Element. href = https://leetcode.com/problems/majority-element/
+    /// </summary>
+    class P169MajorityElement {
+        public static int MajorityElement (int[] nums) {
+            int majority = nums.Length / 2;
+            int count;
+            int analyzeInt;
+
+            while (nums.Length > 0) {
+                count = 1;
+                analyzeInt = nums[0];
+                for (int i = 1; i < nums.Length; i++) {
+                    if (analyzeInt == nums[i]) {
+                        count++;
+                    }
+                }
+
+                if (count > majority) {
+                    return analyzeInt;
+                }
+
+                nums = nums.Where(x => x != analyzeInt).ToArray();
+            }
+
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Test/P169MajorityElementTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P169MajorityElementTest {
+        [TestMethod]
+        public void FindMajorityElement () {
+            int[] test = new int[3] { 3, 2, 3 };
+            Assert.AreEqual(3, P169MajorityElement.MajorityElement(test));
+
+            test = new int[7] { 2, 2, 1, 1, 1, 2, 2 };
+            Assert.AreEqual(2, P169MajorityElement.MajorityElement(test));
+        }
+    }
+}
