d2247ef03c59362e80f472e8139496513338071d
Ji <jjeonie@gmail.com>
2020-05-06 22:43:45 +1200

    LeetCode P387 First Unique Character in a String

---
+++ LeetCode/Src/P387FirstUniqueCharacterInString.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P387 First Unique Character In String. href = https://leetcode.com/problems/first-unique-character-in-a-string/
+    /// </summary>
+    class P387FirstUniqueCharacterInString {
+        public static int FirstUniqChar (string s) {
+            bool isUniqueChar;
+            string analyze = s;
+            char analyzeChar;
+
+            while (analyze.Length > 0) {
+                isUniqueChar = true;
+                analyzeChar = analyze[0];
+                for (int i = 1; i < analyze.Length; i++) {
+                    if (analyzeChar == analyze[i]) {
+                        isUniqueChar = false;
+                        break;
+                    }
+                }
+
+                if (isUniqueChar) {
+                    return s.IndexOf(analyzeChar);
+                }
+
+                analyze = analyze.Replace(analyzeChar.ToString(), string.Empty);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Test/P387FirstUniqueCharacterInStringTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P387FirstUniqueCharacterInStringTest {
+        [TestMethod]
+        public void FindFirstUniqueCharacterInString () {
+            Assert.AreEqual(0, P387FirstUniqueCharacterInString.FirstUniqChar("leetcode"));
+            Assert.AreEqual(2, P387FirstUniqueCharacterInString.FirstUniqChar("loveleetcode"));
+        }
+
+        [TestMethod]
+        public void FindNoUniqueCharacterInEmptyString () {
+            Assert.AreEqual(-1, P387FirstUniqueCharacterInString.FirstUniqChar(""));
+        }
+    }
+}
