a18245968a69418f22bd65c377cd294d6c4fcd22
Ji <jjeonie@gmail.com>
2020-05-08 22:15:10 +1200

    LeetCode P965 Univalued Binary Tree

---
+++ LeetCode/Src/P965UnivaluedBinaryTree.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P965 Univalued Binary Tree. href = https://leetcode.com/problems/univalued-binary-tree/
+    /// Key: DFS
+    /// </summary>
+    class P965UnivaluedBinaryTree : TreeNode {
+        public static bool IsUnivalTree (TreeNode root) {
+            return Dfs(root, root.val);
+        }
+
+        private static bool Dfs (TreeNode root, int val) {
+            if (root == null) { // End of tree
+                return true;
+            }
+
+            if (root.val != val) {
+                return false;
+            }
+
+            return Dfs(root.left, val) && Dfs(root.right, val);
+        }
+    }
+}
---
+++ LeetCode/Test/P965UnivaluedBinaryTreeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P965UnivaluedBinaryTreeTest : TreeNode {
+        [TestMethod]
+        public void IsUnivaluedBinaryTree () {
+            TreeNode treeNode = new TreeNode(1, new TreeNode(1, new TreeNode(1, new TreeNode(1))), new TreeNode(1, new TreeNode(1)));
+            Assert.IsTrue(P965UnivaluedBinaryTree.IsUnivalTree(treeNode));
+        }
+
+        [TestMethod]
+        public void IsNotUnivaluedBinaryTree () {
+            TreeNode treeNode = new TreeNode(1, new TreeNode(1, new TreeNode(1, new TreeNode(1))), new TreeNode(1, new TreeNode(2)));
+            Assert.IsFalse(P965UnivaluedBinaryTree.IsUnivalTree(treeNode));
+        }
+    }
+}
