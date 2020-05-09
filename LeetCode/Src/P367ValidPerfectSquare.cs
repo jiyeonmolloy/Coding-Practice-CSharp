896a97da0ed3fd6cebb340ca4733d6623ce2173b
Ji <jjeonie@gmail.com>
2020-05-09 19:51:21 +1200

    LeetCide P367 Valid Perfect Square Test

---
+++ LeetCode/Src/P367ValidPerfectSquare.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P367 Valid Perfect Square. href = https://leetcode.com/problems/valid-perfect-square/
+    /// </summary>
+    class P367ValidPerfectSquare {
+        public static bool IsPerfectSquare (int num) {
+            for (long i = 1; i * i <= num; i++) {
+                if (i * i == num) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P367ValidPerfectSquareTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P367ValidPerfectSquareTest {
+        [TestMethod]
+        public void IsPerfectSquare () {
+            Assert.IsTrue(P367ValidPerfectSquare.IsPerfectSquare(16));
+        }
+
+        [TestMethod]
+        public void IsNotPerfectSquare () {
+            Assert.IsFalse(P367ValidPerfectSquare.IsPerfectSquare(14));
+        }
+    }
+}
