6ce8852111e76f460e1846a2fc208a81da73a427
Ji <jjeonie@gmail.com>
2020-05-10 16:43:20 +1200

    LeetCode Weekly Contest 188: P1442 Count Triplets That Can Form Two Arrays Of Equal XOR Test

---
+++ LeetCode/Src/P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LeetCode.Src {
+    class P1442CountTripletsThatCanFormTwoArraysOfEqualXOR {
+        /// <summary>
+        /// P1442 Count Triplets That Can Form Two Arrays Of Equal XOR. href = https://leetcode.com/problems/count-triplets-that-can-form-two-arrays-of-equal-xor/
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static int CountTriplets (int[] arr) {
+            int count = 0;
+            int a, b;
+            int length = arr.Length;
+
+            for (int i = 0; i < length; i++) {
+                for (int j = i + 1; j < length; j++) {
+                    if (i < j) {
+                        a = PerformXOROnArray(i, j, arr);
+                        for (int k = j; k < length; k++) {
+                            if (j <= k) {
+                                b = PerformXOROnArray(j, k + 1, arr);
+
+                                if (a == b) {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        private static int PerformXOROnArray (int start, int end, int[] arr) {
+            int output = 0;
+            for (int i = start; i < end; i++) {
+                output ^= arr[i];
+            }
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1442CountTripletsThatCanFormTwoArraysOfEqualXORTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1442CountTripletsThatCanFormTwoArraysOfEqualXORTest {
+        [TestMethod]
+        public void CountTripletsThatCanFormTwoArraysOfEqualXOR () {
+            int[] test = new int[5] { 2, 3, 1, 6, 7 };
+            Assert.AreEqual(4, P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.CountTriplets(test));
+
+            test = new int[5] { 1, 1, 1, 1, 1 };
+            Assert.AreEqual(10, P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.CountTriplets(test));
+
+            test = new int[2] { 2, 3 };
+            Assert.AreEqual(0, P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.CountTriplets(test));
+
+            test = new int[5] { 1, 3, 5, 7, 9 };
+            Assert.AreEqual(3, P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.CountTriplets(test));
+
+            test = new int[9] { 7, 11, 12, 9, 5, 2, 7, 17, 22 };
+            Assert.AreEqual(8, P1442CountTripletsThatCanFormTwoArraysOfEqualXOR.CountTriplets(test));
+        }
+    }
+}
