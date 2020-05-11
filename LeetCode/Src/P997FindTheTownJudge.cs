58e33f7f7178d9a8d3e69ef14dd689dea863e1cf
Ji <jjeonie@gmail.com>
2020-05-11 19:33:26 +1200

    LeetCode P997 Find The Town Judge

---
+++ LeetCode/Src/P997FindTheTownJudge.cs
@@ -0,0 +1,30 @@
+﻿
+namespace LeetCode.Src {
+    class P997FindTheTownJudge {
+        /// <summary>
+        /// P997 Find The Town Judge. href = 
+        /// </summary>
+        /// <param name="N"></param>
+        /// <param name="trust"></param>
+        /// <returns></returns>
+        public static int FindJudge (int N, int[][] trust) {
+            int[] trustArray = new int[N + 1]; // Store the person's relationship (Inbound - Outbound)
+            const int MAIN = 0;
+            const int TARGET = 1;
+
+            foreach (int[] singleTrust in trust) {
+                trustArray[singleTrust[MAIN]]--; // Outbount
+                trustArray[singleTrust[TARGET]]++; // Inbound
+            }
+
+            for (int i = 1; i <= N; ++i) {
+                // Mafia satisfies the condition: (Inbound - Outbound) =  N - 1
+                if (trustArray[i] == N - 1) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Test/P997FindTheTownJudgeTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P997FindTheTownJudgeTest {
+        [TestMethod]
+        public void FindTownJudgeTest () {
+            int[][] trust = new int[1][] { new int[2] { 1, 2 } };
+            Assert.AreEqual(2, P997FindTheTownJudge.FindJudge(2, trust));
+
+            trust = new int[2][] { new int[2] { 1, 3 }, new int[2] { 2, 3 } };
+            Assert.AreEqual(3, P997FindTheTownJudge.FindJudge(3, trust));
+
+            trust = new int[3][] { new int[2] { 1, 3 }, new int[2] { 2, 3 }, new int[2] { 3, 1 } };
+            Assert.AreEqual(-1, P997FindTheTownJudge.FindJudge(3, trust));
+
+            trust = new int[2][] { new int[2] { 1, 2 }, new int[2] { 2, 3 } };
+            Assert.AreEqual(-1, P997FindTheTownJudge.FindJudge(3, trust));
+
+            trust = new int[5][] { new int[2] { 1, 3 }, new int[2] { 1, 4 }, new int[2] { 2, 3 }, new int[2] { 2, 4 }, new int[2] { 4, 3 } };
+            Assert.AreEqual(3, P997FindTheTownJudge.FindJudge(4, trust));
+        }
+    }
+}
