8ef3548e098e20958daa08abd94e0b45b064dc98
Ji <jjeonie@gmail.com>
2020-05-12 19:35:33 +1200

    LeetCode P540: Single Element In A Sorted Array

---
+++ LeetCode/Src/P540SingleElementInASortedArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Src {
+    class P540SingleElementInASortedArray {
+        /// <summary>
+        /// P540 Single Element In A Sorted Array. href = https://leetcode.com/problems/single-element-in-a-sorted-array/
+        /// Key: 
+        /// XOR of a number with itself is 0
+        /// XOR of a number with 0 is itself
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNonDuplicate (int[] nums) {
+            int nonDuplicate = nums[0];
+
+            for (int i = 1; i < nums.Length; i++) {
+                nonDuplicate = nonDuplicate ^ nums[i];
+            }
+
+            return nonDuplicate;
+        }
+    }
+}
---
+++ LeetCode/Test/P540SingleElementInASortedArrayTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P540SingleElementInASortedArrayTest {
+        [TestMethod]
+        public void FindSingleElementInASortedArray () {
+            int[] test = new int[9] { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+            Assert.AreEqual(2, P540SingleElementInASortedArray.SingleNonDuplicate(test));
+
+            test = new int[7] { 3, 3, 7, 7, 10, 11, 11 };
+            Assert.AreEqual(10, P540SingleElementInASortedArray.SingleNonDuplicate(test));
+        }
+    }
+}
