98b8c00f6bd8323cccce43669ef163c165217d28
Ji <jjeonie@gmail.com>
2020-05-14 17:29:54 +1200

    LeetCode P402 Remove K Digits

---
+++ LeetCode/Src/P402RemoveKDigits.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    class P402RemoveKDigits {
+        /// <summary>
+        /// P402 Remove K Digits. href = https://leetcode.com/problems/remove-k-digits/
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static string RemoveKdigits (string num, int k) {
+            List<int> numArray = num.Select(s => (s - '0')).ToArray().ToList();
+            int size = numArray.Count;
+
+            if (size == k) {
+                return "0";
+            }
+
+            int i = 0;
+            while (k != 0 && i < (numArray.Count - 1)) {
+                if (numArray[i] > numArray[i + 1]) {
+                    numArray.RemoveAt(i);
+                    k--;
+
+                    if (i > 0) {
+                        i--;
+                    }
+                } else {
+                    i++;
+                }
+            }
+
+            if (k > 0) {
+                numArray.Reverse();
+                numArray.RemoveRange(0, k);
+            }
+
+            if (numArray.All(x => x == 0)) {
+                return "0";
+            } else {
+                return String.Join("", numArray.Select(x => x.ToString()).ToArray()).TrimStart(new Char[] { '0' });
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/P402RemoveKDigitsTest.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P402RemoveKDigitsTest {
+        [TestMethod]
+        public void FindSmallestNumberByRemovingKDigits () {
+            string num = "1432219";
+            int k = 3;
+            Assert.AreEqual("1219", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "14";
+            k = 2;
+            Assert.AreEqual("0", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "10200";
+            k = 1;
+            Assert.AreEqual("200", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "10";
+            k = 2;
+            Assert.AreEqual("0", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "112";
+            k = 1;
+            Assert.AreEqual("11", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "10";
+            k = 1;
+            Assert.AreEqual("0", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "100";
+            k = 1;
+            Assert.AreEqual("0", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "5337";
+            k = 2;
+            Assert.AreEqual("33", P402RemoveKDigits.RemoveKdigits(num, k));
+
+            num = "212";
+            k = 1;
+            Assert.AreEqual("12", P402RemoveKDigits.RemoveKdigits(num, k));
+        }
+    }
+}
