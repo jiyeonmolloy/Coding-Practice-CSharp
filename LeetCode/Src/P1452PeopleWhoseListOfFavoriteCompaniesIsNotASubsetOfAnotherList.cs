b35b6c9a42fc78bf8c2a37d0ab12a63309b8edac
Ji <jjeonie@gmail.com>
2020-05-17 19:19:00 +1200

    LeetCode WeeklyContest 189: P1452 People Whose List of Favorite Companies Is Not a Subset of Another List

---
+++ LeetCode/Src/P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src.Common {
+    class P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList {
+        /// <summary>
+        /// P1452 People Whose List of Favorite Companies Is Not a Subset of Another List. href = https://leetcode.com/problems/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list/
+        /// </summary>
+        /// <param name="favoriteCompanies"></param>
+        /// <returns></returns>
+        public static IList<int> PeopleIndexes (IList<IList<string>> favoriteCompanies) {
+            List<HashSet<string>> companyHashSet = new List<HashSet<string>>();
+            IList<int> output = new List<int>();
+
+            foreach (var companyList in favoriteCompanies) {
+                companyHashSet.Add(companyList.ToHashSet());
+            }
+
+            int size = companyHashSet.Count;
+            bool isSubset;
+            for (int i = 0; i < size; i++) {
+                isSubset = false;
+                for (int j = 0; j < size; j++) {
+                    if (i != j && companyHashSet[i].IsSubsetOf(companyHashSet[j])) {
+                        isSubset = true;
+                        break;
+                    }
+                }
+
+                if (!isSubset) {
+                    output.Add(i);
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherListTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherListTest {
+        [TestMethod]
+        public void PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList () {
+            IList<IList<string>> favoriteCompanies = new List<IList<string>>(){
+                new List<string>(){ "leetcode", "google", "facebook" },
+                new List<string>(){ "google","microsoft" },
+                new List<string>(){ "google","facebook" },
+                new List<string>(){ "google" },
+                new List<string>(){ "amazon" },
+            };
+
+            IList<int> expected = new List<int>() { 0, 1, 4 };
+            Assert.IsTrue(expected.SequenceEqual(P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList.PeopleIndexes(favoriteCompanies)));
+
+            favoriteCompanies = new List<IList<string>>(){
+                new List<string>(){ "leetcode", "google", "facebook" },
+                new List<string>(){ "leetcode","amazon" },
+                new List<string>(){ "facebook","google" }
+            };
+
+            expected = new List<int>() { 0, 1 };
+            Assert.IsTrue(expected.SequenceEqual(P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList.PeopleIndexes(favoriteCompanies)));
+
+            favoriteCompanies = new List<IList<string>>(){
+                new List<string>(){ "leetcode" },
+                new List<string>(){ "google" },
+                new List<string>(){ "facebook" },
+                new List<string>(){ "amazon" }
+            };
+
+            expected = new List<int>() { 0, 1, 2, 3 };
+            Assert.IsTrue(expected.SequenceEqual(P1452PeopleWhoseListOfFavoriteCompaniesIsNotASubsetOfAnotherList.PeopleIndexes(favoriteCompanies)));
+        }
+    }
+}
