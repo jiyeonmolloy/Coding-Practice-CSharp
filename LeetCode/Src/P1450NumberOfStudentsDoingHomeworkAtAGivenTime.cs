7ca539c73f9415534770a6ca37d28d43ec2b5c94
Ji <jjeonie@gmail.com>
2020-05-17 19:07:22 +1200

    LeetCode P1450 Number of Students Doing Homework at a Given Time

---
+++ LeetCode/Src/P1450NumberOfStudentsDoingHomeworkAtAGivenTime.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    class P1450NumberOfStudentsDoingHomeworkAtAGivenTime {
+        /// <summary>
+        /// P1450 Number of Students Doing Homework at a Given Time. href = https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+        /// </summary>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
+        /// <param name="queryTime"></param>
+        /// <returns></returns>
+        public static int BusyStudent (int[] startTime, int[] endTime, int queryTime) {
+            int busyStudent = 0;
+
+            for (int i = 0; i < startTime.Count(); i++) {
+                if (startTime[i] > queryTime) {
+                    continue;
+                } else {
+                    if (endTime[i] >= queryTime) {
+                        busyStudent++;
+                    }
+                }
+            }
+
+            return busyStudent;
+        }
+    }
+}
---
+++ LeetCode/Test/P1450NumberOfStudentsDoingHomeworkAtAGivenTimeTest.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1450NumberOfStudentsDoingHomeworkAtAGivenTimeTest {
+        [TestMethod]
+
+        public void FindNumberOfStudentsDoingHomeworkAtAGivenTime () {
+            int[] startTime = new int[3] { 1, 2, 3 };
+            int[] endTime = new int[3] { 3, 2, 7 };
+            int queryTime = 4;
+
+            Assert.AreEqual(1, P1450NumberOfStudentsDoingHomeworkAtAGivenTime.BusyStudent(startTime, endTime, queryTime));
+
+            startTime = new int[1] { 4 };
+            endTime = new int[1] { 4 };
+            queryTime = 4;
+
+            Assert.AreEqual(1, P1450NumberOfStudentsDoingHomeworkAtAGivenTime.BusyStudent(startTime, endTime, queryTime));
+
+            startTime = new int[9] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            endTime = new int[9] { 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            queryTime = 5;
+
+            Assert.AreEqual(5, P1450NumberOfStudentsDoingHomeworkAtAGivenTime.BusyStudent(startTime, endTime, queryTime));
+        }
+
+        [TestMethod]
+        public void NoStudentsDoingHomeworkAtAGivenTime () {
+            int[] startTime = new int[1] { 4 };
+            int[] endTime = new int[1] { 4 };
+            int queryTime = 5;
+
+            Assert.AreEqual(0, P1450NumberOfStudentsDoingHomeworkAtAGivenTime.BusyStudent(startTime, endTime, queryTime));
+        }
+    }
+}
