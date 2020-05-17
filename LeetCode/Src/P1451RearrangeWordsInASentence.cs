0dfba6e222dbe8ec080709ad92e896e17c01624b
Ji <jjeonie@gmail.com>
2020-05-17 19:11:09 +1200

    LeetCode P1451 Rearrange Words in a Sentence

---
+++ LeetCode/Src/P1451RearrangeWordsInASentence.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    class P1451RearrangeWordsInASentence {
+        /// <summary>
+        /// P1451 Rearrange Words in a Sentence. href = https://leetcode.com/problems/rearrange-words-in-a-sentence/
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string ArrangeWords (string text) {
+            string[] wordArray = text.Split(' ');
+            wordArray = wordArray.OrderBy(x => x.Length).ToArray();
+
+            // combine to form output string
+            string firstWord = wordArray.First();
+            string output = firstWord.First().ToString().ToUpper() + firstWord.Substring(1).ToLower();
+            bool first = true;
+
+            foreach (string word in wordArray) {
+                if (!first) {
+                    output += " " + word.ToLower();
+                } else {
+                    first = false;
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1451RearrangeWordsInASentenceTest.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1451RearrangeWordsInASentenceTest {
+        [TestMethod]
+        public void RearrangeWordsInASentence () {
+            string text = "Leetcode is cool";
+            Assert.AreEqual("Is cool leetcode", P1451RearrangeWordsInASentence.ArrangeWords(text));
+
+            text = "Keep calm and code on";
+            Assert.AreEqual("On and keep calm code", P1451RearrangeWordsInASentence.ArrangeWords(text));
+
+            text = "To be or not to be";
+            Assert.AreEqual("To be or to be not", P1451RearrangeWordsInASentence.ArrangeWords(text));
+        }
+    }
+}
