21709db2174a4e880a565a5710276ac12fd8fb7d
Ji <jjeonie@gmail.com>
2020-05-19 20:40:43 +1200

    LeetCode P438 Find all anagrams in a string

---
+++ LeetCode/Src/P438FindAllAnagramsInAString.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    class P438FindAllAnagramsInAString {
+        /// <summary>
+        /// P438 Find All Anagrams In A String. href = "https://leetcode.com/problems/find-all-anagrams-in-a-string/"
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static IList<int> FindAnagrams (string s, string p) {
+            IList<int> anagramIndexList = new List<int>();
+            const int ALPHABET_MAX_COUNT = 26;
+            int sLen = s.Length;
+            int pLen = p.Length;
+
+            if (pLen > sLen) {
+                return anagramIndexList;
+            }
+
+            int[] pAlphabetCount = new int[ALPHABET_MAX_COUNT];
+            int[] sAlphabetCount = new int[ALPHABET_MAX_COUNT];
+            bool isAnagram;
+            int anagramIndex;
+            foreach (char c in p) {
+                pAlphabetCount[c - 'a']++;
+            }
+
+            for (int i = 0; i < sLen; i++) {
+                sAlphabetCount[s[i] - 'a']++;
+
+                if (i >= pLen - 1) {
+                    isAnagram = true;
+                    for (int j = 0; j < ALPHABET_MAX_COUNT; j++) {
+                        if (sAlphabetCount[j] != pAlphabetCount[j]) {
+                            isAnagram = false;
+                            break;
+                        }
+                    }
+
+                    anagramIndex = i - pLen + 1;
+
+                    if (isAnagram) {
+                        anagramIndexList.Add(anagramIndex);
+                    }
+
+                    sAlphabetCount[s[anagramIndex] - 'a']--;
+                }
+            }
+
+            return anagramIndexList;
+        }
+    }
+}
---
+++ LeetCode/Test/P438FindAllAnagramsInAStringTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P438FindAllAnagramsInAStringTest {
+        [TestMethod]
+        public void FindAllAnagramsInAString () {
+            string s = "cbaebabacd";
+            string p = "abc";
+
+            IList<int> indexList = new List<int> { 0, 6 };
+
+            Assert.IsTrue(indexList.SequenceEqual(P438FindAllAnagramsInAString.FindAnagrams(s, p)));
+
+            s = "abab";
+            p = "ab";
+
+            indexList = new List<int> { 0, 1, 2 };
+
+            Assert.IsTrue(indexList.SequenceEqual(P438FindAllAnagramsInAString.FindAnagrams(s, p)));
+        }
+
+        [TestMethod]
+        public void FindNoAnagramsInAString () {
+            string s = "";
+            string p = "a";
+
+            IList<int> indexList = new List<int>();
+
+            Assert.IsTrue(indexList.SequenceEqual(P438FindAllAnagramsInAString.FindAnagrams(s, p)));
+        }
+    }
+}
