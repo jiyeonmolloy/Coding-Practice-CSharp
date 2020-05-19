1631572077f94d0971473d01aae37c320bb8d27c
Ji <jjeonie@gmail.com>
2020-05-19 20:54:30 +1200

    LeetCode P567 Permutation in string

---
+++ LeetCode/Src/P567PermutationInString.cs
@@ -0,0 +1,45 @@
+﻿
+namespace LeetCode.Src {
+    class P567PermutationInString {
+        public static bool CheckInclusion (string s1, string s2) {
+            const int ALPHABET_MAX_COUNT = 26;
+            int s1Len = s1.Length;
+            int s2Len = s2.Length;
+
+            if (s1Len > s2Len) {
+                return false;
+            }
+
+            int[] s1AlphabetCount = new int[ALPHABET_MAX_COUNT];
+            int[] s2AlphabetCount = new int[ALPHABET_MAX_COUNT];
+            bool isAnagram;
+            int anagramIndex;
+            foreach (char c in s1) {
+                s1AlphabetCount[c - 'a']++;
+            }
+
+            for (int i = 0; i < s2Len; i++) {
+                s2AlphabetCount[s2[i] - 'a']++;
+
+                if (i >= s1Len - 1) {
+                    isAnagram = true;
+                    for (int j = 0; j < ALPHABET_MAX_COUNT; j++) {
+                        if (s1AlphabetCount[j] != s2AlphabetCount[j]) {
+                            isAnagram = false;
+                            break;
+                        }
+                    }
+
+                    if (isAnagram) {
+                        return isAnagram;
+                    }
+
+                    anagramIndex = i - s1Len + 1;
+                    s2AlphabetCount[s2[anagramIndex] - 'a']--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P567PermutationInStringTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P567PermutationInStringTest {
+        [TestMethod]
+        public void PermutationInString () {
+            string s1 = "ab";
+            string s2 = "eidbaooo";
+
+            Assert.IsTrue(P567PermutationInString.CheckInclusion(s1, s2));
+
+            s1 = "ab";
+            s2 = "eidboaoo";
+
+            Assert.IsFalse(P567PermutationInString.CheckInclusion(s1, s2));
+        }
+    }
+}
