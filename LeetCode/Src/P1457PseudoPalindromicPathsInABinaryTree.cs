21500268e3ee04886f636f9333f7027e0f9ad2ff
Ji <jjeonie@gmail.com>
2020-05-24 19:21:45 +1200

    LeetCode Weekly Contest 190 P1457 Pseudo-Palindromic Paths in a Binary Tree

---
+++ LeetCode/Src/P1457PseudoPalindromicPathsInABinaryTree.cs
@@ -0,0 +1,54 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    class P1457PseudoPalindromicPathsInABinaryTree : TreeNode {
+        /// <summary>
+        /// P1457 Pseudo-Palindromic Paths in a Binary Tree. href = https://leetcode.com/problems/pseudo-palindromic-paths-in-a-binary-tree/
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static int PseudoPalindromicPaths (TreeNode root) {
+            int[] stateArray = new int[9];
+            int result = 0;
+            Dfs(root, stateArray, ref result);
+            return result;
+        }
+
+        private static void Dfs (TreeNode root, int[] stateArray, ref int result) {
+            if (root == null) { // End of tree
+                return;
+            }
+
+            stateArray[root.val - 1]++;
+
+
+            if (root.left == null && root.right == null) {
+                if (IsPalindrome(stateArray)) {
+                    result++;
+                }
+            } else {
+                Dfs(root.left, stateArray, ref result);
+                Dfs(root.right, stateArray, ref result);
+            }
+
+            stateArray[root.val - 1]--;
+        }
+
+        private static bool IsPalindrome (int[] stateArray) {
+            // Only one not matching number 
+            // All other numbers must be pairs (even number of appearance)
+            int notMatching = 0;
+            foreach (int state in stateArray) {
+                if (state % 2 == 1) {
+                    notMatching++;
+                }
+            }
+
+            if (notMatching <= 1) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P1457PseudoPalindromicPathsInABinaryTreeTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1457PseudoPalindromicPathsInABinaryTreeTest : TreeNode {
+        [TestMethod]
+        public void PseudoPalindromePathExists () {
+            TreeNode treeNode = new TreeNode(2, new TreeNode(3, new TreeNode(3), new TreeNode(1)), new TreeNode(1, null, new TreeNode(1)));
+            Assert.AreEqual(2, P1457PseudoPalindromicPathsInABinaryTree.PseudoPalindromicPaths(treeNode));
+        }
+
+        [TestMethod]
+        public void PseudoPalindromePathDoesNotExist () {
+            TreeNode treeNode = new TreeNode(9);
+            Assert.AreEqual(1, P1457PseudoPalindromicPathsInABinaryTree.PseudoPalindromicPaths(treeNode));
+
+            treeNode = new TreeNode(2, new TreeNode(1, new TreeNode(1), new TreeNode(3, null, new TreeNode(1))), new TreeNode(1));
+            Assert.AreEqual(1, P1457PseudoPalindromicPathsInABinaryTree.PseudoPalindromicPaths(treeNode));
+        }
+    }
+}
