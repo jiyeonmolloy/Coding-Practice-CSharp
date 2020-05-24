2924e1399dd400e581b65533e2fee1f9ba5a28ad
Ji <jjeonie@gmail.com>
2020-05-24 19:12:35 +1200

    LeetCode Weekly Contest 190 P 1455 Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ LeetCode/Src/P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    class P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence {
+        /// <summary>
+        /// P1455 Check If a Word Occurs As a Prefix of Any Word in a Sentence. href = https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <param name="searchWord"></param>
+        /// <returns></returns>
+        public static int IsPrefixOfWord (String sentence, String searchWord) {
+            string[] wordArray = sentence.Split(' ');
+
+            for (int i = 0; i < wordArray.Length; i++) {
+                if (wordArray[i].StartsWith(searchWord)) {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Test/P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentenceTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentenceTest {
+        [TestMethod]
+        public void CheckIfAWordOccursAsAPrefixOfAnyWordInASentence () {
+            Assert.AreEqual(4, P1455CheckIfAWordOccursAsAPrefixOfAnyWordInASentence.IsPrefixOfWord("i love eating cakeee", "cake"));
+        }
+    }
+}
