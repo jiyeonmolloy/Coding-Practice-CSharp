35917958e5fd935af9afe286569e94b9e0a0775d
Ji <jjeonie@gmail.com>
2020-05-24 19:17:24 +1200

    LeetCode Weekly Contest 190 P1456 Maximum Number of Vowels in a substring of given length

---
+++ LeetCode/Src/P1456MaximumNumberOfVowelsInASubstringOfGivenLength.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LeetCode.Src {
+    class P1456MaximumNumberOfVowelsInASubstringOfGivenLength {
+        /// <summary>
+        /// P1456 Maximum Number of Vowels in a Substring of Given Length. href = https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int MaxVowels (string s, int k) {
+            int count = 0;
+
+            // initialize vowel count
+            for (int i = 0; i < k; i++) {
+                if (IsVowel(s[i])) {
+                    count++;
+                }
+            }
+
+            int maxVowelCount = count;
+            for (int i = k; i < s.Length; i++) {
+                if (IsVowel(s[i - k])) {
+                    count--;
+                }
+
+                if (IsVowel(s[i])) {
+                    count++;
+                }
+
+                if (count > maxVowelCount) {
+                    maxVowelCount = count;
+                }
+            }
+
+            return maxVowelCount;
+        }
+
+        private static bool IsVowel (char c) {
+            return "aeiou".IndexOf(c) >= 0;
+        }
+    }
+}
---
+++ LeetCode/Test/P1456MaximumNumberOfVowelsInASubstringOfGivenLengthTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1456MaximumNumberOfVowelsInASubstringOfGivenLengthTest {
+        [TestMethod]
+        public void MaximumNumberOfVowelsInASubstringOfGivenLength () {
+            Assert.AreEqual(3, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("abciiidef", 3));
+            Assert.AreEqual(2, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("aeiou", 2));
+            Assert.AreEqual(2, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("leetcode", 3));
+            Assert.AreEqual(0, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("rhythms", 4));
+            Assert.AreEqual(1, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("tryhard", 4));
+            Assert.AreEqual(4, P1456MaximumNumberOfVowelsInASubstringOfGivenLength.MaxVowels("weallloveyou", 7));
+        }
+    }
+}
