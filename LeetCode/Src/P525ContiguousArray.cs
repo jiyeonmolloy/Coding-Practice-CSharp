b28d061249554c776cb1d0730f34441e1edec7e1
Ji <jjeonie@gmail.com>
2020-06-02 20:14:50 +1200

    LeetCode P525 Contiguous Array

---
+++ LeetCode/Src/P525ContiguousArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    class P525ContiguousArray {
+        /// <summary>
+        /// P525 Contiguous Array. href = https://leetcode.com/problems/contiguous-array/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int FindMaxLength (int[] nums) {
+            int maxLength = 0;
+            Dictionary<int, int> visited = new Dictionary<int, int>(); // count : index
+            int count = 0;
+
+            visited.Add(count, -1);
+
+            for (int i = 0; i < nums.Length; i++) {
+                count += nums[i] == 0 ? -1 : 1;
+
+                if (visited.ContainsKey(count)) {
+                    maxLength = Math.Max(maxLength, i - visited[count]);
+                } else {
+                    visited.Add(count, i);
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCode/Test/P525ContiguousArrayTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P525ContiguousArrayTest {
+        [TestMethod]
+        public void FindMaxLengthOfContiguousArray () {
+            Assert.AreEqual(2, P525ContiguousArray.FindMaxLength(new int[2] { 0, 1 }));
+            Assert.AreEqual(2, P525ContiguousArray.FindMaxLength(new int[3] { 0, 1, 0 }));
+            Assert.AreEqual(4, P525ContiguousArray.FindMaxLength(new int[4] { 0, 1, 0, 1 }));
+            Assert.AreEqual(2, P525ContiguousArray.FindMaxLength(new int[6] { 0, 1, 1, 1, 0, 1 }));
+        }
+    }
+}
