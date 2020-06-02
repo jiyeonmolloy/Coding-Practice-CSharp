7caa419565888e1d9f193589ec37eb68133a1baa
Ji <jjeonie@gmail.com>
2020-06-02 20:10:44 +1200

    LeetCode P338 Counting Bits

---
+++ LeetCode/Src/P338CountingBits.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LeetCode.Src {
+    class P338CountingBits {
+        /// <summary>
+        /// P338 Counting Bits. href = https://leetcode.com/problems/counting-bits/
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static int[] CountBits (int num) {
+            int[] dp = new int[num + 1];
+            dp[0] = 0;
+            for (int i = 1; i < num + 1; i++) {
+                if (i % 2 != 0) {
+                    dp[i] = dp[i - 1] + 1; // odd
+                } else {
+                    dp[i] = dp[i / 2]; // even
+                }
+            }
+            return dp;
+        }
+    }
+}
---
+++ LeetCode/Test/P338CountingBitsTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P338CountingBitsTest {
+        [TestMethod]
+        public void CountBitsTest () {
+            CollectionAssert.AreEqual(new int[3] { 0, 1, 1 }, P338CountingBits.CountBits(2));
+            CollectionAssert.AreEqual(new int[6] { 0, 1, 1, 2, 1, 2 }, P338CountingBits.CountBits(5));
+        }
+    }
+}
