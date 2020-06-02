927465fea6e4eca930b406770d939c61a2622086
Ji <jjeonie@gmail.com>
2020-06-02 20:05:57 +1200

    LeetCode P226 Invert Binary Tree

---
+++ LeetCode/Src/P226InvertBinaryTree.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    class P226InvertBinaryTree : TreeNode{
+        /// <summary>
+        /// P226 Invert Binary Tree. href = https://leetcode.com/problems/invert-binary-tree/
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static TreeNode InvertTree (TreeNode root) {
+            if (root == null) {
+                return root;
+            }
+
+            // Invert left and right values
+            TreeNode originalLeftValue = root.left;
+            root.left = root.right;
+            root.right = originalLeftValue;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}
---
+++ LeetCode/Test/P226InvertBinaryTreeTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P226InvertBinaryTreeTest : TreeNode {
+        [TestMethod]
+        public void InvertBinaryTreeTest () {
+            TreeNode invertedBindaryTree = P226InvertBinaryTree.InvertTree(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9))));
+            invertedBindaryTree.Should().Equals(new TreeNode(4, new TreeNode(7, new TreeNode(9), new TreeNode(6)), new TreeNode(2, new TreeNode(3), new TreeNode(1))));
+        }
+    }
+}
