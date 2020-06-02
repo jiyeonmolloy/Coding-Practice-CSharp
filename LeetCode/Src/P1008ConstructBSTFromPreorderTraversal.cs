78c689dbcc287396059184380681bf2dcaeaf217
Ji <jjeonie@gmail.com>
2020-06-02 20:18:30 +1200

    LeetCode P1008 Construct BST From Preorder Traversal

---
+++ LeetCode/Src/P1008ConstructBSTFromPreorderTraversal.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    class P1008ConstructBSTFromPreorderTraversal : TreeNode {
+        /// <summary>
+        /// P1008 Construct Binary Search Tree from Preorder Traversal. href = https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+        /// </summary>
+        /// <param name="preorder"></param>
+        /// <returns></returns>
+        public static TreeNode BstFromPreorder (int[] preorder) {
+            TreeNode root = new TreeNode(preorder[0]);
+
+            for (int i = 1; i < preorder.Length; i++) {
+                InsertNode(root, preorder[i]);
+            }
+
+            return root;
+        }
+
+        private static void InsertNode (TreeNode root, int value) {
+            TreeNode current = root;
+            TreeNode parent = null;
+
+            while (current != null) {
+                parent = current;
+                current = current.val > value ? current.left : current.right;
+            }
+
+            if (parent.val > value) {
+                parent.left = new TreeNode(value);
+            } else {
+                parent.right = new TreeNode(value);
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/P1008ConstructBSTFromPreorderTraversalTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1008ConstructBSTFromPreorderTraversalTest : TreeNode {
+        [TestMethod]
+        public void ConstructBstFromPreorder () {
+            TreeNode treeNode = new TreeNode(8, new TreeNode(5, new TreeNode(1), new TreeNode(7)), new TreeNode(10, null, new TreeNode(12)));
+            int[] preorderArray = new int[6] { 8, 5, 1, 7, 10, 12 };
+            TreeNode preorderTreeNode = P1008ConstructBSTFromPreorderTraversal.BstFromPreorder(preorderArray);
+            preorderTreeNode.Should().Equals(treeNode);
+        }
+    }
+}
