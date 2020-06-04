988e360807f1e88d53eb070cec48915f95540534
Ji <jjeonie@gmail.com>
2020-06-04 19:55:16 +1200

    LeetCode P344 Reverse String

---
+++ LeetCode/Src/P344ReverseString.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P344 Reverse String. href = https://leetcode.com/problems/reverse-string/
+    /// </summary>
+    class P344ReverseString {
+        public static char[] ReverseString (char[] s) {
+            char temp;
+            int length = s.Length;
+            int endIndex;
+            for (int i = 0; i < length / 2; i++) {
+                endIndex = length - 1 - i;
+
+                if (s[i].Equals(s[endIndex])) {
+                    continue;
+                }
+
+                temp = s[i];
+                s[i] = s[endIndex];
+                s[endIndex] = temp;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ LeetCode/Test/P344ReverseStringTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P344ReverseStringTest {
+        [TestMethod]
+        public void ReverseString () {
+            CollectionAssert.AreEqual(new char[5] { 'o', 'l', 'l', 'e', 'h' }, P344ReverseString.ReverseString(new char[5] { 'h', 'e', 'l', 'l', 'o' }));
+            CollectionAssert.AreEqual(new char[4] { 'y', 'b', 'o', 'T' }, P344ReverseString.ReverseString(new char[4] { 'T', 'o', 'b', 'y' }));
+        }
+    }
+}
