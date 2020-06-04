dafbaf5c1a2036baf846258b7163c135c9fa5dc9
Ji <jjeonie@gmail.com>
2020-06-04 21:39:17 +1200

    LeetCode P1 Two Sum

---
+++ LeetCode/Src/P1TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    class P1TwoSum {
+        /// <summary>
+        /// P1 Two Sum. href = https://leetcode.com/problems/two-sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum (int[] nums, int target) {
+            Dictionary<int, int> complementDict = new Dictionary<int, int>(); // Key: Complement, Value: Index
+            int complement;
+            for (int i = 0; i < nums.Length; i++) {
+                complement = target - nums[i];
+                if (complementDict.ContainsKey(complement)) {
+                    return new int[] { complementDict[complement], i };
+                } else {
+                    complementDict[nums[i]] = i;
+                }
+            }
+
+            throw new ArgumentException("Parameter does not provide a two sum solution", "nums");
+        }
+    }
+}
---
+++ LeetCode/Test/P1TwoSumTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1TwoSumTest {
+        [TestMethod]
+        public void TwoSumTest () {
+            CollectionAssert.AreEqual(new int[2] { 0, 1 }, P1TwoSum.TwoSum(new int[4] { 2, 7, 11, 15 }, 9));
+        }
+    }
+}
