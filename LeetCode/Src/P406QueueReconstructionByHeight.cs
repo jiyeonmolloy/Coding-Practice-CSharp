b617ffa40f83de4e62314ebe060d8a172bec0c7f
Ji <jjeonie@gmail.com>
2020-06-07 17:51:27 +1200

    LeetCode P406 Queue Reconstruction By Height

---
+++ LeetCode/Src/P406QueueReconstructionByHeight.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    class P406QueueReconstructionByHeight {
+        /// <summary>
+        /// P406 Queue Reconstruction By Height. href = https://leetcode.com/problems/queue-reconstruction-by-height/
+        /// </summary>
+        /// <param name="people"></param>
+        /// <returns></returns>
+        public static int[][] ReconstructQueue (int[][] people) {
+            const int HEIGHT = 0;
+            const int PEOPLE_INFRONT = 1;
+            List<int[]> reconstruct = new List<int[]>();
+            IOrderedEnumerable<int[]> sorted = people.ToList().OrderByDescending(x => x[HEIGHT]).ThenBy(x => x[PEOPLE_INFRONT]);
+
+            foreach (int[] person in sorted) {
+                reconstruct.Insert(person[PEOPLE_INFRONT], person);
+            }
+
+            return reconstruct.ToArray();
+        }
+    }
+}
---
+++ LeetCode/Test/P406QueueReconstructionByHeightTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P406QueueReconstructionByHeightTest {
+        [TestMethod]
+        public void ReconstructQueueTest () {
+            int[][] input = new int[6][] { new int[2] { 7, 0 }, new int[2] { 4, 4 }, new int[2] { 7, 1 }, new int[2] { 5, 0 }, new int[2] { 6, 1 }, new int[2] { 5, 2 } };
+            int[][] expected = new int[6][] { new int[2] { 5, 0 }, new int[2] { 7, 0 }, new int[2] { 5, 2 }, new int[2] { 6, 1 }, new int[2] { 4, 4 }, new int[2] { 7, 1 } };
+
+            P406QueueReconstructionByHeight.ReconstructQueue(input).Should().Equals(expected);
+        }
+    }
+}
