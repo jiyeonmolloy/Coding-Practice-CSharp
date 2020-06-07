d73c7a5ee8566df4da0241b9472be75bf2c8ae38
Ji <jjeonie@gmail.com>
2020-06-07 17:13:56 +1200

    Weekly Contest 192 P1471 The K Strongest Values In An Array

---
+++ LeetCode/Src/P1471TheKStrongestValuesInAnArray.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1471 The K Strongest Values In An Array. href = https://leetcode.com/problems/the-k-strongest-values-in-an-array/
+    /// </summary>
+    class P1471TheKStrongestValuesInAnArray {
+        public static int[] GetStrongest (int[] arr, int k) {
+            int size = arr.Length;
+            // Sort array in asc order
+            List<int> list = arr.ToList();
+            list.Sort();
+
+            // Calculate median
+            int position = (size - 1) / 2;
+            int median = list[position];
+
+            // Find strength value for each element
+            int[] strongest = new int[k];
+            int leftIndex = 0;
+            int rightIndex = size - 1;
+            int leftValue, rightValue;
+
+            int count = 0;
+            while (count != k) {
+                leftValue = list[leftIndex];
+                rightValue = list[rightIndex];
+
+                if (LeftValueIsStronger(leftValue, rightValue, median)) {
+                    strongest[count] = leftValue;
+                    leftIndex++;
+                } else {
+                    strongest[count] = rightValue;
+                    rightIndex--;
+                }
+
+                count++;
+            }
+
+            // Return strongest elements
+            return strongest;
+        }
+
+        private static bool LeftValueIsStronger (int leftValue, int rightValue, int median) {
+            int value1Strength = (leftValue - median) * -1; // left of median, so always negative
+            int value2Strength = rightValue - median; // right of median, so always positive
+
+            if (value1Strength == value2Strength) {
+                return false;
+            }
+
+            return value1Strength > value2Strength;
+        }
+    }
+}
---
+++ LeetCode/Test/P1471TheKStrongestValuesInAnArrayTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1471TheKStrongestValuesInAnArrayTest {
+        [TestMethod]
+        public void GetStrongestElementsTest () {
+            CollectionAssert.AreEqual(new int[5] { 11, 8, 6, 6, 7 }, P1471TheKStrongestValuesInAnArray.GetStrongest(new int[6] { 6, 7, 11, 7, 6, 8 }, 5));
+            CollectionAssert.AreEqual(new int[2] { 5, 1 }, P1471TheKStrongestValuesInAnArray.GetStrongest(new int[5] { 1, 2, 3, 4, 5 }, 2));
+            CollectionAssert.AreEqual(new int[2] { 5, 5 }, P1471TheKStrongestValuesInAnArray.GetStrongest(new int[5] { 1, 1, 3, 5, 5 }, 2));
+        }
+    }
+}
