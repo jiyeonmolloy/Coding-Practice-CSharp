e9e5550887610c43fabd9637b9d7e9ace9922188
Ji <jjeonie@gmail.com>
2020-06-07 17:16:43 +1200

    LeetCode Weekly Contest 192 P1470 Shuffle The Array

---
+++ LeetCode/Src/P1470ShuffleTheArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1470 Shuffle The Array. href = https://leetcode.com/problems/shuffle-the-array/
+    /// </summary>
+    class P1470ShuffleTheArray {
+        public static int[] Shuffle (int[] nums, int n) {
+            int move;
+            int count = 1;
+            int value;
+            List<int> list = nums.ToList();
+            for (int i = n; i < nums.Length; i++) {
+                move = n - count;
+                value = list[i];
+                list.RemoveAt(i);
+                list.Insert(i - move, value);
+                count++;
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ LeetCode/Test/P1470ShuffleTheArrayTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1470ShuffleTheArrayTest {
+        [TestMethod]
+        public void ShuffleTest () {
+            CollectionAssert.AreEqual(new int[6] { 2, 3, 5, 4, 1, 7 }, P1470ShuffleTheArray.Shuffle(new int[6] { 2, 5, 1, 3, 4, 7 }, 3));
+            CollectionAssert.AreEqual(new int[8] { 1, 4, 2, 3, 3, 2, 4, 1 }, P1470ShuffleTheArray.Shuffle(new int[8] { 1, 2, 3, 4, 4, 3, 2, 1 }, 4));
+        }
+    }
+}
