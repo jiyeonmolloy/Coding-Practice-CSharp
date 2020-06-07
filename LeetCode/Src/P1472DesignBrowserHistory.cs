2a2b865cfc53c43b263c88083286107a676435b6
Ji <jjeonie@gmail.com>
2020-06-07 17:09:55 +1200

    LeetCode Weekly Contest 192 P1472 Design Browser History

---
+++ LeetCode/Src/P1472DesignBrowserHistory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1472 Design Browser History. href = https://leetcode.com/problems/design-browser-history/
+    /// </summary>
+    class P1472DesignBrowserHistory {
+        private int index;
+        private List<string> browserList = new List<string>();
+
+        public P1472DesignBrowserHistory (string homepage) {
+            browserList.Clear();
+            browserList.Add(homepage);
+            index = 0;
+        }
+
+        public void Visit (string url) {
+            int size = browserList.Count;
+
+            if (size > index + 1) {
+                // Remove forward history
+                browserList.RemoveRange(index + 1, browserList.Count - index - 1);
+            }
+
+            browserList.Add(url);
+            index++;
+        }
+
+        public string Back (int steps) {
+            index -= steps;
+
+            if (index < 0) {
+                index = 0;
+            }
+
+            return browserList[index];
+        }
+
+        public string Forward (int steps) {
+            int size = browserList.Count;
+            index += steps;
+
+            if (index >= browserList.Count) {
+                index = size - 1;
+            }
+
+            return browserList[index];
+        }
+    }
+}
---
+++ LeetCode/Test/P1472DesignBrowserHistoryTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1472DesignBrowserHistoryTest {
+        [TestMethod]
+        public void BrowserHistoryTest () {
+            string homepage = "leetcode.com";
+            P1472DesignBrowserHistory browserHistory = new P1472DesignBrowserHistory(homepage);
+            browserHistory.Visit("google.com");
+            browserHistory.Visit("facebook.com");
+            browserHistory.Visit("youtube.com");
+            Assert.AreEqual("facebook.com", browserHistory.Back(1));
+            Assert.AreEqual("google.com", browserHistory.Back(1));
+            Assert.AreEqual("facebook.com", browserHistory.Forward(1));
+            browserHistory.Visit("linkedin.com");
+            Assert.AreEqual("linkedin.com", browserHistory.Forward(2));
+            Assert.AreEqual("google.com", browserHistory.Back(2));
+            Assert.AreEqual("leetcode.com", browserHistory.Back(7));
+        }
+    }
+}
