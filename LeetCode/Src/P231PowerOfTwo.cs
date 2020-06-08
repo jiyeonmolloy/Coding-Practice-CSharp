c03266182e5c4f3396b23354f57e93a3fcc303fc
Ji <jjeonie@gmail.com>
2020-06-08 21:55:59 +1200

    LeetCode P231 Power Of Two

---
+++ LeetCode/Src/P231PowerOfTwo.cs
@@ -0,0 +1,16 @@
+﻿
+namespace LeetCode.Src {
+    class P231PowerOfTwo {
+        /// <summary>
+        /// P231 Power Of Two. href = https://leetcode.com/problems/power-of-two/
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsPowerOfTwo (int n) {
+            if (n <= 0) {
+                return false;
+            }
+            return (n & (n - 1)) == 0;
+        }
+    }
+}
---
+++ LeetCode/Test/P231PowerOfTwoTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P231PowerOfTwoTest {
+        [TestMethod]
+        public void IsPowerOfTwoTest () {
+            Assert.IsTrue(P231PowerOfTwo.IsPowerOfTwo(1));
+            Assert.IsTrue(P231PowerOfTwo.IsPowerOfTwo(16));
+            Assert.IsFalse(P231PowerOfTwo.IsPowerOfTwo(218));
+            Assert.IsFalse(P231PowerOfTwo.IsPowerOfTwo(0));
+        }
+    }
+}
