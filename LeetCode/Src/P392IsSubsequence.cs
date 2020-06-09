d8ee78dab7a98fc5e19326e1ea24d062d9662ffb
Ji <jjeonie@gmail.com>
2020-06-09 19:57:03 +1200

    LeetCode P392 Is Subsequence

---
+++ LeetCode/Src/P392IsSubsequence.cs
@@ -0,0 +1,33 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P392 Is Subsequence
+    ///  href = https://leetcode.com/problems/is-subsequence/
+    /// </summary>
+    class P392IsSubsequence {
+        public static bool IsSubsequence (string s, string t) {
+            int sLength = s.Length;
+            int tLength = t.Length;
+
+            if (sLength > tLength) {
+                return false;
+            }
+
+            if (sLength == 0) {
+                return true;
+            }
+
+            int sIndex = 0;
+            for (int i = 0; i < tLength; i++) {
+                if (s[sIndex] == t[i]) {
+                    sIndex++;
+                    if (sIndex == sLength) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P392IsSubsequenceTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P392IsSubsequenceTest {
+        [TestMethod]
+        public void IsSubsequenceTest () {
+            Assert.IsTrue(P392IsSubsequence.IsSubsequence("abc", "ahbgdc"));
+            Assert.IsFalse(P392IsSubsequence.IsSubsequence("axc", "ahbgdc"));
+            Assert.IsTrue(P392IsSubsequence.IsSubsequence(String.Empty, "ahbgdc"));
+        }
+    }
+}
