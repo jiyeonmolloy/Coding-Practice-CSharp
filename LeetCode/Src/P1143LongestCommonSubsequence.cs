cc662f6ab1c4dce8dc2c16254ce064fdc3e1d091
Ji <jjeonie@gmail.com>
2020-06-09 20:55:22 +1200

    LeetCode P1143LongestCommonSubsequence

---
+++ LeetCode/Src/P1143LongestCommonSubsequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1143 Longest Common Subsequence
+    ///  href = https://leetcode.com/problems/longest-common-subsequence/
+    /// </summary>
+    class P1143LongestCommonSubsequence {
+        public static int LongestCommonSubsequence (string text1, string text2) {
+            int text1Length = text1.Length;
+            int text2Length = text2.Length;
+
+            int[,] dp = new int[text1Length + 1, text2Length + 1];
+
+            for (int i = 1; i <= text1Length; i++) {
+                for (int j = 1; j <= text2Length; j++) {
+                    if (text1[i - 1].Equals(text2[j - 1])) {
+                        dp[i, j] = Math.Max(dp[i, j], dp[i - 1, j - 1] + 1);
+                    } else {
+                        dp[i, j] = Math.Max(dp[i, j - 1], dp[i - 1, j]);
+                    }
+                }
+            }
+
+            return dp[text1Length, text2Length];
+        }
+    }
+}
---
+++ LeetCode/Test/P1143LongestCommonSubsequenceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1143LongestCommonSubsequenceTest {
+        [TestMethod]
+        public void LongestCommonSubsequenceTest () {
+            Assert.AreEqual(3, P1143LongestCommonSubsequence.LongestCommonSubsequence("abcde", "ace"));
+            Assert.AreEqual(3, P1143LongestCommonSubsequence.LongestCommonSubsequence("abc", "abc"));
+            Assert.AreEqual(0, P1143LongestCommonSubsequence.LongestCommonSubsequence("abc", "def"));
+        }
+    }
+}
