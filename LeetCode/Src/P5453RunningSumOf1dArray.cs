4225e713c84a8aee9278b2dbc19e12497af7116e
Ji <jjeonie@gmail.com>
2020-06-14 22:06:06 +1200

    LeetCode Weekly Contest 193 P5453 Running Sum Of 1d Array

---
+++ LeetCode/Src/P5453RunningSumOf1dArray.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P5453 Running Sum Of 1d Array
+    ///  href = https://leetcode.com/problems/running-sum-of-1d-array/
+    /// </summary>
+    class P5453RunningSumOf1dArray {
+        public static int[] RunningSum (int[] nums) {
+            for (int i = 1; i < nums.Length; i++) {
+                nums[i] += nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ LeetCode/Test/P5453RunningSumOf1dArrayTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P5453RunningSumOf1dArrayTest {
+        [TestMethod]
+        public void RunningSumTest () {
+            CollectionAssert.AreEqual(new int[4] { 1, 3, 6, 10 }, P5453RunningSumOf1dArray.RunningSum(new int[4] { 1, 2, 3, 4 }));
+            CollectionAssert.AreEqual(new int[5] { 1, 2, 3, 4, 5 }, P5453RunningSumOf1dArray.RunningSum(new int[5] { 1, 1, 1, 1, 1 }));
+            CollectionAssert.AreEqual(new int[5] { 3, 4, 6, 16, 17 }, P5453RunningSumOf1dArray.RunningSum(new int[5] { 3, 1, 2, 10, 1 }));
+        }
+    }
+}
