87b51b33fb2d08ff2ab22bf4107ff2ff5a057afa
Ji <jjeonie@gmail.com>
2020-06-14 22:01:52 +1200

    P35 Search Insert Position

---
+++ LeetCode/Src/P35SearchInsertPosition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P35 Search Insert Position
+    ///  href = https://leetcode.com/problems/search-insert-position/
+    /// </summary>
+    class P35SearchInsertPosition {
+        public static int SearchInsert (int[] nums, int target) {
+            int left = 0;
+            int right = nums.Length - 1;
+            int mid;
+
+            while (left <= right) {
+                mid = left + ((right - left) / 2);
+
+                if (nums[mid] == target) {
+                    return mid;
+                }
+
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
---
+++ LeetCode/Test/P35SearchInsertPositionTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P35SearchInsertPositionTest {
+        [TestMethod]
+        public void SearchInsertTest () {
+            Assert.AreEqual(2, P35SearchInsertPosition.SearchInsert(new int[4] { 1, 3, 5, 6 }, 5));
+            Assert.AreEqual(1, P35SearchInsertPosition.SearchInsert(new int[4] { 1, 3, 5, 6 }, 2));
+            Assert.AreEqual(4, P35SearchInsertPosition.SearchInsert(new int[4] { 1, 3, 5, 6 }, 7));
+            Assert.AreEqual(2, P35SearchInsertPosition.SearchInsert(new int[4] { 1, 3, 5, 6 }, 4));
+            Assert.AreEqual(0, P35SearchInsertPosition.SearchInsert(new int[4] { 1, 3, 5, 6 }, 0));
+        }
+    }
+}
