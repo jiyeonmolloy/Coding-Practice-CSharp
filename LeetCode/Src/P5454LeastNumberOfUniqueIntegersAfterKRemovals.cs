f9fcc3f33ca7933107f3571422e989559ee9b7a9
Ji <jjeonie@gmail.com>
2020-06-14 22:09:46 +1200

    LeetCode Weekly Contest 193 P5454 Least Number Of Unique Integers After K Removals

---
+++ LeetCode/Src/P5454LeastNumberOfUniqueIntegersAfterKRemovals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P5454 Least Number Of Unique Integers After K Removals
+    ///  href = https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+    /// </summary>
+    class P5454LeastNumberOfUniqueIntegersAfterKRemovals {
+        public static int FindLeastNumOfUniqueInts (int[] arr, int k) {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            int total = arr.Length;
+            int target;
+            for (int i = 0; i < total; i++) {
+                target = arr[i];
+                if (dict.ContainsKey(target)) {
+                    dict[target]++;
+                } else {
+                    dict.Add(target, 1);
+                }
+            }
+
+            List<KeyValuePair<int, int>> orderedList = dict.OrderBy(x => x.Value).ToList();
+
+            int lastValue;
+            while (k > 0) {
+                lastValue = orderedList[0].Value;
+
+                k -= lastValue;
+
+                if (k >= 0) {
+                    orderedList.RemoveAt(0);
+                }
+            }
+
+            return orderedList.Count;
+        }
+    }
+}
---
+++ LeetCode/Test/P5454LeastNumberOfUniqueIntegersAfterKRemovalsTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P5454LeastNumberOfUniqueIntegersAfterKRemovalsTest {
+        [TestMethod]
+        public void FindLeastNumOfUniqueIntsTest () {
+            Assert.AreEqual(1, P5454LeastNumberOfUniqueIntegersAfterKRemovals.FindLeastNumOfUniqueInts(new int[3] { 5, 5, 4 }, 1));
+            Assert.AreEqual(2, P5454LeastNumberOfUniqueIntegersAfterKRemovals.FindLeastNumOfUniqueInts(new int[7] { 4, 3, 1, 1, 3, 3, 2 }, 3));
+        }
+    }
+}
