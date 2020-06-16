4e3d0d809a5900b20cd159c8e72614b491d109f3
Ji <jjeonie@gmail.com>
2020-06-16 22:37:58 +1200

    LeetCode P700 Search In A Binary Search Tree

---
+++ LeetCode/Src/P700SearchInABinarySearchTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P700SearchInABinarySearchTree
+    ///  href = https://leetcode.com/problems/search-in-a-binary-search-tree/
+    /// </summary>
+    class P700SearchInABinarySearchTree : TreeNode {
+        public static TreeNode SearchBST (TreeNode root, int val) {
+            while (root != null) {
+                if (root.val == val) {
+                    return root;
+                }
+
+                if (root.val > val) {
+                    root = root.left;
+                } else if (root.val < val) {
+                    root = root.right;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ LeetCode/Test/P700SearchInABinarySearchTreeTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P700SearchInABinarySearchTreeTest {
+        [TestMethod]
+        public void SearchBSTTest () {
+            TreeNode BST = P700SearchInABinarySearchTree.SearchBST(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7)), 2);
+            BST.Should().Equals(new TreeNode(2, new TreeNode(1), new TreeNode(3)));
+            BST = P700SearchInABinarySearchTree.SearchBST(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9))), 2);
+            BST.Should().Equals(null);
+        }
+    }
+}
