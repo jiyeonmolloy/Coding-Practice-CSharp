67333e8bc893006c76650a5e4db6fe8d39e10314
Ji <jjeonie@gmail.com>
2020-06-16 21:42:04 +1200

    LeetCode P468 Validate IP Address

---
+++ LeetCode/Src/P468ValidateIPAddress.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P468 Validate IP Address
+    ///  href = https://leetcode.com/problems/validate-ip-address/
+    /// </summary>
+    class P468ValidateIPAddress {
+        public static string ValidIPAddress (string IP) {
+            if (IP.Contains('.')) {
+                return IsIPv4(IP) ? "IPv4" : "Neither";
+            }
+
+            if (IP.Contains(':')) {
+                return IsIPv6(IP) ? "IPv6" : "Neither";
+            }
+
+            return "Neither";
+        }
+
+        private static bool IsIPv4 (string IP) {
+            // valid IPv4?
+            // four decimal numbers, 0 - 255
+            // separated by dots ;)
+            // leading zeros are invalid
+
+            char delimiter = '.';
+
+            if (IP[0] == delimiter || IP[IP.Length - 1] == delimiter) {
+                return false;
+            }
+
+            string[] addresses = IP.Split(delimiter);
+
+            if (addresses.Length != 4) {
+                return false;
+            }
+
+            int addressValue;
+            foreach (string address in addresses) {
+                if (int.TryParse(address, out addressValue)) {
+                    if (addressValue > 255) {
+                        return false;
+                    }
+                    if ((addressValue < 10 && address.Length > 1) ||
+                               (addressValue < 100 && address.Length > 2)) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool IsIPv6 (string IP) {
+            // eight groups of four hex digits
+            // each group representing 16 bits
+            // separated by colons! ;)
+            // leading zeroes are all good
+            // consecutive colons are invalid
+
+            char delimiter = ':';
+
+            if (IP[0] == delimiter || IP[IP.Length - 1] == delimiter) {
+                return false;
+            }
+
+            string[] addresses = IP.Split(delimiter);
+
+            if (addresses.Length != 8) {
+                return false;
+            }
+
+            bool isDigit, isHexLetter;
+            foreach (string address in addresses) {
+                if (address == String.Empty || address.Length > 4) {
+                    return false;
+                }
+
+                foreach (char c in address.ToLower()) {
+                    isDigit = c >= '0' && c <= '9';
+                    isHexLetter = c >= 'a' && c <= 'f';
+                    if (!(isDigit || isHexLetter)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Test/P468ValidateIPAddressTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P468ValidateIPAddressTest {
+        [TestMethod]
+        public void ValidIPAddressTest () {
+            Assert.AreEqual("IPv4", P468ValidateIPAddress.ValidIPAddress("172.16.254.1"));
+            Assert.AreEqual("Neither", P468ValidateIPAddress.ValidIPAddress("172.16.254.01"));
+            Assert.AreEqual("IPv6", P468ValidateIPAddress.ValidIPAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"));
+            Assert.AreEqual("Neither", P468ValidateIPAddress.ValidIPAddress("2001:0db8:85a3::8A2E:0370:7334"));
+            Assert.AreEqual("Neither", P468ValidateIPAddress.ValidIPAddress("256.256.256.256"));
+            Assert.AreEqual("Neither", P468ValidateIPAddress.ValidIPAddress("12.12.12"));
+        }
+    }
+}
