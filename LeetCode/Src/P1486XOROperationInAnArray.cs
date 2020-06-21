3d850b47bcf3834712cc1e44aadc98a82378ed16
Ji <jjeonie@gmail.com>
2020-06-21 20:37:06 +1200

    LeetCode Weekly Contest 195 P1486 XOR Operation in an Array

---
+++ LeetCode/Src/P1486XOROperationInAnArray.cs
@@ -0,0 +1,18 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1486 XOR Operation in an Array
+    ///  href = https://leetcode.com/problems/xor-operation-in-an-array/
+    /// </summary>
+    class P1486XOROperationInAnArray {
+        public static int XorOperation (int n, int start) {
+            int output = 0;
+
+            for (int i = 0; i < n; i++) {
+                output ^= start + 2 * i;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1486XOROperationInAnArrayTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1486XOROperationInAnArrayTest {
+        [TestMethod]
+        public void XorOperationTest () {
+            Assert.AreEqual(8, P1486XOROperationInAnArray.XorOperation(5, 0));
+            Assert.AreEqual(8, P1486XOROperationInAnArray.XorOperation(4, 3));
+            Assert.AreEqual(7, P1486XOROperationInAnArray.XorOperation(1, 7));
+            Assert.AreEqual(2, P1486XOROperationInAnArray.XorOperation(10, 5));
+        }
+    }
+}
