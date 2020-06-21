c9f12451530f05425aab239e7ac088c299454e5f
Ji <jjeonie@gmail.com>
2020-06-21 20:43:42 +1200

    LeetCode Weekly Contest 194 P1487 Making File Names Unique

---
+++ LeetCode/Src/P1487MakingFileNamesUnique.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1487 Making File Names Unique
+    ///  href = https://leetcode.com/problems/making-file-names-unique/
+    /// </summary>
+    class P1487MakingFileNamesUnique {
+        public static string[] GetFolderNames (string[] names) {
+            Dictionary<string, int> nameDict = new Dictionary<string, int>();
+            List<string> output = new List<String>();
+            string newName = String.Empty;
+            int freq;
+
+            foreach (string name in names) {
+                if (nameDict.ContainsKey(name)) {
+                    freq = nameDict[name];
+                    newName = name + "(" + freq + ")"; // can only happen once
+
+                    while (nameDict.ContainsKey(newName)) {
+                        freq++;
+                        newName = name + "(" + freq + ")"; // keep same name
+                    }
+
+                    output.Add(newName);
+                    nameDict[name]++;
+                    nameDict.Add(newName, 1);
+                } else {
+                    output.Add(name);
+                    nameDict.Add(name, 1);
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
---
+++ LeetCode/Test/P1487MakingFileNamesUniqueTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1487MakingFileNamesUniqueTest {
+        [TestMethod]
+        public void GetFolderNamesTest () {
+            CollectionAssert.AreEqual(new string[4] { "pes", "fifa", "gta", "pes(2019)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[4] { "pes", "fifa", "gta", "pes(2019)" }));
+            CollectionAssert.AreEqual(new string[4] { "gta", "gta(1)", "gta(2)", "avalon" }, P1487MakingFileNamesUnique.GetFolderNames(new string[4] { "gta", "gta(1)", "gta", "avalon" }));
+            CollectionAssert.AreEqual(new string[3] { "Will", "Will(4)", "Will(1)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[3] { "Will", "Will(4)", "Will" }));
+            CollectionAssert.AreEqual(new string[5] { "onepiece", "onepiece(1)", "onepiece(2)", "onepiece(3)", "onepiece(4)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[5] { "onepiece", "onepiece(1)", "onepiece(2)", "onepiece(3)", "onepiece" }));
+            CollectionAssert.AreEqual(new string[4] { "wano", "wano(1)", "wano(2)", "wano(3)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[4] { "wano", "wano", "wano", "wano" }));
+            CollectionAssert.AreEqual(new string[4] { "kaido", "kaido(1)", "kaido(2)", "kaido(1)(1)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[4] { "kaido", "kaido(1)", "kaido", "kaido(1)" }));
+            CollectionAssert.AreEqual(new string[5] { "kaido", "kaido(1)", "kaido(2)", "kaido(1)(1)", "kaido(2)(1)" }, P1487MakingFileNamesUnique.GetFolderNames(new string[5] { "kaido", "kaido(1)", "kaido", "kaido(1)", "kaido(2)" }));
+        }
+    }
+}
