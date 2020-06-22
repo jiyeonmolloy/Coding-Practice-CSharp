04c0dc0660894f6d5d04be9d26ff3551b1c2fa63
Ji <jjeonie@gmail.com>
2020-06-22 19:41:22 +1200

    LeetCode P137 Single Number II

---
+++ LeetCode/Src/P137SingleNumberII.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P137 Single Number II
+    ///  href = https://leetcode.com/problems/single-number-ii/
+    /// </summary>
+    class P137SingleNumberII {
+        public static int SingleNumber (int[] nums) {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int target;
+
+            for (int i = 0; i < nums.Length; i++) {
+                target = nums[i];
+                if (dict.ContainsKey(target)) {
+                    dict[target]++;
+                } else {
+                    dict.Add(target, 1);
+                }
+            }
+
+            return dict.First(x => x.Value == 1).Key;
+        }
+    }
+}
---
+++ LeetCode/Test/P137SingleNumberIITest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P137SingleNumberIITest {
+        [TestMethod]
+        public void SingleNumberTest () {
+            Assert.AreEqual(3, P137SingleNumberII.SingleNumber(new int[4] { 2, 2, 3, 2 }));
+            Assert.AreEqual(99, P137SingleNumberII.SingleNumber(new int[7] { 0, 1, 0, 1, 0, 1, 99 }));
+        }
+    }
+}
