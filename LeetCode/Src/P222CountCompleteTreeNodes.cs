39a0e2d35adcd936d5243aa73bf7bb649a2180ff
Ji <jjeonie@gmail.com>
2020-06-23 20:45:57 +1200

    LeetCode P222 Count Complete Tree Nodes

---
+++ LeetCode/Src/P222CountCompleteTreeNodes.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P222 Count Complete Tree Nodes
+    /// href = https://leetcode.com/problems/count-complete-tree-nodes/
+    /// </summary>
+    class P222CountCompleteTreeNodes :  TreeNode {
+        public static int CountNodes (TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int leftCount = CountNodes(root.left);
+            int rightCount = CountNodes(root.right);
+
+            return leftCount + rightCount + 1;
+        }
+    }
+}
---
+++ LeetCode/Test/P222CountCompleteTreeNodesTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P222CountCompleteTreeNodesTest : TreeNode {
+        [TestMethod]
+        public void CountNodesTest () {
+            Assert.AreEqual(6, P222CountCompleteTreeNodes.CountNodes(
+                new TreeNode(1,
+                    new TreeNode(2,
+                        new TreeNode(4), new TreeNode(5)),
+                    new TreeNode(3,
+                        new TreeNode(6)))));
+        }
+    }
+}
