7ef2cea09428313a9b02ceb9409316bd682c052b
Ji <jjeonie@gmail.com>
2020-06-27 19:37:41 +1200

    LeetCode P129 Sum Root To Leaf Numbers

---
+++ LeetCode/Src/P129SumRootToLeafNumbers.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P129 Sum Root to Leaf Numbers
+    ///  href = https://leetcode.com/problems/sum-root-to-leaf-numbers/
+    /// </summary>
+    class P129SumRootToLeafNumbers : TreeNode {
+        public static int SumNumbers (TreeNode root) {
+            return Dfs(root, 0); ;
+        }
+
+        private static int Dfs (TreeNode root, int val) {
+            if (root == null) {
+                return 0;
+            }
+
+            int sum = val * 10 + root.val;
+            if (root.left == null && root.right == null) {
+                return sum;
+            }
+
+            return Dfs(root.left, sum) + Dfs(root.right, sum);
+        }
+    }
+}
---
+++ LeetCode/Test/P129SumRootToLeafNumbersTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P129SumRootToLeafNumbersTest {
+        [TestMethod]
+        public void SumNumbersTest () {
+            Assert.AreEqual(25, P129SumRootToLeafNumbers.SumNumbers(new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+            Assert.AreEqual(1026, P129SumRootToLeafNumbers.SumNumbers(new TreeNode(4, new TreeNode(9, new TreeNode(5), new TreeNode(1)), new TreeNode(0))));
+        }
+    }
+}
