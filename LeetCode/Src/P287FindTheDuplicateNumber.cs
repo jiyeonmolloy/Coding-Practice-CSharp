fc8d16923861f5512b857b210c2a80834e93841b
Ji <jjeonie@gmail.com>
2020-06-27 19:57:10 +1200

    LeetCode P287 Find the duplicate number

---
+++ LeetCode/Src/P287FindTheDuplicateNumber.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P287 Find the duplicate number
+    ///  href = https://leetcode.com/problems/find-the-duplicate-number/
+    /// </summary>
+    class P287FindTheDuplicateNumber {
+        public static int FindDuplicate (int[] nums) {
+            int fast = nums[0];
+            int slow = fast;
+
+            do {
+                // Perform cycle detection 
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            } while (fast != slow);
+
+            // Find entry point
+            int node = nums[0];
+            while (node != slow) {
+                slow = nums[slow];
+                node = nums[node];
+            }
+
+            return node;
+        }
+    }
+}
---
+++ LeetCode/Test/P287FindTheDuplicateNumberTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P287FindTheDuplicateNumberTest {
+        [TestMethod]
+        public void FindDuplicateTest () {
+            Assert.AreEqual(2, P287FindTheDuplicateNumber.FindDuplicate(new int[5] { 1, 3, 4, 2, 2 }));
+            Assert.AreEqual(3, P287FindTheDuplicateNumber.FindDuplicate(new int[5] { 3, 1, 3, 4, 2 }));
+            Assert.AreEqual(9, P287FindTheDuplicateNumber.FindDuplicate(new int[10] { 2, 5, 9, 6, 9, 3, 8, 9, 7, 1 })); // Duplicate number is 7?
+        }
+    }
+}
