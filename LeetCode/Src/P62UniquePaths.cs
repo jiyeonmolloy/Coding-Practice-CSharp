fba19060de8edfa0dbe5e89e18031073a4208243
Ji <jjeonie@gmail.com>
2020-06-30 20:02:04 +1200

    LeetCode P62 Unique Paths

---
+++ LeetCode/Src/P62UniquePaths.cs
@@ -0,0 +1,29 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P62 Unique Paths
+    ///  href = https://leetcode.com/problems/unique-paths/
+    /// </summary>
+    class P62UniquePaths {
+        public static int UniquePaths (int m, int n) {
+            if (m == 1 || n == 1) {
+                return 1;
+            }
+
+            int[,] dp = new int[m + 1, n + 1];
+            dp[1, 1] = 1;
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (i == 1 && j == 1) {
+                        continue;
+                    }
+
+                    dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
---
+++ LeetCode/Test/P62UniquePathsTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P62UniquePathsTest {
+        [TestMethod]
+        public void UniquePathsTest () {
+            Assert.AreEqual(3, P62UniquePaths.UniquePaths(3, 2));
+            Assert.AreEqual(28, P62UniquePaths.UniquePaths(7, 3));
+        }
+    }
+}
