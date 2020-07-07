1669b994ca22e77e944220bccbc4c8c8e3c0c751
Ji <jjeonie@gmail.com>
2020-07-07 22:01:20 +1200

    LeetCode P461 Hamming Distance

---
+++ LeetCode/Src/P461HammingDistance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P461 Hamming Distance
+    ///  href = https://leetcode.com/problems/hamming-distance/
+    /// </summary>
+    class P461HammingDistance {
+        public static int HammingDistance (int x, int y) {
+            return Convert.ToString(x ^ y, 2).Count(b => b == '1');
+        }
+    }
+}
---
+++ LeetCode/Test/P461HammingDistanceTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P461HammingDistanceTest {
+        [TestMethod]
+        public void HammingDistanceTest () {
+            Assert.AreEqual(2, P461HammingDistance.HammingDistance(1, 4));
+        }
+    }
+}
