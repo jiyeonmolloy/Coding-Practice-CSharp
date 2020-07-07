9adf06edb4f8a62d06a9863a3910c5936ce0b46d
Ji <jjeonie@gmail.com>
2020-07-07 22:04:44 +1200

    LeetCode P332 Reconstruct Itinerary

---
+++ LeetCode/Src/P332ReconstructItinerary.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P332 Reconstruct Itinerary
+    ///  href = https://leetcode.com/problems/reconstruct-itinerary/
+    /// </summary>
+    class P332ReconstructItinerary {
+        public static IList<string> FindItinerary (IList<IList<string>> tickets) {
+            IList<string> itinerary = new List<string>();
+            Dictionary<string, List<string>> ticketDict = new Dictionary<string, List<string>>();
+
+            foreach (IList<string> ticket in tickets) {
+                string from = ticket[0];
+                string to = ticket[1];
+
+                if (ticketDict.ContainsKey(from)) {
+                    ticketDict[from].Add(to);
+                } else {
+                    ticketDict.Add(from, new List<string>());
+                    ticketDict[from].Add(to);
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> item in ticketDict) {
+                item.Value.Sort();
+            }
+
+            itinerary.Add("JFK");
+            dfs(ticketDict, "JFK", tickets.Count, itinerary);
+            return itinerary;
+        }
+
+        private static bool dfs (Dictionary<string, List<string>> ticket, string start, int trips, IList<string> itinerary) {
+            if (trips == 0) {
+                return true;
+            }
+
+            if (!ticket.ContainsKey(start)) {
+                return false;
+            }
+
+            string finish;
+
+            for (int i = 0; i < ticket[start].Count; i++) {
+                finish = ticket[start][0];
+                ticket[start].RemoveAt(0);
+                itinerary.Add(finish);
+
+                if (dfs(ticket, finish, trips - 1, itinerary)) {
+                    return true;
+                }
+
+                ticket[start].Add(finish);
+                itinerary.RemoveAt(itinerary.Count - 1);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P332ReconstructItineraryTest.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P332ReconstructItineraryTest {
+        [TestMethod]
+        public void FindItineraryTest () {
+            IList<IList<string>> itinerary = new List<IList<string>>(){
+                new List<string>(){ "MUC", "LHR" },
+                new List<string>(){ "JFK", "MUC" },
+                new List<string>(){ "SFO", "SJC" },
+                new List<string>(){ "LHR", "SFO" }
+            };
+
+            IList<string> expected = new List<string>() { "JFK", "MUC", "LHR", "SFO", "SJC" };
+
+            Assert.IsTrue(expected.SequenceEqual(P332ReconstructItinerary.FindItinerary(itinerary)));
+
+            itinerary = new List<IList<string>>(){
+                new List<string>(){ "JFK","SFO" },
+                new List<string>(){ "JFK","ATL" },
+                new List<string>(){ "SFO","ATL" },
+                new List<string>(){ "ATL", "JFK" },
+                new List<string>(){ "ATL", "SFO" }
+            };
+
+            expected = new List<string>() { "JFK", "ATL", "JFK", "SFO", "ATL", "SFO" };
+
+            Assert.IsTrue(expected.SequenceEqual(P332ReconstructItinerary.FindItinerary(itinerary)));
+        }
+    }
+}
