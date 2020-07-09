c3944fafd7a4c97deab70c1dfebd78dca9925c2f
Ji <jjeonie@gmail.com>
2020-07-09 22:12:56 +1200

    LeetCode P662 Maximum Width of Binary Tree

---
+++ LeetCode/Src/P662MaximumWidthOfBinaryTree.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P662 Maximum Width of Binary Tree
+    ///  href = https://leetcode.com/problems/maximum-width-of-binary-tree/
+    /// </summary>
+    class P662MaximumWidthOfBinaryTree : TreeNode {
+        private class CustomNode {
+            public int Position;
+            public TreeNode Node;
+
+            public CustomNode(TreeNode node, int position) {
+                Node = node;
+                Position = position;
+            }
+        }
+
+        public static int WidthOfBinaryTree (TreeNode root) {
+                if (root == null) {
+                    return 0;
+                }
+
+                Queue<CustomNode> queue = new Queue<CustomNode>();
+                CustomNode customNode = new CustomNode(root, 1);
+
+                queue.Enqueue(customNode);
+
+                CustomNode node;
+                int maxWidth = 0;
+                int firstPosition, lastPosition;
+                int currentLevelWidth, position;
+                int size;
+
+                while (queue.Count > 0) {
+                    size = queue.Count;
+                    firstPosition = 0;
+                    lastPosition = 0;
+
+                    // Compute each level of Binary Tree
+                    for (int i = 0; i < size; i++) {
+                        node = queue.Dequeue();
+                        position = node.Position;
+
+                        if (i == 0) {
+                            firstPosition = position;
+                        }
+
+                        if (i == size - 1) {
+                            lastPosition = position;
+                        }
+
+                        if (node.Node.left != null) {
+                            queue.Enqueue(new CustomNode(node.Node.left, 2 * position - 1));
+                        }
+
+                        if (node.Node.right != null) {
+                            queue.Enqueue(new CustomNode(node.Node.right, 2 * position));
+                        }
+                    }
+
+                    currentLevelWidth = lastPosition - firstPosition + 1;
+                    maxWidth = currentLevelWidth > maxWidth ? currentLevelWidth : maxWidth;
+                }
+
+                return maxWidth;
+            }
+        }
+}
+
---
+++ LeetCode/Test/P662MaximumWidthOfBinaryTreeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P662MaximumWidthOfBinaryTreeTest {
+        [TestMethod]
+        public void WidthOfBinaryTreeTest () {
+            TreeNode treeNode = new TreeNode(1, new TreeNode(3, new TreeNode(5), new TreeNode(3)), new TreeNode(2, null, new TreeNode(9)));
+            P662MaximumWidthOfBinaryTree.WidthOfBinaryTree(treeNode);
+
+            treeNode = new TreeNode(2, new TreeNode(3, new TreeNode(5), new TreeNode(3)));
+            P662MaximumWidthOfBinaryTree.WidthOfBinaryTree(treeNode);
+
+            treeNode = new TreeNode(2, new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2)), null);
+            P662MaximumWidthOfBinaryTree.WidthOfBinaryTree(treeNode);
+        }
+    }
+}
