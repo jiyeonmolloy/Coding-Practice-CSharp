c3f6dc51333f68a763868356984ec03168cf6d00
Ji <jjeonie@gmail.com>
2020-07-12 21:48:55 +1200

    LeetCode Weekly Contest 197 P1513 Number of Substrings with only 1s

---
+++ LeetCode/Src/P1513NumberOfSubstringsWithOnly1s.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1513 Number of Substrings with only 1s
+    ///  href = https://leetcode.com/problems/number-of-substrings-with-only-1s/
+    ///  key : Partial Sum
+    /// </summary>
+    class P1513NumberOfSubstringsWithOnly1s {
+        public static int NumSub (string s) {
+            const char delimiter = '0';
+            const int mod = (int)(1e9) + 7;
+            string[] subStrings = s.Split(delimiter);
+            long indexes = subStrings.Sum(subString => PartialSum(subString.Length) % mod);
+            return (int)indexes;
+        }
+
+        private static long PartialSum (long num) { 
+            return num * (num + 1) / 2;
+        }
+    }
+}
---
+++ LeetCode/Test/P1513NumberOfSubstringsWithOnly1sTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1513NumberOfSubstringsWithOnly1sTest {
+        [TestMethod]
+        public void NumSubTest () {
+            Assert.AreEqual(21, P1513NumberOfSubstringsWithOnly1s.NumSub("111111"));
+            Assert.AreEqual(9, P1513NumberOfSubstringsWithOnly1s.NumSub("0110111"));
+            Assert.AreEqual(2, P1513NumberOfSubstringsWithOnly1s.NumSub("101"));
+            Assert.AreEqual(0, P1513NumberOfSubstringsWithOnly1s.NumSub("000"));
+            Assert.AreEqual(62, P1513NumberOfSubstringsWithOnly1s.NumSub("1111111111011010011"));
+        }
+    }
+}
