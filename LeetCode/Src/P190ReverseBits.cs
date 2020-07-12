1434bfc16ec3b9dcd731d0be84ae6c60eaa0689f
Ji <jjeonie@gmail.com>
2020-07-12 21:40:43 +1200

    LeetCode P190 Reverse Bits

---
+++ LeetCode/Src/P190ReverseBits.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P190 Reverse Bits
+    ///  href = https://leetcode.com/problems/reverse-bits/
+    /// </summary>
+    class P190ReverseBits {
+        public static uint ReverseBits (uint n) {
+            uint reversed = 0;
+
+            uint nLast;
+            for (int i = 0; i < 32; i++) {
+                reversed <<= 1;
+                nLast = n & 1;
+                reversed |= nLast;
+                n >>= 1;
+            }
+
+            return reversed;
+        }
+    }
+}
---
+++ LeetCode/Test/P190ReverseBitsTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P190ReverseBitsTest {
+        [TestMethod]
+        public void ReverseBitsTest () {
+            Assert.AreEqual((uint)964176192, P190ReverseBits.ReverseBits(43261596));
+            Assert.AreEqual(3221225471, P190ReverseBits.ReverseBits(4294967293));
+        }
+    }
+}
