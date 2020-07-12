c275cef4444580c777c11211df830b57c6d9ebee
Ji <jjeonie@gmail.com>
2020-07-12 18:41:36 +1200

    LeetCode Weekly Contest 197 P 1512 Number of Good Pairs

---
+++ LeetCode/Src/P1512NumberOfGoodPairs.cs
@@ -0,0 +1,25 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1512 Number of Good Pairs
+    ///  href = https://leetcode.com/problems/number-of-good-pairs/
+    /// </summary>
+    class P1512NumberOfGoodPairs {
+        public static int NumIdenticalPairs (int[] nums) {
+            int targetValue, targetIndex;
+            int pairs = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                targetIndex = i;
+                targetValue = nums[targetIndex];
+
+                for (int j = targetIndex + 1; j < nums.Length; j++) {
+                    if (targetValue == nums[j]) {
+                        pairs++;
+                    }
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
---
+++ LeetCode/Test/P1512NumberOfGoodPairsTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1512NumberOfGoodPairsTest {
+        [TestMethod]
+        public void NumIdenticalPairsTest () {
+            Assert.AreEqual(4, P1512NumberOfGoodPairs.NumIdenticalPairs(new int[6] { 1, 2, 3, 1, 1, 3 }));
+            Assert.AreEqual(6, P1512NumberOfGoodPairs.NumIdenticalPairs(new int[4] { 1, 1, 1, 1 }));
+            Assert.AreEqual(0, P1512NumberOfGoodPairs.NumIdenticalPairs(new int[3] { 1, 2, 3 }));
+        }
+    }
+}
