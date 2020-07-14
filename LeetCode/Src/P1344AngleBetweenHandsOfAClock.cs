5c2fec012d80b24dff1acd2f63a1aa8f8d0ca086
Ji <jjeonie@gmail.com>
2020-07-14 20:17:23 +1200

    LeetCode P1344 Angle Between Hands of a Clock

---
+++ LeetCode/Src/P1344AngleBetweenHandsOfAClock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1344 Angle Between Hands of a Clock
+    ///  href = https://leetcode.com/problems/angle-between-hands-of-a-clock/
+    /// </summary>
+    class P1344AngleBetweenHandsOfAClock {
+        public static double AngleClock (int hour, int minutes) {
+            double minuteAngle = minutes * 6;
+            double hourAngle = CalculateHourAngle(hour, minutes);
+            double difference = Math.Abs(minuteAngle - hourAngle); ;
+
+            return difference > 180 ? (360 - difference) : difference;
+        }
+
+        private static double CalculateHourAngle (int hour, int minutes) {
+            hour = hour == 12 ? 0 : hour;
+            return hour * 30 + 0.5 * minutes;
+        }
+    }
+}
---
+++ LeetCode/Test/P1344AngleBetweenHandsOfAClockTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1344AngleBetweenHandsOfAClockTest {
+        [TestMethod]
+        public void AngleClockTest () {
+            Assert.AreEqual(165, P1344AngleBetweenHandsOfAClock.AngleClock(12, 30));
+            Assert.AreEqual(75, P1344AngleBetweenHandsOfAClock.AngleClock(3, 30));
+            Assert.AreEqual(7.5, P1344AngleBetweenHandsOfAClock.AngleClock(3, 15));
+            Assert.AreEqual(155, P1344AngleBetweenHandsOfAClock.AngleClock(4, 50));
+            Assert.AreEqual(0, P1344AngleBetweenHandsOfAClock.AngleClock(12, 0));
+            Assert.AreEqual(76.50000, P1344AngleBetweenHandsOfAClock.AngleClock(1, 57));
+        }
+    }
+}
