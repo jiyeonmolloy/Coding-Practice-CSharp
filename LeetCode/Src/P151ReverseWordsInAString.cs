9153e569fa2562d2ba9144ae53b3d0677e932b26
Ji <jjeonie@gmail.com>
2020-07-15 19:42:17 +1200

    LeetCode P151 Reverse Words in a String

---
+++ LeetCode/Src/P151ReverseWordsInAString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P151 Reverse Words in a String
+    ///  href = https://leetcode.com/problems/reverse-words-in-a-string/
+    /// </summary>
+    class P151ReverseWordsInAString {
+        public static string ReverseWords (string s) {
+            List<string> wordArray = s.Trim().Split(' ').Where(w => !string.IsNullOrEmpty(w)).ToList();
+            string reversed = String.Empty;
+
+            for (int i = wordArray.Count - 1; i >= 0; i--) {
+                if (i != 0) {
+                    reversed += wordArray[i] + " ";
+                } else {
+                    reversed += wordArray[i]; // last word
+                }
+            }
+
+            return reversed;
+        }
+    }
+}
---
+++ LeetCode/Test/P151ReverseWordsInAStringTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P151ReverseWordsInAStringTest {
+        [TestMethod]
+        public void ReverseWordsTest () {
+            Assert.AreEqual("world! hello", P151ReverseWordsInAString.ReverseWords("  hello world!  "));
+            Assert.AreEqual("great the will", P151ReverseWordsInAString.ReverseWords("will the   great"));
+            Assert.AreEqual("friday on donuts", P151ReverseWordsInAString.ReverseWords("donuts  on    friday  "));
+        }
+    }
+}
