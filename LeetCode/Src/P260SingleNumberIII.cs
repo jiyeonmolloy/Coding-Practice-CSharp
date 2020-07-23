1acdb71b3e517558ba7d74de1165c53ad7760b34
Ji <jjeonie@gmail.com>
2020-07-23 20:44:45 +1200

    LeetCode P260 Single Number III

---
+++ LeetCode/Src/P260SingleNumberIII.cs
@@ -0,0 +1,33 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P260 Single Number III
+    ///  href = https://leetcode.com/problems/single-number-iii/
+    /// </summary>
+    class P260SingleNumberIII {
+        public static int[] SingleNumber (int[] nums) {
+            int length = nums.Length;
+            int xorSum = 0;
+
+            foreach (int num in nums) {
+                xorSum ^= num;
+            }
+
+            int bitOffset = 1;
+            while ((xorSum & bitOffset) == 0) {
+                bitOffset <<= 1;
+            }
+
+            int firstNumber = 0, secondNumber = 0;
+            foreach (int num in nums) {
+                if ((num & bitOffset) > 0) {
+                    firstNumber ^= num;
+                } else {
+                    secondNumber ^= num;
+                }
+            }
+
+            return new int[2] { firstNumber, secondNumber };
+        }
+    }
+}
---
+++ LeetCode/Test/P260SingleNumberIIITest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P260SingleNumberIIITest {
+        [TestMethod]
+        public void SingleNumberTest () {
+            CollectionAssert.AreEqual(new int[2] { 3, 5 }, P260SingleNumberIII.SingleNumber(new int[6] { 1, 2, 1, 3, 2, 5 }));
+        }
+    }
+}
