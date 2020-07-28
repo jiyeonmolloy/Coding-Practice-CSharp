6fc4228cfb3d3852c1651c9c521250ab39c674dd
Ji <jjeonie@gmail.com>
2020-07-28 21:49:24 +1200

    LeetCode P621 Task Scheduler

---
+++ LeetCode/Src/P621TaskScheduler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P621 Task Scheduler
+    ///  href = https://leetcode.com/problems/task-scheduler/
+    /// </summary>
+    class P621TaskScheduler {
+        public static int LeastInterval (char[] tasks, int n) {
+            int totalNum = tasks.Length;
+            if (n == 0) {
+                return totalNum;
+            }
+
+            Dictionary<char, int> taskDict = new Dictionary<char, int>();
+            foreach (char task in tasks) {
+                if (taskDict.ContainsKey(task)) {
+                    taskDict[task]++;
+                } else {
+                    taskDict.Add(task, 1);
+                }
+            }
+
+            int[] charFreqPairs = taskDict.OrderBy(x => x.Value).Reverse().Select(x => x.Value).ToArray();
+
+            int largestFreq = charFreqPairs[0];
+            int coolDown = (largestFreq - 1) * n;
+
+            int currentFreq;
+            for (int i = 1; i < charFreqPairs.Length; i++) {
+                currentFreq = charFreqPairs[i];
+                if (currentFreq == largestFreq) {
+                    coolDown -= currentFreq;
+                    coolDown++;
+                } else {
+                    coolDown -= currentFreq;
+                }
+            }
+
+            if (coolDown > 0) {
+                return coolDown + totalNum;
+            }
+
+            return totalNum;
+        }
+    }
+}
---
+++ LeetCode/Test/P621TaskSchedulerTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P621TaskSchedulerTest {
+        [TestMethod]
+        public void LeastIntervalTest () {
+            Assert.AreEqual(8, P621TaskScheduler.LeastInterval(new char[6] { 'A', 'A', 'A', 'B', 'B', 'B' }, 2));
+            Assert.AreEqual(6, P621TaskScheduler.LeastInterval(new char[6] { 'A', 'A', 'A', 'B', 'B', 'B' }, 0));
+            Assert.AreEqual(16, P621TaskScheduler.LeastInterval(new char[12] { 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G' }, 2));
+        }
+    }
+}
