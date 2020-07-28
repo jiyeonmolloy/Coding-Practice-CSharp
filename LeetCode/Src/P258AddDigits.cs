eb6015fc7441ff626cd9a96a2948a77ff6ee2e62
Ji <jjeonie@gmail.com>
2020-07-28 21:39:53 +1200

    LeetCode P258 Add Digits

---
+++ LeetCode/Src/P258AddDigits.cs
@@ -0,0 +1,17 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P258 Add Digits
+    ///  href = https://leetcode.com/problems/add-digits/
+    /// </summary>
+    class P258AddDigits {
+        public static int AddDigits (int num) {
+            if (num <= 0) {
+                return num;
+            }
+
+            int remainder = num % 9;
+            return remainder == 0 ? 9 : remainder;
+        }
+    }
+}
---
+++ LeetCode/Test/P258AddDigitsTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P258AddDigitsTest {
+        [TestMethod]
+        public void AddDigitsTest () {
+            Assert.AreEqual(2, P258AddDigits.AddDigits(101));
+        }
+    }
+}
