416fa185301539c3a4e862c0ee2eb8bec328641a
Ji <jjeonie@gmail.com>
2020-07-30 19:24:51 +1200

    LeetCode P53 Maximum Subarray

---
+++ LeetCode/Src/P53MaximumSubarray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P53 Maximum Subarray
+    ///  key: Kadane's Algorithm
+    ///  href = https://leetcode.com/problems/maximum-subarray/
+    /// </summary>
+    class P53MaximumSubarray {
+        public static int MaxSubArray (int[] nums) {
+            int maxCurrent = nums[0];
+            int maxGlobal = maxCurrent;
+
+            int current;
+            for (int i = 1; i < nums.Length; i++) {
+                current = nums[i];
+                maxCurrent = Math.Max(maxCurrent + current, current);
+                maxGlobal = Math.Max(maxCurrent, maxGlobal);
+            }
+
+            return maxGlobal;
+        }
+    }
+}
---
+++ LeetCode/Test/P53MaximumSubarrayTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P53MaximumSubarrayTest {
+        [TestMethod]
+        public void MaxSubArrayTest () {
+            Assert.AreEqual(5, P53MaximumSubarray.MaxSubArray(new int[4] { -2, 3, 2, -1 }));
+            Assert.AreEqual(6, P53MaximumSubarray.MaxSubArray(new int[9] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+        }
+    }
+}
