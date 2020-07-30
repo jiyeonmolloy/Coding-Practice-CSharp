74c46ff88aca70b8d24d8b890bb9c8988d2285c2
Ji <jjeonie@gmail.com>
2020-07-30 19:21:03 +1200

    LeetCode P121 Best Time To Buy And Sell Stock

---
+++ LeetCode/Src/P121BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P121 Best Time To Buy and Sell Stock
+    ///  Key: Kadane's Algorithm
+    ///  href = https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    /// </summary>
+    class P121BestTimeToBuyAndSellStock {
+        public static int MaxProfit (int[] prices) {
+            int size = prices.Length;
+            if (size == 0 || size == 1) {
+                return 0;
+            }
+
+            int buyPrice = prices[0];
+            int maxProfit = 0;
+
+            int currentPrice;
+            for (int i = 1; i < prices.Length; i++) {
+                currentPrice = prices[i];
+
+                if (currentPrice < buyPrice) {
+                    buyPrice = currentPrice;
+                } else {
+                    maxProfit = Math.Max(currentPrice - buyPrice, maxProfit);
+                }
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ LeetCode/Test/P121BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P121BestTimeToBuyAndSellStockTest {
+        [TestMethod]
+        public void MaxProfitTest () {
+            Assert.AreEqual(5, P121BestTimeToBuyAndSellStock.MaxProfit(new int[6] { 7, 1, 5, 3, 6, 4 }));
+            Assert.AreEqual(0, P121BestTimeToBuyAndSellStock.MaxProfit(new int[5] { 7, 6, 4, 3, 1 }));
+            Assert.AreEqual(0, P121BestTimeToBuyAndSellStock.MaxProfit(new int[1] { 7 }));
+        }
+    }
+}
