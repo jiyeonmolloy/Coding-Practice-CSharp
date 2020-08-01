a9624955079a5cd2206aca2bac9afe89e4cfb361
Ji <jjeonie@gmail.com>
2020-08-01 20:59:01 +1200

    LeetCode P520 Detect Capital

---
+++ LeetCode/Src/P520DetectCapital.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P520 Detect Capital
+    ///  href = https://leetcode.com/problems/detect-capital/
+    /// </summary>
+    class P520DetectCapital {
+        public static bool DetectCapitalUse (string word) {
+            if (Char.IsUpper(word[0])) {
+                word = word.Substring(1);
+            }
+            return word.All(Char.IsUpper) || word.All(Char.IsLower);
+        }
+    }
+}
---
+++ LeetCode/Test/P520DetectCapitalTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P520DetectCapitalTest {
+        [TestMethod]
+        public void DetectCapitalTest () {
+            Assert.IsTrue(P520DetectCapital.DetectCapitalUse("TOBY"));
+            Assert.IsTrue(P520DetectCapital.DetectCapitalUse("leetcode"));
+            Assert.IsFalse(P520DetectCapital.DetectCapitalUse("FlaG"));
+            Assert.IsTrue(P520DetectCapital.DetectCapitalUse("Tobies"));
+        }
+    }
+}
