70a6aac14ed1b1b6bf11d8ff14f34983c37f2fc3
Ji <jjeonie@gmail.com>
2020-08-03 22:18:02 +1200

    LeetCode P705 Design HashSet

---
+++ LeetCode/Src/P705DesignHashSet.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P705 Design HashSet
+    ///  href = https://leetcode.com/problems/design-hashset/
+    /// </summary>
+    class P705DesignHashSet {
+        private bool[] record;
+
+        /** Initialize your data structure here. */
+        public P705DesignHashSet () {
+            record = new bool[1000001];
+        }
+
+        public void Add (int key) {
+            record[key] = true;
+        }
+
+        public void Remove (int key) {
+            record[key] = false;
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains (int key) {
+            return record[key];
+        }
+    }
+}
---
+++ LeetCode/Test/P705DesignHashSetTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P705DesignHashSetTest {
+        [TestMethod]
+        public void MyHashSetTest () {
+            P705DesignHashSet hashSet = new P705DesignHashSet();
+            hashSet.Add(1);
+            hashSet.Add(2);
+            Assert.IsTrue(hashSet.Contains(1));    // returns true
+            Assert.IsFalse(hashSet.Contains(3));    // returns false (not found)
+            hashSet.Add(2);
+            hashSet.Contains(2);    // returns true
+            hashSet.Remove(2);
+            Assert.IsFalse(hashSet.Contains(2));    // returns false (already removed)
+        }
+    }
+}
