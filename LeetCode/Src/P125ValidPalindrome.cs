2cd85a566486268e1b7da72633db0456dac3c0d5
Ji <jjeonie@gmail.com>
2020-08-03 22:12:05 +1200

    LeetCode P125 Valid Palindrome

---
+++ LeetCode/Src/P125ValidPalindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P125 Valid Palindrome
+    ///  href = https://leetcode.com/problems/valid-palindrome/
+    /// </summary>
+    class P125ValidPalindrome {
+        public static bool IsPalindrome (string s) {
+            char[] palindromeArray = s.Trim().ToLower().ToCharArray().Where(Char.IsLetterOrDigit).ToArray();
+
+            if (palindromeArray.Length == 0) {
+                return true;
+            }
+
+            int start = 0;
+            int end = palindromeArray.Length - 1;
+
+            while (start < end) {
+                if (palindromeArray[start].Equals(palindromeArray[end])) {
+                    start++;
+                    end--;
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Test/P125ValidPalindromeTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P125ValidPalindromeTest {
+        [TestMethod]
+        public void IsPalindromeTest () {
+            Assert.IsTrue(P125ValidPalindrome.IsPalindrome("A man, a plan, a canal: Panama"));
+            Assert.IsFalse(P125ValidPalindrome.IsPalindrome("race a car"));
+        }
+    }
+}
