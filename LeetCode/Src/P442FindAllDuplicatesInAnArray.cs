864548e5868df79aadf38e2cd6026c641fc5c55f
Ji <jjeonie@gmail.com>
2020-08-06 20:22:59 +1200

    LeetCode P442 Find All Duplicates in an Array

---
+++ LeetCode/Src/P442FindAllDuplicatesInAnArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P442 Find All Duplicates in an Array
+    ///  href = https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    /// </summary>
+    class P442FindAllDuplicatesInAnArray {
+        public static IList<int> FindDuplicates (int[] nums) {
+            IList<int> duplicateList = new List<int>();
+            int index;
+
+            for (int i = 0; i < nums.Length; i++) {
+                index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0) {
+                    duplicateList.Add(Math.Abs(index + 1));
+                }
+
+                nums[index] = -nums[index];
+            }
+
+            return duplicateList;
+        }
+    }
+}
---
+++ LeetCode/Test/P442FindAllDuplicatesInAnArrayTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P442FindAllDuplicatesInAnArrayTest {
+        [TestMethod]
+        public void FindDuplicatesTest () {
+            IList<int> expected = new List<int>() { 2, 3 };
+            Assert.IsTrue(expected.SequenceEqual(P442FindAllDuplicatesInAnArray.FindDuplicates(new int[8] { 4, 3, 2, 7, 8, 2, 3, 1 })));
+        }
+    }
+}
