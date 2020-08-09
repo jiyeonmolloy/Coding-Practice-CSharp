6d62e9c7c5c4f5396acddf75177220e90d3e3961
Ji <jjeonie@gmail.com>
2020-08-09 22:17:24 +1200

    LeetCode P208 Implement Trie (Prefix Tree)

---
+++ LeetCode/Src/P208ImplementTriePrefixTree.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P208 Implement Trie (Prefix Tree)
+    ///  href = https://leetcode.com/problems/implement-trie-prefix-tree/
+    /// </summary>
+    class P208ImplementTriePrefixTree {
+        private Node root;
+
+        /** Initialize your data structure here. */
+        public P208ImplementTriePrefixTree () {
+            root = new Node();
+        }
+
+        /** Inserts a word into the trie. */
+        public void Insert (string word) {
+            Node current = root;
+
+            foreach (char character in word) {
+                if (!current.Next.ContainsKey(character)) {
+                    current.Next.Add(character, new Node());
+                }
+                current = current.Next[character];
+            }
+
+            current.IsCompleteWord = true;
+        }
+
+        /** Returns if the word is in the trie. */
+        public bool Search (string word) {
+            Node current = root;
+
+            foreach (char character in word) {
+                if (!current.Next.ContainsKey(character)) {
+                    return false;
+                }
+                current = current.Next[character];
+            }
+
+            return current.IsCompleteWord;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public bool StartsWith (string prefix) {
+            Node current = root;
+
+            foreach (char character in prefix) {
+                if (!current.Next.ContainsKey(character)) {
+                    return false;
+                }
+                current = current.Next[character];
+            }
+
+            return true;
+        }
+    }
+
+    public class Node {
+        public Dictionary<char, Node> Next;
+        public bool IsCompleteWord;
+
+        public Node () {
+            Next = new Dictionary<char, Node>();
+            IsCompleteWord = false;
+        }
+    }
+}
---
+++ LeetCode/Test/P208ImplementTriePrefixTreeTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P208ImplementTriePrefixTreeTest {
+        [TestMethod]
+        public void MyHashSetTest () {
+            P208ImplementTriePrefixTree trie = new P208ImplementTriePrefixTree();
+            trie.Insert("apple");
+            trie.Search("apple");   // returns true
+            trie.Search("app");     // returns false
+            trie.StartsWith("app"); // returns true
+            trie.Insert("app");
+            trie.Search("app");     // returns true
+        }
+    }
+}
