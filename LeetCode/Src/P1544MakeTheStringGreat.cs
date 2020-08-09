554b2c8474cc9c75e707f8144aeb7d5eb6ea6d1e
Ji <jjeonie@gmail.com>
2020-08-09 22:20:51 +1200

    LeetCode Weekly Contest 201 P1544 Make The String Great

---
+++ LeetCode/Src/P1544MakeTheStringGreat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1544 Make The String Great
+    ///  href = https://leetcode.com/problems/make-the-string-great/
+    /// </summary>
+    class P1544MakeTheStringGreat {
+        public static string MakeGood (string s) {
+            int index = 0;
+            Char current, next;
+            while (index <= s.Length - 2) {
+                current = s[index];
+                next = s[index + 1];
+                if (Char.ToLower(current) == Char.ToLower(next)) {
+                    if ((Char.IsUpper(current) && Char.IsLower(next)) || (Char.IsUpper(next) && Char.IsLower(current))) {
+                        s = s.Remove(index, 2);
+
+                        if (index == s.Length) {
+                            break;
+                        }
+
+                        if (index != 0) {
+                            index -= 1;
+                        }
+
+                        continue;
+                    }
+                }
+
+                index++;
+            }
+
+            return s;
+        }
+    }
+}
---
+++ LeetCode/Test/P1544MakeTheStringGreatTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1544MakeTheStringGreatTest {
+        [TestMethod]
+        public void MakeGoodTest () {
+            Assert.AreEqual(String.Empty, P1544MakeTheStringGreat.MakeGood("eeAaEE"));
+            Assert.AreEqual("leetcode", P1544MakeTheStringGreat.MakeGood("leEeetcode"));
+            Assert.AreEqual(String.Empty, P1544MakeTheStringGreat.MakeGood("abBAcC"));
+            Assert.AreEqual("s", P1544MakeTheStringGreat.MakeGood("s"));
+        }
+    }
+}
