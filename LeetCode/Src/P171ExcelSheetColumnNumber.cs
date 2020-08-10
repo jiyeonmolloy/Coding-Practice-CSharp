f5c64767aeede827ae55013397942f1c2033c983
Ji <jjeonie@gmail.com>
2020-08-10 22:05:20 +1200

    LeetCode P171 Excel Sheet Column Number

---
+++ LeetCode/Src/P171ExcelSheetColumnNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P171ExcelSheetColumnNumber
+    ///  href = https://leetcode.com/problems/excel-sheet-column-number/
+    /// </summary>
+    class P171ExcelSheetColumnNumber {
+        public static int TitleToNumber (string s) {
+            const int ALPHABET_MAX_COUNT = 26;
+            s = s.ToLower();
+            int size = s.Length;
+
+            int column = s[size - 1] - 'a' + 1;
+            int count = 1;
+            for (int i = size - 2; i >= 0; i--) {
+                column += (s[i] - 'a' + 1) * (int)Math.Pow(ALPHABET_MAX_COUNT, count);
+                count++;
+            }
+
+            return column;
+        }
+    }
+}
---
+++ LeetCode/Test/P171ExcelSheetColumnNumberTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P171ExcelSheetColumnNumberTest {
+        [TestMethod]
+        public void TitleToNumberTest () {
+            Assert.AreEqual(1, P171ExcelSheetColumnNumber.TitleToNumber("A"));
+            Assert.AreEqual(28, P171ExcelSheetColumnNumber.TitleToNumber("AB"));
+            Assert.AreEqual(701, P171ExcelSheetColumnNumber.TitleToNumber("ZY"));
+            Assert.AreEqual(703, P171ExcelSheetColumnNumber.TitleToNumber("AAA"));
+        }
+    }
+}
