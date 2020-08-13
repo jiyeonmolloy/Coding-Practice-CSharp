a28267974c10a7171cb9d1d6dc2d817517bbf87b
Ji <jjeonie@gmail.com>
2020-08-13 22:01:31 +1200

    LeetCode P119 Pascal's Triangle II

---
+++ LeetCode/Src/P119PascalsTriangleII.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P119 Pascal's Triangle II
+    ///  href = https://leetcode.com/problems/pascals-triangle-ii/
+    /// </summary>
+    class P119PascalsTriangleII {
+        public static IList<int> GetRow (int rowIndex) {
+            IList<int> previousRow = new List<int> {
+                1
+            };
+
+            IList<int> row = new List<int> { 1 };
+            for (int rowNumber = 1; rowNumber <= rowIndex; rowNumber++) {
+                row = new List<int> { 1 };
+
+
+                for (int i = 1; i < rowNumber; i++) {
+                    row.Add(previousRow[i - 1] + previousRow[i]);
+                }
+
+                row.Add(1);
+                previousRow.Clear();
+                previousRow = row;
+            }
+
+            return row;
+        }
+    }
+}
---
+++ LeetCode/Test/P119PascalsTriangleIITest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P119PascalsTriangleIITest {
+        [TestMethod]
+        public void GetRowTest () {
+            IList<int> expectedRow = new List<int>() { 1, 3, 3, 1 };
+            Assert.IsTrue(expectedRow.SequenceEqual(P119PascalsTriangleII.GetRow(3)));
+
+            expectedRow = new List<int>() { 1 };
+            Assert.IsTrue(expectedRow.SequenceEqual(P119PascalsTriangleII.GetRow(0)));
+        }
+    }
+}
