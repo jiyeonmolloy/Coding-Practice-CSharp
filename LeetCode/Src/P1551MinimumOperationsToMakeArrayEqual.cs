e43820490dc233619b496334ba022d0b4a102b94
Ji <jjeonie@gmail.com>
2020-08-17 21:09:50 +1200

    LeetCode P1551 Minimum Operations To Make Array Equal

---
+++ LeetCode/Src/P1551MinimumOperationsToMakeArrayEqual.cs
@@ -0,0 +1,20 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1551 Minimum Operations To Make Array Equal 
+    ///  href = https://leetcode.com/problems/minimum-operations-to-make-array-equal/
+    /// </summary>
+    class P1551MinimumOperationsToMakeArrayEqual {
+        public static int MinOperations (int n) {
+            int middleIndex = n / 2;
+            if (n % 2 == 1) {
+                // if n is odd
+                return middleIndex * middleIndex + middleIndex;
+            }
+
+            // if n is even
+            middleIndex -= 1;
+            return middleIndex * (middleIndex + 1) + middleIndex + 1;
+        }
+    }
+}
---
+++ LeetCode/Test/P1551MinimumOperationsToMakeArrayEqualTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1551MinimumOperationsToMakeArrayEqualTest {
+        [TestMethod]
+        public void MinOperationsTest () {
+            // odd
+            Assert.AreEqual(6, P1551MinimumOperationsToMakeArrayEqual.MinOperations(5));
+            Assert.AreEqual(12, P1551MinimumOperationsToMakeArrayEqual.MinOperations(7));
+            Assert.AreEqual(2, P1551MinimumOperationsToMakeArrayEqual.MinOperations(3));
+            Assert.AreEqual(0, P1551MinimumOperationsToMakeArrayEqual.MinOperations(1));
+            // even
+            Assert.AreEqual(1, P1551MinimumOperationsToMakeArrayEqual.MinOperations(2));
+            Assert.AreEqual(9, P1551MinimumOperationsToMakeArrayEqual.MinOperations(6));
+            Assert.AreEqual(16, P1551MinimumOperationsToMakeArrayEqual.MinOperations(8));
+        }
+    }
+}
