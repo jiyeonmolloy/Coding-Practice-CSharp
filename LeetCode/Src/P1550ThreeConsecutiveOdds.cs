bd9f26cc8e1c5de26695bac9e5c3e164f9ac97b5
Ji <jjeonie@gmail.com>
2020-08-17 20:04:26 +1200

    LeetCode P1550 Three Consecutive Odds

---
+++ LeetCode/Src/P1550ThreeConsecutiveOdds.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1550 Three Consecutive Odds
+    ///  href = https://leetcode.com/problems/three-consecutive-odds/
+    /// </summary>
+    class P1550ThreeConsecutiveOdds {
+        public static bool ThreeConsecutiveOdds (int[] arr) {
+            int count = 0;
+            bool start = false;
+            foreach (int element in arr) {
+                if ((count == 0 || (start && count > 0)) && (element % 2 == 1)) {
+                    count++;
+                    start = true;
+                } else {
+                    count = 0;
+                    start = false;
+                }
+
+                if (count == 3) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P1550ThreeConsecutiveOddsTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1550ThreeConsecutiveOddsTest {
+        [TestMethod]
+        public void ThreeConsecutiveOddsTest () {
+            Assert.IsFalse(P1550ThreeConsecutiveOdds.ThreeConsecutiveOdds(new int[4] { 2, 6, 4, 1 }));
+            Assert.IsTrue(P1550ThreeConsecutiveOdds.ThreeConsecutiveOdds(new int[9] { 1, 2, 34, 3, 4, 5, 7, 23, 12 }));
+        }
+    }
+}
