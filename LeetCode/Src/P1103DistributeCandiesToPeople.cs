1be06c84ec4277a4b50e0a55ad8e7941a7379e35
Ji <jjeonie@gmail.com>
2020-08-18 20:01:37 +1200

    LeetCode P1103 Distribute Candies To People

---
+++ LeetCode/Src/P1103DistributeCandiesToPeople.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1103 Distribute Candies To People
+    ///  href = https://leetcode.com/problems/distribute-candies-to-people/
+    /// </summary>
+    class P1103DistributeCandiesToPeople {
+        public static int[] DistributeCandies (int candies, int num_people) {
+            int[] people = new int[num_people];
+            int currentCandy = 1;
+            int currentPerson = 0;
+
+            while (candies >= 0) {
+
+                people[currentPerson] += Math.Min(currentCandy, candies);
+                candies -= currentCandy;
+                currentCandy++;
+                currentPerson = (currentPerson + 1) % num_people;
+            }
+
+            return people;
+        }
+    }
+}
---
+++ LeetCode/Test/P1103DistributeCandiesToPeopleTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1103DistributeCandiesToPeopleTest {
+        [TestMethod]
+        public void DistributeCandiesTest () {
+            CollectionAssert.AreEqual(new int[4] { 1, 2, 3, 1 }, P1103DistributeCandiesToPeople.DistributeCandies(7, 4));
+            CollectionAssert.AreEqual(new int[3] { 5, 2, 3 }, P1103DistributeCandiesToPeople.DistributeCandies(10, 3));
+            CollectionAssert.AreEqual(new int[4] { 15, 18, 15, 12 }, P1103DistributeCandiesToPeople.DistributeCandies(60, 4));
+        }
+    }
+}
