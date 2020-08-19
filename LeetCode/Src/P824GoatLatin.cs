8a5bcee86e0522b4090846239a6c05e19ee09b1e
Ji <jjeonie@gmail.com>
2020-08-19 21:41:22 +1200

    LeetCode P824 Goat Latin

---
+++ LeetCode/Src/P824GoatLatin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P824 Goat Latin
+    ///  href = https://leetcode.com/problems/goat-latin/
+    /// </summary>
+    class P824GoatLatin {
+        public static string ToGoatLatin (string S) {
+            string[] words = S.Split(' ');
+
+            string word;
+            char first;
+            for (int i = 0; i < words.Length; i++) {
+                word = words[i].ToLower();
+                if (!word.StartsWith("a") && !word.StartsWith("e") && !word.StartsWith("i") && !word.StartsWith("o") && !word.StartsWith("u")) {
+                    first = words[i][0];
+                    words[i] = words[i].Remove(0, 1);
+                    words[i] += first;
+                }
+                words[i] += "ma" + AddAs(i + 1);
+            }
+
+            return string.Join(" ", words);
+        }
+
+        private static string AddAs (int number) {
+            string a = String.Empty;
+            for (int i = 0; i < number; i++) {
+                a += "a";
+            }
+
+            return a;
+        }
+    }
+}
---
+++ LeetCode/Test/P824GoatLatinTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P824GoatLatinTest {
+        [TestMethod]
+        public void ToGoatLatinTest () {
+            Assert.AreEqual("Imaa peaksmaaa oatGmaaaa atinLmaaaaa", P824GoatLatin.ToGoatLatin("I speak Goat Latin"));
+            Assert.AreEqual("heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa", P824GoatLatin.ToGoatLatin("The quick brown fox jumped over the lazy dog"));
+            Assert.AreEqual("obiesTmaa leepingSmaaa", P824GoatLatin.ToGoatLatin("Tobies Sleeping"));
+        }
+    }
+}
