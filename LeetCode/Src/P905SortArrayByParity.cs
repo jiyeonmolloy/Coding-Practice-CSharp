2905ebd3c3aa300b861251f7756cd44920a6f70b
Ji <jjeonie@gmail.com>
2020-08-23 19:58:20 +1200

    LeetCode P905 Sort Array By Parity

---
+++ LeetCode/Src/P905SortArrayByParity.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P905 Sort Array By Parity
+    ///  href = https://leetcode.com/problems/sort-array-by-parity/
+    /// </summary>
+    class P905SortArrayByParity {
+        public static int[] SortArrayByParity (int[] A) {
+            int[] even = A.Where(x => x % 2 == 0).ToArray();
+            int[] odd = A.Where(x => x % 2 == 1).ToArray();
+
+            return even.Concat(odd).ToArray();
+        }
+    }
+}
---
+++ LeetCode/Test/P905SortArrayByParityTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P905SortArrayByParityTest {
+        [TestMethod]
+        public void SortArrayByParityTest () {
+            CollectionAssert.AreEqual(new int[4] { 2, 4, 3, 1 }, P905SortArrayByParity.SortArrayByParity(new int[4] { 3, 1, 2, 4 }));
+        }
+    }
+}
