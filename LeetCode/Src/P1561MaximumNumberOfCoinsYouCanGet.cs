b11a635a3680eea48d4bfdad5d6fa734e513bd97
Ji <jjeonie@gmail.com>
2020-08-23 19:37:09 +1200

    LeetCode Weekly Contest 203 P1561 Maximum Number Of Coins You Can Get

---
+++ LeetCode/Src/P1561MaximumNumberOfCoinsYouCanGet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1561 Maximum Number Of Coins You Can Get
+    ///  href = https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+    /// </summary>
+    class P1561MaximumNumberOfCoinsYouCanGet {
+        public static int MaxCoins (int[] piles) {
+            int size = piles.Length;
+            int lowestCount = size / 3;
+
+            Array.Sort(piles);
+
+            int result = 0;
+            for (int i = lowestCount; i < piles.Length; i += 2) {
+                result += piles[i];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Test/P1561MaximumNumberOfCoinsYouCanGetTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1561MaximumNumberOfCoinsYouCanGetTest {
+        [TestMethod]
+        public void MaxCoinsTest () {
+            Assert.AreEqual(18, P1561MaximumNumberOfCoinsYouCanGet.MaxCoins(new int[9] { 9, 8, 7, 6, 5, 1, 2, 3, 4 }));
+            Assert.AreEqual(9, P1561MaximumNumberOfCoinsYouCanGet.MaxCoins(new int[6] { 2, 4, 1, 2, 7, 8 }));
+            Assert.AreEqual(4, P1561MaximumNumberOfCoinsYouCanGet.MaxCoins(new int[3] { 2, 4, 5 }));
+        }
+    }
+}
