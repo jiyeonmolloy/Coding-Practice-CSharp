d98b1af1abdf3a8bfe68061e2d03e7c312089272
Ji <jjeonie@gmail.com>
2020-08-24 19:58:58 +1200

    LeetCode P404 Sum Of Left Leaves, 100th Question done :)

---
+++ LeetCode/Src/P404SumOfLeftLeaves.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P404 Sum Of Left Leaves
+    ///  href = https://leetcode.com/problems/sum-of-left-leaves/
+    /// </summary>
+    class P404SumOfLeftLeaves {
+        public static int SumOfLeftLeaves (TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int leftCount = SumOfLeftLeaf(root.left, true);
+            int rightCount = SumOfLeftLeaf(root.right, false);
+
+            return leftCount + rightCount;
+        }
+
+        private static int SumOfLeftLeaf (TreeNode root, Boolean isLeftLeaf) {
+            if (root == null) {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null && isLeftLeaf) {
+                return root.val;
+            }
+
+            int leftCount = SumOfLeftLeaf(root.left, true);
+            int rightCount = SumOfLeftLeaf(root.right, false);
+
+            return leftCount + rightCount;
+        }
+    }
+}
---
+++ LeetCode/Test/P404SumOfLeftLeavesTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P404SumOfLeftLeavesTest {
+        [TestMethod]
+        public void SumOfLeftLeavesTest () {
+            Assert.AreEqual(24, P404SumOfLeftLeaves.SumOfLeftLeaves(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(20, new TreeNode(15))))));
+        }
+    }
+}
