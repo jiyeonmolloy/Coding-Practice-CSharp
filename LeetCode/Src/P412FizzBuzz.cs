c6635f63e509ff0d751fd90d6a30c1075dae1b73
Ji <jjeonie@gmail.com>
2020-08-26 21:51:54 +1200

    LeetCode P412 Fizz Buzz

---
+++ LeetCode/Src/P412FizzBuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P412 Fizz Buzz
+    ///  href = https://leetcode.com/problems/fizz-buzz/
+    /// </summary>
+    class P412FizzBuzz {
+        public static IList<string> FizzBuzz (int n) {
+            IList<string> fizzBuzzList = new List<string>();
+            bool divisibleByThree, divisibleByFive;
+            string fizzBuzz;
+
+            for (int i = 1; i < n + 1; i++) {
+                divisibleByThree = i % 3 == 0;
+                divisibleByFive = i % 5 == 0;
+                fizzBuzz = String.Empty;
+
+                if (divisibleByThree) {
+                    fizzBuzz = "Fizz";
+                }
+
+                if (divisibleByFive) {
+                    fizzBuzz += "Buzz";
+                }
+
+                fizzBuzzList.Add(String.IsNullOrEmpty(fizzBuzz) ? i.ToString() : fizzBuzz);
+            }
+
+            return fizzBuzzList;
+        }
+    }
+}
---
+++ LeetCode/Test/P412FizzBuzzTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P412FizzBuzzTest {
+        [TestMethod]
+        public void FizzBuzzTest () {
+            IList<string> expectedResponse = new List<string>() {
+                "1",
+                "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz" };
+            Assert.IsTrue(expectedResponse.SequenceEqual(P412FizzBuzz.FizzBuzz(15)));
+        }
+    }
+}
