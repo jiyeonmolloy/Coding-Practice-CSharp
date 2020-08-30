f44a41f0bdf66ee14d3dde6dec0d277d0f24d89a
Ji <jjeonie@gmail.com>
2020-08-30 21:58:29 +1200

    LeetCode Weekly Contest 204 P1566 Detect Pattern of Length M Repeated K or MoreTimes

---
+++ LeetCode/Src/P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1566 Detect Pattern of Length M Repeated K or MoreTimes
+    ///  href = https://leetcode.com/problems/detect-pattern-of-length-m-repeated-k-or-more-times/
+    /// </summary>
+    class P1566DetectPatternOfLengthMRepeatedKOrMoreTimes {
+        public static bool ContainsPattern (int[] arr, int m, int k) {
+            int startIndex = 0;
+            int index = 0;
+            int patterns = 1;
+
+            // Slow..
+            List<int> list = arr.ToList();
+            List<int> current = new List<int> { };
+            List<int> target = new List<int> { };
+
+            target = list.GetRange(startIndex, m);
+            index = m;
+            current = list.GetRange(index, m);
+            while (index < arr.Length) {
+                if (Enumerable.SequenceEqual(target, current)) {
+                    patterns++;
+
+                    if (patterns == k) {
+                        return true;
+                    }
+
+                    index += m;
+
+                    if (index + m > arr.Length) {
+                        break;
+                    }
+                } else {
+                    patterns = 1;
+                    startIndex++;
+                    index = startIndex + m;
+
+                    if (startIndex + m > arr.Length) {
+                        break;
+                    }
+
+                    if (index + m > arr.Length) {
+                        break;
+                    }
+
+                    target = list.GetRange(startIndex, m);
+                }
+
+                current = list.GetRange(index, m);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P1566DetectPatternOfLengthMRepeatedKOrMoreTimesTest.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1566DetectPatternOfLengthMRepeatedKOrMoreTimesTest {
+        [TestMethod]
+        public void ContainsPatternTest () {
+            Assert.IsTrue(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[6] { 1, 2, 4, 4, 4, 4 }, 1, 3));
+            Assert.IsTrue(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[8] { 1, 1, 1, 3, 1, 2, 1, 2 }, 2, 2));
+            Assert.IsFalse(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[6] { 1, 2, 1, 2, 1, 3 }, 2, 3));
+            Assert.IsFalse(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[5] { 1, 2, 3, 1, 2 }, 2, 2));
+            Assert.IsFalse(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[4] { 2, 2, 2, 2 }, 2, 3));
+            Assert.IsFalse(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[4] { 2, 2, 2, 2 }, 2, 3));
+            Assert.IsFalse(P1566DetectPatternOfLengthMRepeatedKOrMoreTimes.ContainsPattern(new int[26] { 3, 6, 6, 6, 5, 1, 5, 2, 2, 3, 1, 5, 2, 6, 1, 5, 1, 2, 6, 3, 3, 5, 3, 6, 3, 4 }, 6, 2));
+        }
+    }
+}
