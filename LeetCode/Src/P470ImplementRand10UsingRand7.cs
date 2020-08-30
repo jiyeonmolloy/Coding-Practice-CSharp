1568eecb55e30abdb3a3973653847127361994e9
Ji <jjeonie@gmail.com>
2020-08-30 22:01:50 +1200

    LeetCode P470 Implement Rand10 Using Rand7

---
+++ LeetCode/Src/P470ImplementRand10UsingRand7.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P470 Implement Rand10 Using Rand7
+    ///  href = https://leetcode.com/problems/implement-rand10-using-rand7/
+    /// </summary>
+    class P470ImplementRand10UsingRand7 {
+        private static int Rand7 () {
+            return new Random().Next(1, 7);
+        }
+
+        public static int Rand10 () {
+            int row;
+            int col;
+            int index = 100;
+
+            while (index > 40) {
+                row = Rand7();
+                col = Rand7();
+                // Generate value between 0 .. 49
+                // Ignore values between 41 .. 49 (Only from 1 .. 9)
+                index = col + (row - 1) * 7;
+            }
+
+            return 1 + (index - 1) % 10;
+        }
+    }
+}
---
+++ LeetCode/Test/P470ImplementRand10UsingRand7Test.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P470ImplementRand10UsingRand7Test {
+        [TestMethod]
+        public void Rand7Test () {
+            Assert.IsTrue(P470ImplementRand10UsingRand7.Rand10() < 11);
+        }
+    }
+}
