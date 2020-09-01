bc61e9b7205cb76387788210c1219c915eb84bec
Ji <jjeonie@gmail.com>
2020-09-01 18:35:25 +1200

    Fix build

--- LeetCode/Test/P450DeleteNodeInABSTTest.cs
+++ LeetCode/Test/P450DeleteNodeInABSTTest.cs
@@ -5,7 +5,7 @@ using FluentAssertions;
 
 namespace LeetCode.Test {
     [TestClass]
-    public class P450DeleteNodeInABSTTest {
+    public class P450DeleteNodeInABSTTest : TreeNode {
         [TestMethod]
         public void DeleteNodeTest () {
             TreeNode BST = P450DeleteNodeInABST.DeleteNode(new TreeNode(5, new TreeNode(3, new TreeNode(2), new TreeNode(4)), new TreeNode(6, right = new TreeNode(7))), 3);
