4f17e710207ba872733de85bf07b92f8d58234a6
Ji <jjeonie@gmail.com>
2020-09-02 21:58:26 +1200

    LeetCode P217 Contains Duplicate

---
+++ LeetCode/Src/P217ContainsDuplicate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P217 Contains Duplicate
+    ///  href = https://leetcode.com/problems/contains-duplicate/submissions/
+    /// </summary>
+    class P217ContainsDuplicate {
+        public static bool ContainsDuplicate (int[] nums) {
+            HashSet<int> hashSet = new HashSet<int>(nums);
+            return hashSet.Count != nums.Length;
+        }
+    }
+}
---
+++ LeetCode/Test/P217ContainsDuplicateTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P217ContainsDuplicateTest {
+        [TestMethod]
+        public void ContainsDuplicateTest () {
+            Assert.IsTrue(P217ContainsDuplicate.ContainsDuplicate(new int[4] {1,2,3,1}));
+            Assert.IsFalse(P217ContainsDuplicate.ContainsDuplicate(new int[4] { 1, 2, 3, 4 }));
+        }
+    }
+}
