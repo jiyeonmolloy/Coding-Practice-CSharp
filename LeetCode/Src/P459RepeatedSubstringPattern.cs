0e77dde09ae0205b81b909f3a54f1bf4fb617a01
Ji <jjeonie@gmail.com>
2020-09-03 22:08:20 +1200

    LeetCode P459 Repeated Substring Pattern

---
+++ LeetCode/Src/P459RepeatedSubstringPattern.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P459 Repeated Substring Pattern
+    ///  href = https://leetcode.com/problems/repeated-substring-pattern/
+    /// </summary>
+    class P459RepeatedSubstringPattern {
+        public static bool RepeatedSubstringPattern (string s) {
+            return (s + s).Substring(1, s.Length * 2 - 2).Contains(s);
+        }
+    }
+}
---
+++ LeetCode/Test/P459RepeatedSubstringPatternTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P459RepeatedSubstringPatternTest {
+        [TestMethod]
+        public void RepeatedSubstringPatternTest () {
+            Assert.IsTrue(P459RepeatedSubstringPattern.RepeatedSubstringPattern("abab"));
+            Assert.IsTrue(P459RepeatedSubstringPattern.RepeatedSubstringPattern("tobytobytoby"));
+            Assert.IsFalse(P459RepeatedSubstringPattern.RepeatedSubstringPattern("tobies"));
+        }
+    }
+}
