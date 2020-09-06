2da7d868fb7f57c9547c44129aff417078f46461
Ji <jjeonie@gmail.com>
2020-09-06 22:12:28 +1200

    LeetCode P1305 All Elements In Two Binary Search Trees

---
+++ LeetCode/Src/P1305AllElementsInTwoBinarySearchTrees.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1305 All Elements In Two Binary Search Trees
+    ///  href = https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+    /// </summary>
+    class P1305AllElementsInTwoBinarySearchTrees : TreeNode {
+        public static IList<int> GetAllElements (TreeNode root1, TreeNode root2) {
+            IList<int> order = new List<int>();
+            Dfs(root1, order);
+            IList<int> order2 = new List<int>();
+            Dfs(root2, order2);
+
+            return order.Concat(order2).OrderBy(x => x).ToList(); ;
+        }
+
+        private static void Dfs (TreeNode root, IList<int> list) {
+            if (root == null) {
+                return;
+            }
+
+            Dfs(root.left, list);
+            Dfs(root.right, list);
+
+            list.Add(root.val);
+        }
+    }
+}
---
+++ LeetCode/Test/P1305AllElementsInTwoBinarySearchTreesTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1305AllElementsInTwoBinarySearchTreesTest : TreeNode {
+        [TestMethod]
+        public void GetAllElementsTest () {
+            IList<int> expected = new List<int>() { 0, 1, 1, 2, 3, 4 };
+            expected.Should().BeEquivalentTo(P1305AllElementsInTwoBinarySearchTrees.GetAllElements(
+                new TreeNode(2, new TreeNode(1), new TreeNode(4)),
+                new TreeNode(1, new TreeNode(0), new TreeNode(3))));
+        }
+    }
+}
