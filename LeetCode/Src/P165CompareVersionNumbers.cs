e31cdd5cc4813f4c1554dfc6cd7959942a1330b7
Ji <jjeonie@gmail.com>
2020-09-09 19:59:17 +1200

    LeetCode P165 Compare Version Numbers

---
+++ LeetCode/Src/P165CompareVersionNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P165 Compare Version Numbers
+    ///  href = https://leetcode.com/problems/compare-version-numbers/
+    /// </summary>
+    class P165CompareVersionNumbers {
+        public static int CompareVersion (string version1, string version2) {
+            // -1 means v2 is larger
+            // 1 means v1 is larger
+            // 1 means v1 and v2 are the same
+
+            string[] v1Array = version1.Split('.');
+            string[] v2Array = version2.Split('.');
+
+            int index = 0;
+            int total = Math.Max(v1Array.Length, v2Array.Length) - 1;
+
+            int v1, v2;
+            while (index <= total) {
+                v1 = GetVersion(index, v1Array);
+                v2 = GetVersion(index, v2Array);
+
+                if (v1 > v2) {
+                    return 1;
+                } else if (v2 > v1) {
+                    return -1;
+                }
+
+                index++;
+            }
+
+            return 0;
+        }
+
+        private static int GetVersion (int index, string[] versions) {
+            if (index > versions.Length - 1) {
+                return 0;
+            }
+
+            string version = versions[index].TrimStart(new Char[] { '0' });
+            return version.Equals(String.Empty) ? 0 : int.Parse(version);
+        }
+    }
+}
---
+++ LeetCode/Test/P165CompareVersionNumbersTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P165CompareVersionNumbersTest {
+        [TestMethod]
+        public void CompareVersionTest () {
+            Assert.AreEqual(-1, P165CompareVersionNumbers.CompareVersion("0.1", "1.1"));
+            Assert.AreEqual(1, P165CompareVersionNumbers.CompareVersion("1.0.1", "1"));
+            Assert.AreEqual(-1, P165CompareVersionNumbers.CompareVersion("7.5.2.4", "7.5.3"));
+            Assert.AreEqual(0, P165CompareVersionNumbers.CompareVersion("1.01", "1.001"));
+            Assert.AreEqual(0, P165CompareVersionNumbers.CompareVersion("1.0", "1.0.0"));
+        }
+    }
+}
