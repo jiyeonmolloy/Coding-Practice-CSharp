49687e17af9ea5c683b48f6f300f0226d966b7fe
Ji <jjeonie@gmail.com>
2020-09-13 21:49:13 +1200

    Weekly Contest 206 LeetCode P1582 Special Positions in a Binary Matrix

---
+++ LeetCode/Src/P1582SpecialPositionsInABinaryMatrix.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1582 Special Positions In A Binary Matrix
+    ///  href = https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+    /// </summary>
+    class P1582SpecialPositionsInABinaryMatrix {
+        public static int NumSpecial (int[][] mat) {
+            int rows = mat.Length;
+            int cols = mat[0].Length;
+            int result = 0;
+
+            List<int> currentRow = new List<int>();
+            int targetCol = 0;
+            bool validCol = false;
+            for (int i = 0; i < rows; i++) {
+                currentRow = mat[i].ToList();
+                validCol = false;
+                if (currentRow.Count(x => x == 1) == 1) {
+                    // iterate vertically through column to validate
+                    targetCol = currentRow.IndexOf(1);
+                    for (int j = 0; j < rows; j++) {
+                        if (mat[j][targetCol] == 1) {
+                            if (validCol) {
+                                validCol = false;
+                                break;
+                            }
+                            validCol = true;
+                        }
+                    }
+
+                    if (validCol) {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Test/P1582SpecialPositionsInABinaryMatrixTest.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1582SpecialPositionsInABinaryMatrixTest {
+        [TestMethod]
+        public void NumSpecialTest () {
+            int[][] input = new int[3][] {
+                new int[3] { 1,0,0 },
+                new int[3] { 0,0,1 },
+                new int[3] { 1, 0, 0 }
+            };
+            Assert.AreEqual(1, P1582SpecialPositionsInABinaryMatrix.NumSpecial(input));
+
+            input = new int[3][] {
+                new int[3] { 1,0,0 },
+                new int[3] { 0,1,0 },
+                new int[3] { 0, 0, 1 }
+            };
+            Assert.AreEqual(3, P1582SpecialPositionsInABinaryMatrix.NumSpecial(input));
+
+            input = new int[4][] {
+                new int[4] { 0,0,0,1 },
+                new int[4] { 1,0,0,0 },
+                new int[4] { 0,1,1,0 },
+                new int[4] { 0, 0, 0, 0 }
+            };
+            Assert.AreEqual(2, P1582SpecialPositionsInABinaryMatrix.NumSpecial(input));
+
+            input = new int[5][] {
+                new int[5] { 0,0,0,0,0 },
+                new int[5] { 1,0,0,0,0 },
+                new int[5] { 0,1,0,0,0 },
+                new int[5] { 0, 0, 1, 0, 0 },
+                new int[5] { 0,0,0,1,1 },
+            };
+            Assert.AreEqual(3, P1582SpecialPositionsInABinaryMatrix.NumSpecial(input));
+
+            input = new int[5][] {
+                new int[8] { 0,0,0,0,0,1,0,0 },
+                new int[8] { 0,0,0,0,1,0,0,1 },
+                new int[8] { 0,0,0,0,1,0,0,0},
+                new int[8] { 1,0,0,0,1,0,0,0 },
+                new int[8] { 0,0,1,1,0,0,0,0 }
+            };
+            Assert.AreEqual(1, P1582SpecialPositionsInABinaryMatrix.NumSpecial(input));
+        }
+    }
+}
