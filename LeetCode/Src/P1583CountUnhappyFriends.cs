ef01e91846da8d08e427b093134005349dd87282
Ji <jjeonie@gmail.com>
2020-09-14 19:33:11 +1200

    LeetCode P1583 Count Unhappy Friends

---
+++ LeetCode/Src/P1583CountUnhappyFriends.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1583 Count Unhappy Friends
+    ///  href = https://leetcode.com/problems/count-unhappy-friends/
+    /// </summary>
+    class P1583CountUnhappyFriends {
+        public static int UnhappyFriends (int n, int[][] preferences, int[][] pairs) {
+            // Store each pair in a pair dictionary to improve performance
+            Dictionary<int, int> pairDictionary = new Dictionary<int, int>();
+            foreach (int[] pair in pairs) {
+                pairDictionary.Add(pair[0], pair[1]);
+                pairDictionary.Add(pair[1], pair[0]);
+            }
+
+            // Store index (preference) of each friend in dictionary to improve performance
+            Dictionary<int, int>[] prefDictionary = new Dictionary<int, int>[n];
+            for (int i = 0; i < n; i++) {
+                prefDictionary[i] = new Dictionary<int, int>();
+                for (int j = 0; j < n - 1; j++) {
+                    // Friend i's preferences
+                    prefDictionary[i].Add(preferences[i][j], j);
+                }
+            }
+
+            int unhappy = 0;
+            int firstFriend, secondFriend;
+            foreach (int[] pair in pairs) {
+                firstFriend = pair[0];
+                secondFriend = pair[1];
+                if (IsUnhappy(firstFriend, secondFriend, preferences[firstFriend], pairDictionary, prefDictionary)) {
+                    unhappy++;
+                }
+
+                if (IsUnhappy(secondFriend, firstFriend, preferences[secondFriend], pairDictionary, prefDictionary)) {
+                    unhappy++;
+                }
+            }
+
+            return unhappy;
+        }
+
+        private static bool IsUnhappy (int firstFriend, int secondFriend, int[] preference,
+            Dictionary<int, int> pairDictionary,
+            Dictionary<int, int>[] preferenceDictionary) {
+
+            int friend, pair;
+            int friendPreference, pairPreference;
+            for (int i = 0; i < preference.Length; i++) {
+                friend = preference[i];
+                if (friend.Equals(secondFriend)) {
+                    break;
+                }
+
+                friendPreference = preferenceDictionary[friend][firstFriend];
+
+                pair = pairDictionary[friend];
+                pairPreference = preferenceDictionary[friend][pair];
+
+                if (friendPreference < pairPreference) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P1583CountUnhappyFriendsTest.cs
@@ -0,0 +1,59 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1583CountUnhappyFriendsTest {
+        [TestMethod]
+        public void UnhappyFriendsTest () {
+            int[][] input = {
+                new[] { 1, 2, 3 },
+                new[] { 3, 2, 0 },
+                new[] { 3, 1, 0 },
+                new[] { 1, 2, 0 }
+            };
+            int[][] friends = {
+                new[] { 0, 1 },
+                new[] { 2, 3 }
+            };
+
+            Assert.AreEqual(2, P1583CountUnhappyFriends.UnhappyFriends(4, input, friends));
+
+            input = new[] {
+                new[] { 1 },
+                new[] { 0 }
+            };
+            friends = new[] {
+                new[] { 1, 0 }
+            };
+
+            Assert.AreEqual(0, P1583CountUnhappyFriends.UnhappyFriends(2, input, friends));
+
+            input = new[] {
+                new[] { 1, 3, 2 },
+                new[] { 2, 3, 0 },
+                new[] { 1, 3, 0 },
+                new[] { 0, 2, 1 }
+            };
+            friends = new[] {
+                new[] { 1, 3 },
+                new[] { 0, 2 }
+            };
+
+            Assert.AreEqual(4, P1583CountUnhappyFriends.UnhappyFriends(4, input, friends));
+
+            input = new[] {
+                new[] { 1, 3, 2 },
+                new[] { 2, 3, 0 },
+                new[] { 1,0,3 },
+                new[] { 1,0,2 }
+            };
+            friends = new[] {
+                new [] { 2, 1 },
+                new [] { 3, 0 }
+            };
+
+            Assert.AreEqual(0, P1583CountUnhappyFriends.UnhappyFriends(4, input, friends));
+        }
+    }
+}
