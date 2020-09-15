e4c74c7e602ff97a157645adf036eacdad490159
Ji <jjeonie@gmail.com>
2020-09-15 19:29:33 +1200

    LeetCode P198 House Robber

---
+++ LeetCode/Src/P198HouseRobber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P198 House Robber
+    ///  href = https://leetcode.com/problems/house-robber/
+    /// </summary>
+    class P198HouseRobber {
+        public static int Rob (int[] nums) {
+            switch (nums.Length) {
+                case 0:
+                    return 0;
+                case 1:
+                    return nums[0];
+            }
+
+            int rob1 = 0;
+            int rob2 = 0;
+
+            int temp;
+            foreach (int n in nums) {
+                // rob1, rob2, n, n+1
+                temp = Math.Max(n + rob1, rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+
+            return rob2;
+        }
+    }
+}
---
+++ LeetCode/Test/P198HouseRobberTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P198HouseRobberTest {
+        [TestMethod]
+        public void RobTest () {
+            Assert.AreEqual(4, P198HouseRobber.Rob(new[] { 1, 2, 3, 1 }));
+            Assert.AreEqual(12, P198HouseRobber.Rob(new[] { 2, 7, 9, 3, 1 }));
+        }
+    }
+}
