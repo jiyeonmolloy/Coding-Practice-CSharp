2f44072be102d86e66c3a9bde5f3bfde365e954d
Ji <jjeonie@gmail.com>
2020-09-15 20:06:15 +1200

    LeetCode P213 House Robber II

---
+++ LeetCode/Src/P213HouseRobberII.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P213 House Robber II
+    ///  href = https://leetcode.com/problems/house-robber-ii/
+    /// </summary>
+    class P213HouseRobberII {
+        public static int Rob (int[] nums) {
+            switch (nums.Length) {
+                case 0:
+                    return 0;
+                case 1:
+                    return nums[0];
+            }
+
+            return Math.Max(FindMaximumMoney(0, nums.Length - 1, nums), FindMaximumMoney(1, nums.Length, nums));
+        }
+
+        private static int FindMaximumMoney (int start, int end, int[] nums) {
+            int rob1 = 0;
+            int rob2 = 0;
+
+            int temp;
+            for (int i = start; i < end; i++) {
+                int n = nums[i];
+                // rob1, rob2, n, n+1
+                temp = Math.Max(n + rob1, rob2);
+                rob1 = rob2;
+                rob2 = temp;
+            }
+
+            return rob2;
+        }
+    }
+}
---
+++ LeetCode/Test/P213HouseRobberIITest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P213HouseRobberIITest {
+        [TestMethod]
+        public void RobTest () {
+            Assert.AreEqual(3, P213HouseRobberII.Rob(new[] { 2, 3, 2 }));
+            Assert.AreEqual(4, P213HouseRobberII.Rob(new[] { 1, 2, 3, 1 }));
+        }
+    }
+}
