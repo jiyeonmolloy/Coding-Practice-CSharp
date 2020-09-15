50baab947b15c10e378c79858e76a4243d1925ce
Ji <jjeonie@gmail.com>
2020-09-15 19:43:18 +1200

    LeetCode P58 Length Of Last Word

---
+++ LeetCode/Src/P58LengthOfLastWord.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P58 Length Of Last Word
+    ///  href = https://leetcode.com/problems/length-of-last-word/
+    /// </summary>
+    class P58LengthOfLastWord {
+        public static int LengthOfLastWord (string s) {
+            if (s.Length == 0) return 0;
+
+            int length = 0;
+            bool foundWord = false;
+            for (int i = s.Length - 1; i >= 0; i--) {
+                if (s[i].Equals(' ')) {
+                    if (!foundWord) {
+                        continue;
+                    }
+                    return length;
+                }
+
+                foundWord = true;
+                length++;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ LeetCode/Test/P58LengthOfLastWordTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P58LengthOfLastWordTest {
+        [TestMethod]
+        public void LengthOfLastWordTest () {
+            Assert.AreEqual(5, P58LengthOfLastWord.LengthOfLastWord("Hello World"));
+            Assert.AreEqual(5, P58LengthOfLastWord.LengthOfLastWord("Donut"));
+            Assert.AreEqual(1, P58LengthOfLastWord.LengthOfLastWord("a"));
+            Assert.AreEqual(1, P58LengthOfLastWord.LengthOfLastWord("b   a    "));
+        }
+    }
+}
