77fd5ee36de2a3e948ea870cad87eb62757da5a5
Ji <jjeonie@gmail.com>
2020-09-21 22:05:56 +1200

    LeetCode P1592 Rearrange Spaces Between Words

---
+++ LeetCode/Src/P1592RearrangeSpacesBetweenWords.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1592 Rearrange Spaces Between Words
+    ///  href = https://leetcode.com/problems/rearrange-spaces-between-words/
+    /// </summary>
+    class P1592RearrangeSpacesBetweenWords {
+        public static string ReorderSpaces (string text) {
+            if (text.Length == 1) {
+                return text;
+            }
+
+            List<string> words = text.Split(' ').Where(x => !String.IsNullOrEmpty(x)).ToList();
+            IEnumerable<char> spaces = text.Where(x => x.Equals(' '));
+            int spaceCount = spaces.Count();
+            string output = String.Empty;
+
+            if (words.Count == 1) {
+                output = words[0];
+                for (int i = 0; i < spaceCount; i++) {
+                    output += ' ';
+                }
+
+                return output;
+            }
+
+            int spacing = spaceCount / (words.Count - 1);
+            int remainderSpace = spaceCount % (words.Count - 1);
+            for (int i = 0; i < words.Count; i++) {
+                string word = words[i];
+                output += word;
+                if (i == words.Count - 1) {
+                    for (int j = 0; j < remainderSpace; j++) {
+                        output += ' ';
+                    }
+                } else {
+                    for (int j = 0; j < spacing; j++) {
+                        output += ' ';
+                    }
+                }
+
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1592RearrangeSpacesBetweenWordsTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1592RearrangeSpacesBetweenWordsTest {
+        [TestMethod]
+        public void ReorderSpacesTest () {
+            Assert.AreEqual("this   is   a   sentence", P1592RearrangeSpacesBetweenWords.ReorderSpaces("  this   is  a sentence "));
+            Assert.AreEqual("practice   makes   perfect ", P1592RearrangeSpacesBetweenWords.ReorderSpaces(" practice   makes   perfect"));
+            Assert.AreEqual("hello   world", P1592RearrangeSpacesBetweenWords.ReorderSpaces("hello   world"));
+            Assert.AreEqual("a", P1592RearrangeSpacesBetweenWords.ReorderSpaces("a"));
+            Assert.AreEqual("a   ", P1592RearrangeSpacesBetweenWords.ReorderSpaces("  a "));
+        }
+    }
+}
