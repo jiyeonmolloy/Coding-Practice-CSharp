4e50aafb36aab2761635066d322cca0bf280f4c8
Ji <jjeonie@gmail.com>
2020-09-24 21:59:31 +1200

    LeetCode P389 Find The Difference

---
+++ LeetCode/Src/P389FindTheDifference.cs
@@ -0,0 +1,22 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P389 Find The Difference
+    ///  href = https://leetcode.com/problems/find-the-difference/
+    /// </summary>
+    class P389FindTheDifference {
+        public static char FindTheDifference (string s, string t) {
+            int difference = 0;
+
+            foreach (char letter in s) {
+                difference ^= letter;
+            }
+
+            foreach (char letter in t) {
+                difference ^= letter;
+            }
+
+            return (char)difference;
+        }
+    }
+}
---
+++ LeetCode/Test/P389FindTheDifferenceTest.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P389FindTheDifferenceTest {
+        [TestMethod]
+        public void FindTheDifferenceTest () {
+            Assert.AreEqual('e', P389FindTheDifference.FindTheDifference("abcd", "abcde"));
+        }
+    }
+}
