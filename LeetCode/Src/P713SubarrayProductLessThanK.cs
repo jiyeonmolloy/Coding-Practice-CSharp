40abab351908ba2400b79e639010dfb1966d263b
Ji <jjeonie@gmail.com>
2020-09-28 22:22:22 +1200

    LeetCode P713 Subarray Product Less Than K

---
+++ LeetCode/Src/P713SubarrayProductLessThanK.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P713 Subarray Product Less Than K
+    ///  href = https://leetcode.com/problems/subarray-product-less-than-k/submissions/
+    ///  Key: Sliding window
+    /// </summary>
+    class P713SubarrayProductLessThanK {
+        public static int NumSubarrayProductLessThanK (int[] nums, int k) {
+            int left = 0;
+            int product = 1;
+            int count = 0;
+
+            for (int right = 0; right < nums.Length; right++) {
+                product *= nums[right];
+
+                while (product >= k && left <= right) {
+                    product /= nums[left];
+                    left++;
+                }
+
+                count += right - left + 1;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode/Test/P713SubarrayProductLessThanKTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P713SubarrayProductLessThanKTest {
+        [TestMethod]
+        public void NumSubarrayProductLessThanKTest () {
+            Assert.AreEqual(8, P713SubarrayProductLessThanK.NumSubarrayProductLessThanK(new[] { 10, 5, 2, 6 }, 100));
+            Assert.AreEqual(0, P713SubarrayProductLessThanK.NumSubarrayProductLessThanK(new[] { 1, 2, 3 }, 0));
+            Assert.AreEqual(0, P713SubarrayProductLessThanK.NumSubarrayProductLessThanK(new[] { 1, 1, 1 }, 1));
+        }
+    }
+}
