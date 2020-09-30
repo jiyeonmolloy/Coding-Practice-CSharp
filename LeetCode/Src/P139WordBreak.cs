9d205d7eef1a6d7fd482645a31f27a45b2e98f4a
Ji <jjeonie@gmail.com>
2020-09-30 22:53:55 +1300

    LeetCode P139 Word Break

---
+++ LeetCode/Src/P139WordBreak.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P139 Word Break
+    ///  href = https://leetcode.com/problems/word-break/
+    /// </summary>
+    class P139WordBreak {
+        public static bool WordBreak (string s, IList<string> wordDict) { // bottom up dp
+            bool[] cache = new bool[s.Length + 1];
+            cache[0] = true;
+
+            string word;
+            for (int i = 1; i <= s.Length; i++) {
+                for (int j = 0; j < i; j++) {
+                    word = s.Substring(j, i - j);
+                    if ((wordDict.Contains(word) && cache[j])) {
+                        cache[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return cache[s.Length];
+        }
+    }
+}
---
+++ LeetCode/Test/P139WordBreakTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P139WordBreakTest {
+        [TestMethod]
+        public void WordBreakTest () {
+            Assert.IsTrue(P139WordBreak.WordBreak("leetcode", new[] { "leet", "code" }));
+            Assert.IsTrue(P139WordBreak.WordBreak("applepenapple", new[] { "apple", "pen" }));
+            Assert.IsFalse(P139WordBreak.WordBreak("catsandog", new[] { "cats", "dog", "sand", "and", "cat" }));
+        }
+    }
+}
