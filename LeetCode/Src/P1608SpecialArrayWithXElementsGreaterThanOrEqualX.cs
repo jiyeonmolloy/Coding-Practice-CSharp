bb767d5123d711b9084b8ca979d8b7e6517f92e5
Ji <jjeonie@gmail.com>
2020-10-08 20:43:18 +1300

    LeetCode P1608 Special Array With X Elements Greater Than Or Equal X

---
+++ LeetCode/Src/P1608SpecialArrayWithXElementsGreaterThanOrEqualX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1608 Special Array With X Elements Greater Than Or Equal X
+    ///  href = https://leetcode.com/problems/special-array-with-x-elements-greater-than-or-equal-x/
+    /// </summary>
+    class P1608SpecialArrayWithXElementsGreaterThanOrEqualX {
+        public static int SpecialArray (int[] nums) {
+            Array.Sort(nums, (a, b) => b.CompareTo(a));
+
+            int special = 0;
+            while (special < nums.Length && nums[special] > special) {
+                special += 1;
+            }
+
+            if (special < nums.Length && nums[special] == special) {
+                return -1;
+            }
+
+            return special;
+        }
+    }
+}
---
+++ LeetCode/Test/P1608SpecialArrayWithXElementsGreaterThanOrEqualXTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1608SpecialArrayWithXElementsGreaterThanOrEqualXTest {
+        [TestMethod]
+        public void SpecialArrayTest () {
+            Assert.AreEqual(3, P1608SpecialArrayWithXElementsGreaterThanOrEqualX.SpecialArray(new int[5] { 0, 4, 3, 0, 4 }));
+            Assert.AreEqual(2, P1608SpecialArrayWithXElementsGreaterThanOrEqualX.SpecialArray(new int[2] { 3, 5 }));
+            Assert.AreEqual(-1, P1608SpecialArrayWithXElementsGreaterThanOrEqualX.SpecialArray(new int[2] { 0, 0 }));
+        }
+    }
+}
