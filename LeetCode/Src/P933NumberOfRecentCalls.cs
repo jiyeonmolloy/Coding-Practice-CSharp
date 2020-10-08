956975623a2496676dbde9ff2978c77b8ad108f8
Ji <jjeonie@gmail.com>
2020-10-08 19:48:30 +1300

    LeetCode P933 Number Of Recent Call

---
+++ LeetCode/Src/P933NumberOfRecentCalls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P933 Number Of Recent Call
+    ///  href = https://leetcode.com/problems/number-of-recent-calls/
+    /// </summary>
+    class P933NumberOfRecentCalls {
+        private List<int> history;
+
+        public P933NumberOfRecentCalls () {
+            history = new List<int>();
+        }
+
+        public int Ping (int t) {
+            history.Add(t);
+            history = history.Where(x => x >= t - 3000).ToList();
+            return history.Count;
+        }
+    }
+}
---
+++ LeetCode/Test/P933NumberOfRecentCallsTest.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P933NumberOfRecentCallsTest {
+        [TestMethod]
+        public void PingTest () {
+            P933NumberOfRecentCalls recentCounter = new P933NumberOfRecentCalls();
+            Assert.AreEqual(1, recentCounter.Ping(1));
+            Assert.AreEqual(2, recentCounter.Ping(100));
+            Assert.AreEqual(3, recentCounter.Ping(3001));
+            Assert.AreEqual(3, recentCounter.Ping(3002));
+
+            recentCounter = new P933NumberOfRecentCalls();
+            Assert.AreEqual(1, recentCounter.Ping(642));
+            Assert.AreEqual(2, recentCounter.Ping(1849));
+            Assert.AreEqual(1, recentCounter.Ping(4921));
+        }
+    }
+}
