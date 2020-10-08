0353013f101a2598f6a5d0420d10741996189d17
Ji <jjeonie@gmail.com>
2020-10-08 20:39:57 +1300

    LeetCode P1009 Complement Of Base 10 Integer

---
+++ LeetCode/Src/P1009ComplementOfBase10Integer.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1009 Complement Of Base 10 Integer
+    /// Same question as P476 Number Complement
+    ///  href = https://leetcode.com/problems/complement-of-base-10-integer/
+    /// </summary>
+    class P1009ComplementOfBase10Integer {
+        public static int BitwiseComplement (int N) {
+            if (N == 0) {
+                return 1;
+            }
+
+            int complement = 0;
+            int factor = 1;
+
+            while (N > 0) {
+                complement += factor * (N % 2 == 0 ? 1 : 0);
+                N /= 2;
+                factor *= 2;
+            }
+
+            return complement;
+        }
+    }
+}
---
+++ LeetCode/Test/P1009ComplementOfBase10IntegerTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1009ComplementOfBase10IntegerTest {
+        [TestMethod]
+        public void BitwiseComplementTest () {
+            Assert.AreEqual(2, P1009ComplementOfBase10Integer.BitwiseComplement(5)); 
+            Assert.AreEqual(0, P1009ComplementOfBase10Integer.BitwiseComplement(7));
+            Assert.AreEqual(5, P1009ComplementOfBase10Integer.BitwiseComplement(10));
+        }
+    }
+}
