5455fb77fe765579b94368c009afe8b3f51e7a9c
Ji <jjeonie@gmail.com>
2020-10-11 19:42:47 +1300

    LeetCode Weekly Contest 210 P1614 Maximum Nesting Depth Of The Parentheses

---
+++ LeetCode/Src/P1614MaximumNestingDepthOfTheParentheses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1614 Maximum Nesting Depth Of The Parentheses
+    ///  href = https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+    /// </summary>
+    class P1614MaximumNestingDepthOfTheParentheses {
+        public static int MaxDepth (string s) {
+            int max = 0;
+            int current = 0;
+            for (int i = 0; i < s.Length; i++) {
+                if (s[i].Equals('(')) {
+                    current++;
+                } else if (s[i].Equals(')')) {
+                    current--;
+                }
+
+                max = Math.Max(current, max);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Test/P1614MaximumNestingDepthOfTheParenthesesTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1614MaximumNestingDepthOfTheParenthesesTest {
+        [TestMethod]
+        public void MaxDepthTest () {
+            Assert.AreEqual(3, P1614MaximumNestingDepthOfTheParentheses.MaxDepth("(1+(2*3)+((8)/4))+1"));
+            Assert.AreEqual(3, P1614MaximumNestingDepthOfTheParentheses.MaxDepth("(1)+((2))+(((3)))"));
+            Assert.AreEqual(1, P1614MaximumNestingDepthOfTheParentheses.MaxDepth("1+(2*3)/(2-1)"));
+            Assert.AreEqual(0, P1614MaximumNestingDepthOfTheParentheses.MaxDepth("1"));
+        }
+    }
+}
