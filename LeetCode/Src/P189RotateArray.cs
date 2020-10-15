4a45742e3260814ed1abe763f12d56c19e8d725f
Ji <jjeonie@gmail.com>
2020-10-15 20:32:37 +1300

    LeetCode P189 Rotate Array

---
+++ LeetCode/Src/P189RotateArray.cs
@@ -0,0 +1,29 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P189 Rotate Array
+    ///  href = https://leetcode.com/problems/rotate-array/
+    /// </summary>
+    class P189RotateArray {
+        public static int[] Rotate (int[] nums, int k) {
+            k %= nums.Length;
+
+            Reverse(nums, 0, nums.Length - 1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+
+            return nums;
+        }
+
+        private static void Reverse (int[] nums, int start, int end) {
+            int temp;
+            while (start < end) {
+                temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/P189RotateArrayTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P189RotateArrayTest {
+        [TestMethod]
+        public void RotateTest () {
+            CollectionAssert.AreEqual(new[] { 5, 6, 7, 1, 2, 3, 4 }, P189RotateArray.Rotate(new[] { 1, 2, 3, 4, 5, 6, 7 }, 3));
+            CollectionAssert.AreEqual(new[] { 3, 99, -1, -100 }, P189RotateArray.Rotate(new[] { -1, -100, 3, 99 }, 2));
+        }
+    }
+}
