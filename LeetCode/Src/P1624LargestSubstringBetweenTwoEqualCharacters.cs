89461b532245873b15905a199a44ab74124cbf1a
Ji <jjeonie@gmail.com>
2020-10-18 22:25:29 +1300

    LeetCode P1624 Largest Substring Between Two Equal Characters

---
+++ LeetCode/Src/P1624LargestSubstringBetweenTwoEqualCharacters.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P1624 Largest Substring Between Two Equal Characters
+    ///  href = https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+    /// </summary>
+    class P1624LargestSubstringBetweenTwoEqualCharacters {
+        public static int MaxLengthBetweenEqualCharacters (string s) {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            char target;
+
+            int max = -1;
+            int distance = 0;
+            for (int i = 0; i < s.Length; i++) {
+                target = s[i];
+                if (dict.ContainsKey(target)) {
+                    distance = i - dict[target] - 1;
+                    if (distance > max) {
+                        max = distance;
+                    }
+                } else {
+                    dict.Add(target, i); // start index
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Test/P1624LargestSubstringBetweenTwoEqualCharactersTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1624LargestSubstringBetweenTwoEqualCharactersTest {
+        [TestMethod]
+        public void MaxLengthBetweenEqualCharactersTest () {
+            Assert.AreEqual(4, P1624LargestSubstringBetweenTwoEqualCharacters.MaxLengthBetweenEqualCharacters("cabbac"));
+            Assert.AreEqual(0, P1624LargestSubstringBetweenTwoEqualCharacters.MaxLengthBetweenEqualCharacters("aa"));
+            Assert.AreEqual(2, P1624LargestSubstringBetweenTwoEqualCharacters.MaxLengthBetweenEqualCharacters("abca"));
+            Assert.AreEqual(-1, P1624LargestSubstringBetweenTwoEqualCharacters.MaxLengthBetweenEqualCharacters("cbzxy"));
+        }
+    }
+}
