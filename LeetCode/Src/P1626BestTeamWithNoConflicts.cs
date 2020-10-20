10c4896b9240cb45611048afa3118874833dbd69
Ji <jjeonie@gmail.com>
2020-10-20 20:20:08 +1300

    LeetCode Weekly Contest 211 P1626 Best Team With No Conflicts

---
+++ LeetCode/Src/P1626BestTeamWithNoConflicts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1626 Best Team With No Conflicts
+    ///  href = https://leetcode.com/problems/best-team-with-no-conflicts/
+    ///  Key: Top-down DP
+    /// </summary>
+    class P1626BestTeamWithNoConflicts {
+        public static int BestTeamScore (int[] scores, int[] ages) {
+            int size = scores.Length;
+
+            scores = Enumerable.Range(0, scores.Length)
+                .Select(t => (t, score: scores[t], age: ages[t]))
+                .OrderByDescending(p => p.age)
+                .ThenByDescending(p => p.score)
+                .Select(p => p.score)
+                .ToArray();
+
+            int max;
+            int[] dp = new int[size];
+            for (int i = 0; i < size; i++) {
+                max = scores[i];
+                for (int j = i - 1; j >= 0; j--) {
+                    if (scores[j] >= scores[i]) {
+                        max = Math.Max(max, dp[j] + scores[i]);
+                    }
+                }
+
+                dp[i] = max;
+            }
+
+            return dp.Max();
+        }
+    }
+}
---
+++ LeetCode/Test/P1626BestTeamWithNoConflictsTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1626BestTeamWithNoConflictsTest {
+        [TestMethod]
+        public void BestTeamScoreTest () {
+            Assert.AreEqual(16, P1626BestTeamWithNoConflicts.BestTeamScore(new int[] { 4, 5, 6, 5 }, new int[] { 2, 1, 2, 1 }));
+            Assert.AreEqual(34, P1626BestTeamWithNoConflicts.BestTeamScore(new int[] { 1, 3, 5, 10, 15 }, new int[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(6, P1626BestTeamWithNoConflicts.BestTeamScore(new int[] { 1, 2, 3, 5 }, new int[] { 8, 9, 10, 1 }));
+        }
+    }
+}
