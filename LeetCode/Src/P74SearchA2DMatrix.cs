8332e522b6ec60a8adea13dafafaf417ca08b6ef
Ji <jjeonie@gmail.com>
2020-10-22 20:33:14 +1300

    LeetCode P74 Search A 2D Matrix

---
+++ LeetCode/Src/P74SearchA2DMatrix.cs
@@ -0,0 +1,42 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P74 Search A 2D Matrix
+    ///  href = https://leetcode.com/problems/search-a-2d-matrix/
+    ///  Key: Binary Search
+    /// </summary>
+    class P74SearchA2DMatrix {
+        public static bool SearchMatrix (int[][] matrix, int target) {
+            if (matrix.Length == 0 || matrix[0].Length == 0) {
+                return false;
+            }
+
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+
+            int left, right;
+            for (int row = 0; row < rows; row++) {
+                left = 0;
+                right = cols - 1;
+
+                if (target < matrix[row][left] || target > matrix[row][right]) continue;
+                int mid;
+                int current;
+                while (left <= right) {
+                    mid = left + ((right - left) / 2);
+                    current = matrix[row][mid];
+
+                    if (current == target) {
+                        return true;
+                    } else if (target < current) {
+                        right = mid - 1;
+                    } else if (target > current) {
+                        left = mid + 1;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P74SearchA2DMatrixTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P74SearchA2DMatrixTest {
+        [TestMethod]
+        public void SearchMatrixTest () {
+            Assert.IsTrue(P74SearchA2DMatrix.SearchMatrix(new int[][] { new[] { 1, 3, 5, 7 }, new[] { 10, 11, 16, 20 }, new[] { 23, 30, 34, 50 } }, 3));
+            Assert.IsFalse(P74SearchA2DMatrix.SearchMatrix(new int[][] { new[] { 1, 3, 5, 7 }, new[] { 10, 11, 16, 20 }, new[] { 23, 30, 34, 50 } }, 13));
+            Assert.IsFalse(P74SearchA2DMatrix.SearchMatrix(new int[][] { }, 0));
+            Assert.IsTrue(P74SearchA2DMatrix.SearchMatrix(new int[][] { new[] { 1 } }, 1));
+        }
+    }
+}
