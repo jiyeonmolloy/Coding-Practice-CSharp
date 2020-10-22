eb86b455454667c323e876e6289e91973e14193e
Ji <jjeonie@gmail.com>
2020-10-22 20:54:55 +1300

    LeetCode P111 Minimum Depth Of Binary Tree

---
+++ LeetCode/Src/P111MinimumDepthOfBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P111 Minimum Depth Of Binary Tree
+    ///  href = https://leetcode.com/problems/minimum-depth-of-binary-tree/
+    /// </summary>
+    class P111MinimumDepthOfBinaryTree : TreeNode {
+        public static int MinDepth (TreeNode root) {
+            if (root == null) {
+                return 0;
+            } else if (root.left == null && root.right == null) {
+                return 1;
+            } else if (root.left == null) {
+                return MinDepth(root.right) + 1;
+            } else if (root.right == null) {
+                return MinDepth(root.left) + 1;
+            }
+
+            return Math.Min(MinDepth(root.left), MinDepth(root.right)) + 1;
+        }
+    }
+}
---
+++ LeetCode/Test/P111MinimumDepthOfBinaryTreeTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P111MinimumDepthOfBinaryTreeTest {
+        [TestMethod]
+        public void SumRootToLeafTest () {
+            Assert.AreEqual(2, P111MinimumDepthOfBinaryTree.MinDepth(new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+            Assert.AreEqual(5, P111MinimumDepthOfBinaryTree.MinDepth(new TreeNode(2,
+                null,
+                new TreeNode(3,
+                    null,
+                    new TreeNode(4,
+                        null,
+                        new TreeNode(5,
+                            null,
+                            new TreeNode(6)))))));
+        }
+    }
+}
