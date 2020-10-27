ae868d8e2dbd692c6599a0c9e39092067d6386e9
Ji <jjeonie@gmail.com>
2020-10-27 22:10:35 +1300

    LeetCode Weekly Contest 212 P1629 Slowest Key

---
+++ LeetCode/Src/P1629SlowestKey.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1629 Slowest Key
+    ///  href = https://leetcode.com/problems/slowest-key/
+    class P1629SlowestKey {
+        public static char SlowestKey (int[] releaseTimes, string keysPressed) {
+            int max = releaseTimes[0];
+            char key = keysPressed[0];
+
+            int length;
+            for (int i = 1; i < releaseTimes.Length; i++) {
+                length = releaseTimes[i] - releaseTimes[i - 1];
+                if (length > max || (length == max && (keysPressed[i] - 'a') > (key - 'a'))) {
+                    max = length;
+                    key = keysPressed[i];
+                }
+            }
+
+            return key;
+        }
+    }
+}
---
+++ LeetCode/Test/P1629SlowestKeyTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1629SlowestKeyTest {
+        [TestMethod]
+        public void SearchTest () {
+            Assert.AreEqual('c', P1629SlowestKey.SlowestKey(new[] { 9, 29, 49, 50 }, "cbcd"));
+            Assert.AreEqual('a', P1629SlowestKey.SlowestKey(new[] { 12, 23, 36, 46, 62 }, "spuda"));
+        }
+    }
+}
