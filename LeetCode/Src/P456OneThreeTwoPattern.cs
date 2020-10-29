3ec6d5e14cba466abba9fdfbde9a87ddb9677dd1
Ji <jjeonie@gmail.com>
2020-10-29 22:28:27 +1300

    LeetCode P456 132 Pattern

---
+++ LeetCode/Src/P456OneThreeTwoPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P456 132 Pattern
+    ///  href = https://leetcode.com/problems/132-pattern/
+    /// </summary>
+    class P456OneThreeTwoPattern {
+        public static bool Find132pattern (int[] nums) {
+            int n = nums.Length;
+            int min = nums[0];
+
+            for (int j = 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (min < nums[k] && nums[k] < nums[j]) {
+                        return true;
+                    }
+                }
+
+                min = Math.Min(nums[j], min);
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/Test/P456OneThreeTwoPatternTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P456OneThreeTwoPatternTest {
+        [TestMethod]
+        public void Find132patternTest () {
+            Assert.IsFalse(P456OneThreeTwoPattern.Find132pattern(new[] { 1, 2, 3, 4 }));
+            Assert.IsTrue(P456OneThreeTwoPattern.Find132pattern(new[] { 3, 1, 4, 2 }));
+            Assert.IsTrue(P456OneThreeTwoPattern.Find132pattern(new[] { -1, 3, 2, 0 }));
+            Assert.IsTrue(P456OneThreeTwoPattern.Find132pattern(new[] { 3, 5, 0, 3, 4 }));
+        }
+    }
+}
