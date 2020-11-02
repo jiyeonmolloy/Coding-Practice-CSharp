3f1bf9e7ae4e33c22b625ea16d9cc78113ca783d
Ji <jjeonie@gmail.com>
2020-11-02 22:24:31 +1300

    LeetCode P1290 Convert Binary Number In A Linked List To Integer

---
+++ LeetCode/Src/P1290ConvertBinaryNumberInALinkedListToInteger.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1290 Convert Binary Number In A Linked List To Integer
+    ///  href = https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+    /// </summary>
+    class P1290ConvertBinaryNumberInALinkedListToInteger : ListNode{
+        public static int GetDecimalValue (ListNode head) {
+            int output = 0;
+            while (head != null) {
+                output = output << 1 | head.val;
+                head = head.next;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ LeetCode/Test/P1290ConvertBinaryNumberInALinkedListToIntegerTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1290ConvertBinaryNumberInALinkedListToIntegerTest : ListNode {
+        [TestMethod]
+        public void ClimbingChairsTest () {
+            Assert.AreEqual(5, P1290ConvertBinaryNumberInALinkedListToInteger.GetDecimalValue(new ListNode(1, new ListNode(0, new ListNode(1)))));
+            Assert.AreEqual(0, P1290ConvertBinaryNumberInALinkedListToInteger.GetDecimalValue(new ListNode(0, new ListNode(0))));
+        }
+    }
+}
