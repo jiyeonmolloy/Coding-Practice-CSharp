542dc4fc7aff941c6a4e7fc122d7fd431e0d3ba8
Ji <jjeonie@gmail.com>
2020-11-03 20:39:28 +1300

    LeetCode P147 Insertion Sort List

---
+++ LeetCode/Src/P147InsertionSortList.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P147 Insertion Sort List
+    ///  href = https://leetcode.com/problems/insertion-sort-list/
+    /// </summary>
+    class P147InsertionSortList : ListNode{
+        public static ListNode InsertionSortList (ListNode head) {
+            ListNode sorted = new ListNode();
+            ListNode current = head;
+            ListNode previous;
+            ListNode next;
+            ListNode nextRound;
+
+            while (current != null) {
+                previous = sorted;
+                next = sorted.next;
+
+                while (next != null) {
+                    if (current.val < next.val) {
+                        break;
+                    }
+                    previous = next;
+                    next = next.next;
+                }
+
+                nextRound = current.next;
+                current.next = next;
+                previous.next = current;
+                current = nextRound;
+            }
+
+            return sorted.next;
+        }
+    }
+}
---
+++ LeetCode/Test/P147InsertionSortListTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P147InsertionSortListTest {
+        [TestMethod]
+        public void InsertionSortListTest () {
+            ListNode expectedList = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+            Assert.IsTrue(expectedList.AreEqual(expectedList, P147InsertionSortList.InsertionSortList(new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3)))))));
+
+            expectedList = new ListNode(-1, new ListNode(0, new ListNode(3, new ListNode(4, new ListNode(5)))));
+            Assert.IsTrue(expectedList.AreEqual(expectedList, P147InsertionSortList.InsertionSortList(new ListNode(-1, new ListNode(0, new ListNode(3, new ListNode(4, new ListNode(5))))))));
+        }
+    }
+}
