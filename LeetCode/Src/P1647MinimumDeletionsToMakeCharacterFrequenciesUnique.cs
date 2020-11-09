0c504687770a45a8704e14a9ee889212757d9d87
Ji <jjeonie@gmail.com>
2020-11-09 22:13:31 +1300

    LeetCode Weekly Contest 214 P1647 Minimum Deletions To Make Character Frequencies Unique

---
+++ LeetCode/Src/P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1647 Minimum Deletions To Make Character Frequencies Unique
+    ///  href = https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+    /// </summary>
+    public class CustomNode {
+        public int Freq;
+        public int Value;
+
+        public CustomNode (int value, int freq) {
+            Value = value;
+            Freq = freq;
+        }
+    }
+
+    class P1647MinimumDeletionsToMakeCharacterFrequenciesUnique {
+        public static int MinDeletions (string s) {
+            int[] frequency = new int[26];
+
+            foreach (char character in s) {
+                frequency[character - 'a']++;
+            }
+
+            SortedDictionary<int, HashSet<int>> setMultiMap = new SortedDictionary<int, HashSet<int>>(); // hashset
+
+            int freq = 0;
+            int target;
+            for (int i = 0; i < frequency.Length; i++) {
+                freq = frequency[i];
+                if (freq == 0) {
+                    continue;
+                }
+                target = i;
+                if (setMultiMap.ContainsKey(freq)) {
+                    setMultiMap[freq].Add(target);
+                } else {
+                    setMultiMap[freq] = new HashSet<int>();
+                    setMultiMap[freq].Add(target);
+                }
+            }
+
+            Queue<CustomNode> queue = new Queue<CustomNode>();
+            int currentCharacter;
+            int moveFreq = 0;
+            for (int i = setMultiMap.Keys.Max(); i > 0; i--) {
+                if (setMultiMap.ContainsKey(i) && setMultiMap[i].Count > 1) {
+                    bool start = true;
+                    foreach (int charNumber in setMultiMap[i]) {
+                        if (start) {
+                            start = false;
+                            continue;
+                        }
+                        currentCharacter = charNumber;
+                        queue.Enqueue(new CustomNode(currentCharacter, i));
+                    }
+                    continue;
+                }
+
+                // De queue one node
+                if (queue.Count > 0 && (!setMultiMap.ContainsKey(i) || setMultiMap.ContainsKey(i) && setMultiMap[i].Count != 1)) {
+                    CustomNode node = queue.Dequeue();
+                    moveFreq += node.Freq - i;
+                }
+            }
+
+            while (queue.Count > 0) {
+                CustomNode unload = queue.Dequeue();
+                moveFreq += unload.Freq;
+            }
+
+            return moveFreq;
+        }
+    }
+}
---
+++ LeetCode/Test/P1647MinimumDeletionsToMakeCharacterFrequenciesUniqueTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1647MinimumDeletionsToMakeCharacterFrequenciesUniqueTest {
+        [TestMethod]
+        public void MinDeletionsTest () {
+            Assert.AreEqual(2, P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.MinDeletions("aaabbbcc")); // 3: a, b ; 2: c
+            Assert.AreEqual(0, P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.MinDeletions("a"));
+            Assert.AreEqual(1, P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.MinDeletions("ab"));
+            Assert.AreEqual(0, P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.MinDeletions("aab"));
+            Assert.AreEqual(2, P1647MinimumDeletionsToMakeCharacterFrequenciesUnique.MinDeletions("ceabaacb"));
+        }
+    }
+}
