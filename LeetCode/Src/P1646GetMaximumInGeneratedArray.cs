9a72f532359b5be7d7fc6dc78a77b689eda34e44
Ji <jjeonie@gmail.com>
2020-11-09 22:08:55 +1300

    LeetCode Weekly Contest 214 P1646 Get Maximum In Generated Array

---
+++ LeetCode/Src/P1646GetMaximumInGeneratedArray.cs
@@ -0,0 +1,43 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1646 Get Maximum In Generated Array
+    ///  href = https://leetcode.com/problems/get-maximum-in-generated-array/
+    /// </summary>
+    class P1646GetMaximumInGeneratedArray {
+        public static int GetMaximumGenerated (int n) {
+            if (n == 0) {
+                return 0;
+            } else if (n == 1) {
+                return 1;
+            }
+
+            int max = 1;
+            int index = 1;
+            int current = 0;
+            int[] nums = new int[n + 1];
+            nums[0] = 0;
+            nums[1] = 1;
+            while (current <= n) {
+                current = 2 * index;
+                if (current <= n) {
+                    nums[current] = nums[index];
+                    if (nums[current] > max) {
+                        max = nums[current];
+                    }
+                }
+
+                current = 2 * index + 1;
+                if (current <= n) {
+                    nums[current] = nums[index] + nums[index + 1];
+                    if (nums[current] > max) {
+                        max = nums[current];
+                    }
+                }
+                index++;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Test/P1646GetMaximumInGeneratedArrayTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1646GetMaximumInGeneratedArrayTest {
+        [TestMethod]
+        public void GetMaximumGeneratedTest () {
+            Assert.AreEqual(3, P1646GetMaximumInGeneratedArray.GetMaximumGenerated(7));
+            Assert.AreEqual(1, P1646GetMaximumInGeneratedArray.GetMaximumGenerated(2));
+            Assert.AreEqual(2, P1646GetMaximumInGeneratedArray.GetMaximumGenerated(3));
+        }
+    }
+}
