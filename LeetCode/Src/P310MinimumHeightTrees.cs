fe4e6d59b871ba08b454a0b1692570fb30d324dc
Ji <jjeonie@gmail.com>
2020-11-13 19:52:14 +1300

    LeetCode P310 Minimum Height Trees

---
+++ LeetCode/Src/P310MinimumHeightTrees.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P310 Minimum Height Trees
+    ///  href = https://leetcode.com/problems/minimum-height-trees/
+    /// </summary>
+    class P310MinimumHeightTrees {
+        public static IList<int> FindMinHeightTrees (int n, int[][] edges) {
+            if (n < 2) {
+                List<int> centroids = new List<int>();
+                for (int i = 0; i < n; i++) {
+                    centroids.Add(i);
+                }
+                return centroids;
+            }
+
+            List<HashSet<int>> neighbours = new List<HashSet<int>>();
+            for (int i = 0; i < n; i++) {
+                neighbours.Add(new HashSet<int>());
+            }
+
+            int start, end;
+            foreach (int[] edge in edges) {
+                start = edge[0];
+                end = edge[1];
+                neighbours[start].Add(end);
+                neighbours[end].Add(start);
+            }
+
+            List<int> leaves = new List<int>();
+            for (int i = 0; i < n; i++) {
+                if (neighbours[i].Count == 1) {
+                    leaves.Add(i);
+                }
+            }
+
+            int remainingNodes = n;
+            List<int> newLeaves;
+            while (remainingNodes > 2) {
+                remainingNodes -= leaves.Count;
+                newLeaves = new List<int>();
+
+                foreach (int leaf in leaves) {
+                    foreach (int neighbour in neighbours[leaf]) {
+                        neighbours[neighbour].Remove(leaf);
+                        if (neighbours[neighbour].Count == 1) {
+                            newLeaves.Add(neighbour);
+                        }
+                    }
+                }
+
+                leaves = newLeaves;
+            }
+
+            return leaves;
+        }
+    }
+}
---
+++ LeetCode/Test/P310MinimumHeightTreesTest.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P310MinimumHeightTreesTest {
+        [TestMethod]
+        public void FindMinHeightTreesTest () {
+            IList<int> roots = new List<int>() { 1 };
+            Assert.IsTrue(roots.SequenceEqual(P310MinimumHeightTrees.FindMinHeightTrees(4, new[] { new[] { 1, 0 }, new[] { 1, 2 }, new[] { 1, 3 } })));
+
+            roots = new List<int>() { 3, 4 };
+            Assert.IsTrue(roots.SequenceEqual(P310MinimumHeightTrees.FindMinHeightTrees(6, new[] { new[] { 3, 0 }, new[] { 3, 1 }, new[] { 3, 2 }, new[] { 3, 4 }, new[] { 5, 4 } })));
+
+            roots = new List<int>() { 0 };
+            Assert.IsTrue(roots.SequenceEqual(P310MinimumHeightTrees.FindMinHeightTrees(1, new int[][] { })));
+
+            roots = new List<int>() { 0, 1 };
+            Assert.IsTrue(roots.SequenceEqual(P310MinimumHeightTrees.FindMinHeightTrees(2, new[] { new[] { 0, 1 } })));
+        }
+    }
+}
