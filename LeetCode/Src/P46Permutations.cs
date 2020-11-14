daa33f5c9a4a76e14f1b458147f25f8d6ddd90c4
Ji <jjeonie@gmail.com>
2020-11-14 22:31:20 +1300

    LeetCode P46 Permutations

---
+++ LeetCode/Src/P46Permutations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P46 Permutations
+    ///  href = https://leetcode.com/problems/permutations/
+    /// </summary>
+    class P46Permutations {
+        public static IList<IList<int>> Permute (int[] nums) {
+            int length = nums.Length;
+            List<IList<int>> permutations = new List<IList<int>>();
+
+            if (length == 0) {
+                return permutations;
+            }
+
+            Dfs(nums, length, new bool[length], new List<int>(), permutations);
+
+            return permutations;
+        }
+
+        private static void Dfs (int[] nums, int length, bool[] visited, IList<int> singlePermutation, IList<IList<int>> permutations) {
+            if (singlePermutation.Count == length) {
+                permutations.Add(new List<int>(singlePermutation));
+            } else {
+                for (int i = 0; i < length; i++) {
+                    if (visited[i]) continue;
+
+                    singlePermutation.Add(nums[i]);
+                    visited[i] = true;
+                    Dfs(nums, length, visited, singlePermutation, permutations);
+                    visited[i] = false;
+                    singlePermutation.RemoveAt(singlePermutation.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/Test/P46PermutationsTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P46PermutationsTest {
+        [TestMethod]
+        public void PermuteTest () {
+            IList<IList<int>> permutations = new List<IList<int>>() {
+                new List<int>() {1, 2, 3},
+                new List<int>() {1, 3, 2},
+                new List<int>() {2, 1, 3},
+                new List<int>() {2, 3, 1},
+                new List<int>() {3, 1, 2},
+                new List<int>() {3, 2, 1},
+            };
+
+            permutations.Should().BeEquivalentTo(P46Permutations.Permute(new[] { 1, 2, 3 }));
+        }
+    }
+}
