d48d20eebf564f42956ea1415ce4512320d59aa9
Ji <jjeonie@gmail.com>
2020-11-16 22:27:31 +1300

    LeetCode P938 Range Sum Of BST

---
+++ LeetCode/Src/P938RangeSumOfBST.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P938 Range Sum Of BST
+    ///  href = https://leetcode.com/problems/range-sum-of-bst/
+    /// </summary>
+    class P938RangeSumOfBST : TreeNode {
+        private int sum = 0;
+        public int RangeSumBSTDfs (TreeNode root, int low, int high) {
+            Dfs(root, low, high);
+            return sum;
+        }
+
+        private void Dfs (TreeNode root, int low, int high) {
+            if (root == null) return;
+
+            int current = root.val;
+            if (current >= low && current <= high) {
+                sum += current;
+            }
+
+            if (current > low) {
+                Dfs(root.left, low, high);
+            }
+
+            if (current < high) {
+                Dfs(root.right, low, high);
+            }
+        }
+
+        public int RangeSumBSTIterative (TreeNode root, int low, int high) {
+            int sum = 0;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            TreeNode current;
+            int currentValue;
+            while (stack.Count > 0) {
+                current = stack.Pop();
+                currentValue = current.val;
+
+                if (currentValue >= low && currentValue <= high) {
+                    sum += currentValue;
+                }
+
+                if (currentValue > low && current.left != null) {
+                    stack.Push(current.left);
+                }
+
+                if (currentValue < high && current.right != null) {
+                    stack.Push(current.right);
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/Test/P938RangeSumOfBSTTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Src;
+using LeetCode.Src.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P938RangeSumOfBSTTest {
+        [TestMethod]
+        public void RangeSumBSTTest () {
+            P938RangeSumOfBST rangeSum = new P938RangeSumOfBST();
+            Assert.AreEqual(32, rangeSum.RangeSumBSTDfs(new TreeNode(10,
+                new TreeNode(5, new TreeNode(3), new TreeNode(7)),
+                new TreeNode(15, null, new TreeNode(18))), 7, 15));
+
+            Assert.AreEqual(32, rangeSum.RangeSumBSTIterative(new TreeNode(10,
+                new TreeNode(5, new TreeNode(3), new TreeNode(7)),
+                new TreeNode(15, null, new TreeNode(18))), 7, 15));
+        }
+    }
+}
