1051d2d740e28f974aa5f7391dde651ecfced3ee
Ji <jjeonie@gmail.com>
2020-11-17 22:41:36 +1300

    LeetCode P845 Longest Mountain In Array

---
+++ LeetCode/Src/P845LongestMountainInArray.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P845 Longest Mountain In Array
+    ///  href = https://leetcode.com/problems/longest-mountain-in-array/
+    /// </summary>
+    class P845LongestMountainInArray {
+        public static int LongestMountain (int[] A) {
+            if (A.Length < 3) {
+                return 0;
+            }
+
+            bool peak = false;
+            bool climb = false;
+            int count = 0;
+            int max = 0;
+            int previousPoint, currentPoint;
+            for (int i = 1; i < A.Length; i++) {
+                previousPoint = A[i - 1];
+                currentPoint = A[i];
+
+                if (climb) {
+                    if (currentPoint > previousPoint && !peak) {
+                        count++;
+                        continue;
+                    }
+
+                    if (currentPoint < previousPoint) {
+                        peak = true;
+                        count++;
+                        continue;
+                    }
+
+                    count++;
+
+                    if (peak) {
+                        max = Math.Max(max, count);
+                    }
+
+                    if (currentPoint > previousPoint) {
+                        count = 1;
+                    } else {
+                        count = 0;
+                        climb = false;
+                    }
+                    peak = false;
+                    continue;
+                }
+
+                if (currentPoint <= previousPoint) continue;
+
+                count = 1;
+                climb = true;
+            }
+
+            return peak ? Math.Max(max, count + 1) : max;
+        }
+    }
+}
---
+++ LeetCode/Test/P845LongestMountainInArrayTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P845LongestMountainInArrayTest {
+        [TestMethod]
+        public void LongestMountainTest () {
+            Assert.AreEqual(5, P845LongestMountainInArray.LongestMountain(new[] { 2, 1, 4, 7, 3, 2, 5 }));
+            Assert.AreEqual(7, P845LongestMountainInArray.LongestMountain(new[] { 1, 2, 3, 4, 3, 2, 1, 2, 3, 2, 1 }));
+            Assert.AreEqual(11, P845LongestMountainInArray.LongestMountain(new[] { 0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0 }));
+            Assert.AreEqual(4, P845LongestMountainInArray.LongestMountain(new[] { 875, 884, 239, 731, 723, 685 }));
+            Assert.AreEqual(0, P845LongestMountainInArray.LongestMountain(new[] { 2, 2, 2 }));
+            Assert.AreEqual(0, P845LongestMountainInArray.LongestMountain(new[] { 2, 1, 4, 7 }));
+        }
+    }
+}
