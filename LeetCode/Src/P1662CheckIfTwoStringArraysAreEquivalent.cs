e15af61df8b8175a808567fae4d747a976c39822
Ji <jjeonie@gmail.com>
2020-11-24 20:04:53 +1300

    LeetCode Weekly Contest 216 P1662 Check If Two String Arrays Are Equivalent

---
+++ LeetCode/Src/P1662CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,15 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P1662 Check If Two String Arrays Are Equivalent
+    ///  href = https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+    /// </summary>
+    class P1662CheckIfTwoStringArraysAreEquivalent {
+        public static bool ArrayStringsAreEqual (string[] word1, string[] word2) {
+            string word1C = string.Join("", word1);
+            string word2C = string.Join("", word2);
+
+            return word1C.Equals(word2C);
+        }
+    }
+}
---
+++ LeetCode/Test/P1662CheckIfTwoStringArraysAreEquivalentTest.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1662CheckIfTwoStringArraysAreEquivalentTest {
+        [[TestMethod]
+        public void ArrayStringsAreEqualTest () {
+            Assert.IsTrue(P1662CheckIfTwoStringArraysAreEquivalent.ArrayStringsAreEqual(new string[] { "ab", "c" }, new string[] { "a", "bc" }));
+            Assert.IsFalse(P1662CheckIfTwoStringArraysAreEquivalent.ArrayStringsAreEqual(new string[] { "a", "cb" }, new string[] { "a", "bc" }));
+        }
+    }
+}
