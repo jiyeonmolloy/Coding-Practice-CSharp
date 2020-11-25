58967f0b6566d58a10f3b0806c9ee12071f51b48
Ji <jjeonie@gmail.com>
2020-11-25 22:07:09 +1300

    LeetCode P227 Basic Calculator II

---
+++ LeetCode/Src/P227BasicCalculatorII.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P227 Basic Calculator II
+    ///  href = https://leetcode.com/problems/basic-calculator-ii/
+    /// </summary>
+    class P227BasicCalculatorII {
+        public static int Calculate (string s) {
+            if (string.IsNullOrEmpty(s)) {
+                return 0;
+            }
+
+            Stack numStack = new Stack();
+            int number = 0;
+            int calculated = 0;
+            char target;
+            char operation = '+';
+            for (int i = 0; i < s.Length; i++) {
+                target = s[i];
+                if (Char.IsDigit(target)) {
+                    number = number * 10 + (target - '0');
+                }
+
+                if (!Char.IsDigit(target) && !Char.IsWhiteSpace(target) || i == s.Length - 1) {
+                    if (operation == '+') {
+                        calculated = number;
+                    } else if (operation == '-') {
+                        calculated = -number;
+                    } else if (operation == '*') {
+                        calculated = (int)numStack.Pop() * number;
+                    } else if (operation == '/') {
+                        calculated = (int)numStack.Pop() / number;
+                    }
+                    numStack.Push(calculated);
+                    operation = target;
+                    number = 0;
+                }
+            }
+
+            int result = 0;
+            while (numStack.Count > 0) {
+                result += (int)numStack.Pop();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/Test/P227BasicCalculatorIITest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P227BasicCalculatorIITest {
+        [TestMethod]
+        public void CalculateTest () {
+            Assert.AreEqual(7, P227BasicCalculatorII.Calculate("3+2*2"));
+            Assert.AreEqual(1, P227BasicCalculatorII.Calculate(" 3/2 "));
+            Assert.AreEqual(5, P227BasicCalculatorII.Calculate(" 3+5 / 2 "));
+        }
+    }
+}
