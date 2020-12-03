aa480a91fbab2cefe0dc8c7807b44ecfa5bebc5c
Ji Paik <jjeonie@gmail.com>
2020-12-03 20:48:11 +1300

    LeetCode Weekly Contest 217 P1672 Richest Customer Wealth

---
+++ LeetCode/Src/P1672RichestCustomerWealth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1672 Richest Customer Wealth
+    /// href = https://leetcode.com/problems/richest-customer-wealth/
+    /// </summary>
+    class P1672RichestCustomerWealth {
+        public static int MaximumWealth (int[][] accounts) {
+            int max = 0;
+            for (int i = 0; i < accounts.Length; i++) {
+                int count = 0;
+                for (int j = 0; j < accounts[0].Length; j++) {
+                    count += accounts[i][j];
+                }
+
+                max = Math.Max(count, max);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Test/P1672RichestCustomerWealthTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1672RichestCustomerWealthTest {
+        [TestMethod]
+        public void MaximumWealthTest () {
+            Assert.AreEqual(10, P1672RichestCustomerWealth.MaximumWealth(new int[][] { new int[] { 1, 5 }, new int[] { 7, 3 }, new int[] { 3, 5 } }));
+        }
+    }
+}
