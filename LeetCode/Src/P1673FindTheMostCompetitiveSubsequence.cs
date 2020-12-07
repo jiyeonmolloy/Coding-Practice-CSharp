d6ad853989ec242792b5970971dd7ad0dba89253
Ji Paik <jjeonie@gmail.com>
2020-12-07 19:37:07 +1300

    LeetCode Weekly Contest 217 P1673 Find The Most Competitive Subsequence

---
+++ LeetCode/Src/P1673FindTheMostCompetitiveSubsequence.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    ///  P1673 Find The Most Competitive Subsequence
+    /// href = https://leetcode.com/problems/find-the-most-competitive-subsequence/
+    /// </summary>
+    class P1673FindTheMostCompetitiveSubsequence {
+        public static int[] MostCompetitive (int[] nums, int k) {
+            // Key: Monotonic Stack
+            int n = nums.Length;
+            if (n == k) {
+                return nums;
+            }
+
+            Stack<int> monoStack = new Stack<int>();
+
+            int remainder;
+            int target;
+            for (int i = 0; i < n; i++) {
+                remainder = n - i;
+                target = nums[i];
+
+                while (monoStack.Count > 0 && monoStack.Peek() > target && (remainder + monoStack.Count) > k) {
+                    monoStack.Pop();
+                }
+
+                monoStack.Push(target);
+            }
+
+            return monoStack.Reverse().Take(k).ToArray();
+        }
+    }
+}
---
+++ LeetCode/Test/P1673FindTheMostCompetitiveSubsequenceTest.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1673FindTheMostCompetitiveSubsequenceTest {
+        [TestMethod]
+        public void MostCompetitiveTest () {
+            CollectionAssert.AreEqual(new int[] { 2, 3, 3, 4 }, P1673FindTheMostCompetitiveSubsequence.MostCompetitive(new int[] { 2, 4, 3, 3, 5, 4, 9, 6 }, 4));
+            CollectionAssert.AreEqual(new int[] { 2, 6 }, P1673FindTheMostCompetitiveSubsequence.MostCompetitive(new int[] { 3, 5, 2, 6 }, 2));
+            CollectionAssert.AreEqual(new int[] { 0, 42, 15 }, P1673FindTheMostCompetitiveSubsequence.MostCompetitive(new int[] { 65, 35, 89, 39, 25, 0, 42, 87, 15 }, 3));
+        }
+    }
+}
