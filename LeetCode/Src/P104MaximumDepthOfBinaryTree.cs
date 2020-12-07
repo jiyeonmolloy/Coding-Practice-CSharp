788dc3be56093195dc8534ac6ec4e4e2483b1d30
Ji Paik <jjeonie@gmail.com>
2020-12-07 19:58:59 +1300

    LeetCode P104 Maximum Depth Of Binary Tree

---
+++ LeetCode/Src/P104MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P104 Maximum Depth Of Binary Tree
+    /// href = https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    /// </summary>
+    class P104MaximumDepthOfBinaryTree : TreeNode {
+        public static int MaxDepth (TreeNode root) {
+            if (root == null) {
+                return 0;
+            } else if (root.left == null && root.right == null) {
+                return 1;
+            } else if (root.left == null) {
+                return MaxDepth(root.right) + 1;
+            } else if (root.right == null) {
+                return MaxDepth(root.left) + 1;
+            }
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
---
+++ LeetCode/Test/P104MaximumDepthOfBinaryTreeTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+using LeetCode.Src.Common;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P104MaximumDepthOfBinaryTreeTest {
+        [TestMethod]
+        public void MaxDepthTest () {
+            Assert.AreEqual(3, P104MaximumDepthOfBinaryTree.MaxDepth(new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)))));
+            Assert.AreEqual(2, P111MinimumDepthOfBinaryTree.MinDepth(new TreeNode(1,
+                null,
+                new TreeNode(2))));
+        }
+    }
+}
