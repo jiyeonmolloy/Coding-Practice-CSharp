0cc3c44a5438b95afb02caccb57031fc8eb1755b
Ji Paik <jjeonie@gmail.com>
2020-12-09 14:17:38 +1300

    LeetCode P1010 Pairs Of Songs With Total Durations Divisible By 60

---
+++ LeetCode/Src/P1010PairsOfSongsWithTotalDurationsDivisibleBy60.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1010 Pairs Of Songs With Total Durations Divisible By 60
+    /// href = https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+    /// </summary>
+    class P1010PairsOfSongsWithTotalDurationsDivisibleBy60 {
+        public static int NumPairsDivisibleBy60 (int[] time) {
+            int[] remaining = new int[60];
+            int count = 0;
+            int target = 0;
+            foreach (int current in time) {
+                target = current % 60;
+                if (target == 0) {
+                    count += remaining[0];
+                } else {
+                    count += remaining[60 - target];
+                }
+
+                remaining[target]++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ LeetCode/Test/P1010PairsOfSongsWithTotalDurationsDivisibleBy60Test.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1010PairsOfSongsWithTotalDurationsDivisibleBy60Test {
+        [TestMethod]
+        public void NumPairsDivisibleBy60Test () {
+            Assert.AreEqual(3, P1010PairsOfSongsWithTotalDurationsDivisibleBy60.NumPairsDivisibleBy60(new[] { 30, 20, 150, 100, 40 }));
+            Assert.AreEqual(3, P1010PairsOfSongsWithTotalDurationsDivisibleBy60.NumPairsDivisibleBy60(new[] { 60, 60, 60 }));
+        }
+    }
+}
