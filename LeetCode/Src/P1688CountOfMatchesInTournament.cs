438a60e32d71a96d1f0955a9573d2ff93d03cc03
Ji Paik <jjeonie@gmail.com>
2020-12-19 11:39:05 +1300

    LeetCode Weekly Contest 219 P1688 Count Of Matches In Tournament

---
+++ LeetCode/Src/P1688CountOfMatchesInTournament.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1688 Count Of Matches In Tournament
+    /// href = https://leetcode.com/problems/count-of-matches-in-tournament/
+    /// </summary>
+    class P1688CountOfMatchesInTournament {
+        public static int NumberOfMatches (int n) {
+            int matches = 0;
+            while (n != 1) {
+                if (n % 2 == 0) {
+                    matches += n / 2;
+                    n = n / 2;
+                } else {
+                    matches += (n - 1) / 2;
+                    n = (n - 1) / 2 + 1;
+                }
+            }
+
+            return matches;
+        }
+    }
+}
---
+++ LeetCode/Test/P1688CountOfMatchesInTournamentTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1688CountOfMatchesInTournamentTest {
+        [TestMethod]
+        public void NumberOfMatchesTest () {
+            Assert.AreEqual(6, P1688CountOfMatchesInTournament.NumberOfMatches(7));
+            Assert.AreEqual(13, P1688CountOfMatchesInTournament.NumberOfMatches(14));
+        }
+    }
+}
