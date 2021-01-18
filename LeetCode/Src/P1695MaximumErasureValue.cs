2f0402bd5d676740d7b6e8f0175ecdc33ba6b644
Ji Paik <jjeonie@gmail.com>
2021-01-18 22:12:23 +1300

    LeetCode Weekly Contest 220 P1695 Maximum Erasure Value

---
+++ LeetCode/Src/P1695MaximumErasureValue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1695 Maximum Erasure Value
+    /// href = https://leetcode.com/problems/maximum-erasure-value/
+    /// </summary>
+    class P1695MaximumErasureValue {
+        public static int MaximumUniqueSubarray (int[] nums) {
+            int n = nums.Length;
+            Queue<int> queue = new Queue<int>();
+
+            int target;
+            int current = 0;
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                target = nums[i];
+
+                int remove = 0;
+
+                if (queue.Contains(target)) { // contains
+                    while (queue.Peek() != target) {
+                        remove = queue.Dequeue();
+                        current -= remove;
+                    }
+
+                    current -= queue.Dequeue();
+                }
+
+
+                current += target;
+                queue.Enqueue(target);
+
+                max = Math.Max(max, current);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCode/Test/P1695MaximumErasureValueTest.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1695MaximumErasureValueTest {
+        [TestMethod]
+        public void MaximumUniqueSubarrayTest () {
+            Assert.AreEqual(2, P1695MaximumErasureValue.MaximumUniqueSubarray(new int[] { 2 }));
+            Assert.AreEqual(17, P1695MaximumErasureValue.MaximumUniqueSubarray(new int[] { 4, 2, 4, 5, 6 }));
+            Assert.AreEqual(8, P1695MaximumErasureValue.MaximumUniqueSubarray(new int[] { 5, 2, 1, 2, 5, 2, 1, 2, 5 }));
+            Assert.AreEqual(10001, P1695MaximumErasureValue.MaximumUniqueSubarray(new int[] { 10000, 1, 10000, 1, 1, 1, 1, 1, 1 }));
+        }
+    }
+}
