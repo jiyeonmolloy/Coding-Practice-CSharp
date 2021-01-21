e4c10ec5279ea49db8f1bedbb583b1395b54dd2c
Ji Paik <jjeonie@gmail.com>
2021-01-21 20:38:39 +1300

    LeetCode P1539 Kth Missing Positive Number

---
+++ LeetCode/Src/P1539KthMissingPositiveNumber.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1539 Kth Missing Positive Number
+    /// href = https://leetcode.com/problems/kth-missing-positive-number/
+    /// </summary>
+    class P1539KthMissingPositiveNumber {
+        public static int FindKthPositive (int[] arr, int k) {
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            foreach (var element in arr) {
+                dictionary.Add(element, 1);
+            }
+
+            int index = 1;
+            while (k > 0) {
+                if (!dictionary.ContainsKey(index)) {
+                    k--;
+                }
+                index++;
+            }
+            return index - 1;
+        }
+    }
+}
---
+++ LeetCode/Test/P1539KthMissingPositiveNumberTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1539KthMissingPositiveNumberTest {
+        [TestMethod]
+        public void FindKthPositiveTest () {
+            Assert.AreEqual(9, P1539KthMissingPositiveNumber.FindKthPositive(new[] { 2, 3, 4, 7, 11 }, 5));
+            Assert.AreEqual(6, P1539KthMissingPositiveNumber.FindKthPositive(new[] { 1, 2, 3, 4 }, 2));
+        }
+    }
+}
