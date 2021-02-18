7fc5cc67e8c5178711da4dea35310d7cc5ca13fb
Ji Paik <jjeonie@gmail.com>
2021-02-18 15:16:16 +1300

    LeetCode P1437 Check If All Ones Are At Least Length K Places Away

---
+++ LeetCode/Src/P1437CheckIfAllOnesAreAtLeastLengthKPlacesAway.cs
@@ -0,0 +1,26 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1437 Check If All Ones Are At Least Length K Places Away
+    /// href = https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+    /// </summary>
+    class P1437CheckIfAllOnesAreAtLeastLengthKPlacesAway {
+        public static bool KLengthApart (int[] nums, int k) {
+            int length = k;
+
+            foreach (int num in nums) {
+                if (num == 1) {
+                    if (length < k) {
+                        return false;
+                    }
+
+                    length = 0;
+
+                } else {
+                    length++;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/Test/P1437CheckIfAllOnesAreAtLeastLengthKPlacesAwayTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1437CheckIfAllOnesAreAtLeastLengthKPlacesAwayTest {
+        [TestMethod]
+        public void KLengthApartTest () {
+            Assert.IsTrue(P1437CheckIfAllOnesAreAtLeastLengthKPlacesAway.KLengthApart(new[] { 1, 0, 0, 0, 1, 0, 0, 1 }, 2));
+            Assert.IsFalse(P1437CheckIfAllOnesAreAtLeastLengthKPlacesAway.KLengthApart(new[] { 1, 0, 0, 1, 0, 1 }, 2));
+        }
+    }
+}
