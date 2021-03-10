7bf5756c1fc35f2254fc1908af99351bda1ce187
Ji Paik <jjeonie@gmail.com>
2021-03-10 15:15:43 +1300

    LeetCode Weekly Contest 232 P1784 Check If Binary String Has At Most One Segment Of Ones

---
+++ LeetCode/Src/P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LeetCode {
+    /// <summary>
+    /// P1784 Check If Binary String Has At Most One Segment Of Ones
+    /// href = https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/
+    /// </summary>
+    class P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes {
+        public static bool CheckOnesSegment (string s) {
+            return !s.Contains("01");
+        }
+    }
+}
---
+++ LeetCode/Test/P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnesTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnesTest {
+        [TestMethod]
+        public void CheckOnesSegmentTest () {
+            Assert.IsTrue(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("11"));
+            Assert.IsFalse(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("1001"));
+            Assert.IsTrue(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("110"));
+            Assert.IsTrue(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("1"));
+            Assert.IsTrue(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("10"));
+            Assert.IsFalse(P1784CheckIfBinaryStringHasAtMostOneSegmentOfOnes.CheckOnesSegment("1100111"));
+        }
+    }
+}
