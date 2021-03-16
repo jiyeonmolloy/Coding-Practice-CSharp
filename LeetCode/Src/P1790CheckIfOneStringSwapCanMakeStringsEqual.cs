b417583e0e0f077b66d874f2fb7c201dfa1aeb4b
Ji Paik <jjeonie@gmail.com>
2021-03-16 17:14:39 +1300

    LeetCode Weekly Contest 232 P1790 Check If One String Swap Can Make Strings Equal

---
+++ LeetCode/Src/P1790CheckIfOneStringSwapCanMakeStringsEqual.cs
@@ -0,0 +1,38 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1790 Check If One String Swap Can Make Strings Equal
+    /// href = https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/
+    /// </summary>
+    class P1790CheckIfOneStringSwapCanMakeStringsEqual {
+        public static bool AreAlmostEqual (string s1, string s2) {
+            bool isDifferent = false;
+            char toSwapS1 = 'a';
+            char toSwapS2 = 'a';
+
+            for (int i = 0; i < s1.Length; i++) {
+                char targetS1 = s1[i];
+                char targetS2 = s2[i];
+                if (targetS1.Equals(targetS2)) {
+                    continue;
+                }
+
+                if (!isDifferent) {
+                    toSwapS1 = targetS1;
+                    toSwapS2 = targetS2;
+                    isDifferent = true;
+                    continue;
+                }
+
+                if (targetS1.Equals(toSwapS2) && targetS2.Equals(toSwapS1)) {
+                    isDifferent = false;
+                    continue;
+                }
+
+                return false;
+            }
+
+            return !isDifferent;
+        }
+    }
+}
---
+++ LeetCode/Test/P1790CheckIfOneStringSwapCanMakeStringsEqualTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1790CheckIfOneStringSwapCanMakeStringsEqualTest {
+        [TestMethod]
+        public void AreAlmostEqualTest () {
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("abce", "dbca"));
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("abcd", "baec"));
+            Assert.IsTrue(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("bank", "kanb"));
+            Assert.IsTrue(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("kelb", "kelb"));
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("abcd", "dcba"));
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("attack", "defend"));
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("a", "d"));
+            Assert.IsTrue(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("a", "a"));
+            Assert.IsFalse(P1790CheckIfOneStringSwapCanMakeStringsEqual.AreAlmostEqual("ab", "ad"));
+        }
+    }
+}
