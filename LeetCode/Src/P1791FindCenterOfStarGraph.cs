65c1468f9fc72fe424ade3a4539d20e83c0c12ad
Ji Paik <jjeonie@gmail.com>
2021-03-16 17:17:06 +1300

    LeetCode Weekly Contest 232 P1791 Find Center Of Star Graph

---
+++ LeetCode/Src/P1791FindCenterOfStarGraph.cs
@@ -0,0 +1,32 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1791 Find Center Of Star Graph
+    /// href = https://leetcode.com/problems/find-center-of-star-graph/
+    /// </summary>
+    class P1791FindCenterOfStarGraph {
+        public static int FindCenter (int[][] edges) {
+            int firstCandidate = edges[0][0];
+            int secondCandidate = edges[0][1];
+
+            bool firstIteration = true;
+            foreach (int[] edge in edges) {
+                if (firstIteration) {
+                    firstIteration = false;
+                    continue;
+                }
+
+                if (firstCandidate == edge[0] || firstCandidate == edge[1]) {
+                    return firstCandidate;
+                }
+
+                if (secondCandidate == edge[0] || secondCandidate == edge[1]) {
+                    return secondCandidate;
+                }
+            }
+
+            // shouldn't reach here  
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/Test/P1791FindCenterOfStarGraphTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1791FindCenterOfStarGraphTest {
+        [TestMethod]
+        public void FindCenterTest () {
+            Assert.AreEqual(2, P1791FindCenterOfStarGraph.FindCenter(new int[][] { new int[] { 1, 2 }, new int[] { 2, 3 }, new int[] { 4, 2 } }));
+            Assert.AreEqual(1, P1791FindCenterOfStarGraph.FindCenter(new int[][] { new int[] { 1, 2 }, new int[] { 5, 1 }, new int[] { 1, 3 } }));
+        }
+    }
+}
