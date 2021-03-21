c51f26d4ada6437aff6b214951fbc2f96f98e9f3
Ji Paik <jjeonie@gmail.com>
2021-03-21 14:39:36 +1300

    LeetCode P1396 Design Underground System

---
+++ LeetCode/Src/P1396DesignUndergroundSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1396 Design Underground System
+    /// href = https://leetcode.com/problems/design-underground-system/
+    /// </summary> P1396DesignUndergroundSystem
+    public class P1396DesignUndergroundSystem {
+        private Dictionary<int, Tuple<string, double>> checkInTimes; // Key:id, station name (start) Value: check-in time
+        private Dictionary<Tuple<string, string>, Tuple<double, double>> totalTimes; // Key: station name (start), station name (destination) Value: total time, freq
+
+        public P1396DesignUndergroundSystem () {
+            checkInTimes = new Dictionary<int, Tuple<string, double>>();
+            totalTimes = new Dictionary<Tuple<string, string>, Tuple<double, double>>();
+        }
+
+        public void CheckIn (int id, string stationName, int t) {
+            if (checkInTimes.ContainsKey(id)) {
+                checkInTimes[id] = new Tuple<string, double>(stationName, t);
+            } else {
+                checkInTimes.Add(id, new Tuple<string, double>(stationName, t));
+            }
+        }
+
+        public void CheckOut (int id, string stationName, int t) {
+            // calculate travel time
+            checkInTimes.TryGetValue(id, out var checkIn);
+            double travelTime = t - checkIn.Item2;
+            string startStation = checkIn.Item1;
+
+            // calculate average times, if entry does not exist create a new one
+            if (totalTimes.TryGetValue(new Tuple<string, string>(startStation, stationName), out var totalTime)) {
+                totalTimes[new Tuple<string, string>(startStation, stationName)] = new Tuple<double, double>((totalTime.Item1 + travelTime), (totalTime.Item2 + 1));
+            } else {
+                totalTimes.Add(new Tuple<string, string>(startStation, stationName), new Tuple<double, double>(travelTime, 1));
+            }
+        }
+
+        public double GetAverageTime (string startStation, string endStation) {
+            if (totalTimes != null && totalTimes.TryGetValue(new Tuple<string, string>(startStation, endStation), out var totalTime)) {
+                return Math.Round(totalTime.Item1 / totalTime.Item2, 5);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/Test/P1396DesignUndergroundSystemTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1396DesignUndergroundSystemTest {
+        [TestMethod]
+        public void UndergroundSystemTest () {
+            P1396DesignUndergroundSystem undergroundSystem = new P1396DesignUndergroundSystem();
+            undergroundSystem.CheckIn(45, "Leyton", 3);
+            undergroundSystem.CheckIn(32, "Paradise", 8);
+            undergroundSystem.CheckIn(27, "Leyton", 10);
+            undergroundSystem.CheckOut(45, "Waterloo", 15);
+            undergroundSystem.CheckOut(27, "Waterloo", 20);
+            undergroundSystem.CheckOut(32, "Cambridge", 22);
+            Assert.AreEqual(14.00000, undergroundSystem.GetAverageTime("Paradise", "Cambridge"));
+            Assert.AreEqual(11.00000, undergroundSystem.GetAverageTime("Leyton", "Waterloo"));
+            undergroundSystem.CheckIn(10, "Leyton", 24);
+            Assert.AreEqual(11.00000, undergroundSystem.GetAverageTime("Leyton", "Waterloo"));
+            undergroundSystem.CheckOut(10, "Waterloo", 38);
+            Assert.AreEqual(12.00000, undergroundSystem.GetAverageTime("Leyton", "Waterloo"));
+        }
+    }
+}
