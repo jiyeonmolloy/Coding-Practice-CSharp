571eb398ffb04d9b52d63d731834997e56e03a01
Ji Paik <jjeonie@gmail.com>
2021-04-11 10:23:30 +1200

    LeetCode P1704 Determine If String Halves Are Alike

---
+++ LeetCode/Src/P1704DetermineIfStringHalvesAreAlike.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1704 Determine If String Halves Are Alike
+    /// href = https://leetcode.com/problems/determine-if-string-halves-are-alike/
+    /// </summary>
+    class P1704DetermineIfStringHalvesAreAlike {
+        public static bool HalvesAreAlike (string s) {
+            int half = s.Length / 2;
+            string firstHalf = s.Substring(0, half);
+            string secondHalf = s.Substring(half, half);
+
+            int firstHalfVowelCount = 0;
+            int secondHalfVowelCount = 0;
+            for (int i = 0; i < half; i++) {
+                if (IsVowel(firstHalf[i])) {
+                    firstHalfVowelCount++;
+                }
+
+                if (IsVowel(secondHalf[i])) {
+                    secondHalfVowelCount++;
+                }
+            }
+
+            return firstHalfVowelCount == secondHalfVowelCount;
+        }
+
+        private static bool IsVowel (char c) {
+            return "aeiouAEIOU".Contains(c);
+        }
+    }
+}
---
+++ LeetCode/Test/P1704DetermineIfStringHalvesAreAlikeTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1704DetermineIfStringHalvesAreAlikeTest {
+        [TestMethod]
+        public void HalvesAreAlikeTest () {
+            Assert.IsTrue(P1704DetermineIfStringHalvesAreAlike.HalvesAreAlike("book"));
+            Assert.IsFalse(P1704DetermineIfStringHalvesAreAlike.HalvesAreAlike("textbook"));
+            Assert.IsFalse(P1704DetermineIfStringHalvesAreAlike.HalvesAreAlike("MerryChristmas"));
+            Assert.IsTrue(P1704DetermineIfStringHalvesAreAlike.HalvesAreAlike("AbCdEfGh"));
+        }
+    }
+}
