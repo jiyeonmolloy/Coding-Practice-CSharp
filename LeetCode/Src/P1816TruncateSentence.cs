a92dfd67a0ac136467560b7c8c266164bf489ff1
Ji Paik <jjeonie@gmail.com>
2021-04-13 20:52:14 +1200

    LeetCode P1816 Truncate Sentence

---
+++ LeetCode/Src/P1816TruncateSentence.cs
@@ -0,0 +1,23 @@
+﻿
+namespace LeetCode.Src {
+    /// <summary>
+    /// P1816 Truncate Sentence
+    /// href = https://leetcode.com/problems/truncate-sentence/
+    /// </summary>
+    class P1816TruncateSentence {
+        public static string TruncateSentence (string s, int k) {
+            string[] words = s.Split(' ');
+            string sentence = words[0];
+
+            if (words.Length == 1) {
+                return s;
+            }
+
+            for (int i = 1; i < k; i++) {
+                sentence += ' ' + words[i];
+            }
+
+            return sentence;
+        }
+    }
+}
---
+++ LeetCode/Test/P1816TruncateSentenceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Src;
+
+namespace LeetCode.Test {
+    [TestClass]
+    public class P1816TruncateSentenceTest {
+        [TestMethod]
+        public void TruncateSentenceTest () {
+            Assert.AreEqual("Hello how are you", P1816TruncateSentence.TruncateSentence("Hello how are you Will", 4));
+            Assert.AreEqual("What is the solution", P1816TruncateSentence.TruncateSentence("What is the solution to this problem", 4));
+            Assert.AreEqual("chopper is not a tanuki", P1816TruncateSentence.TruncateSentence("chopper is not a tanuki", 5));
+        }
+    }
+}
